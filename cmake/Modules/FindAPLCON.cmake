42cbb0c351816954ac6e2fadfe198cd78754f066
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-06-02 14:07:26 +0200

    Except for the dangling GoAT dependency, it compiles now

--- cmake/Modules/FindAPLCON.cmake
+++ cmake/Modules/FindAPLCON.cmake
@@ -2,7 +2,7 @@
 # simplistic, should mark variables as cached or advanced
 
 message(STATUS "Looking for APLCON...")
-
+set(APLCON_FOUND FALSE)
 
 set(APLCON_SEARCH_PATHS
   ${CMAKE_SOURCE_DIR}/../APLCON)
@@ -11,19 +11,26 @@ find_path(APLCON_INCLUDE_DIR APLCON.hpp
   PATHS ${APLCON_SEARCH_PATHS}/src
   NO_DEFAULT_PATH
   )
-
 if(NOT APLCON_INCLUDE_DIR)
   Message(STATUS "Looking for APLCON... - APLCON.hpp not found")
-  if(APLCON_FIND_REQUIRED)
-    message(FATAL_ERROR "APLCON is required, please make sure cmake finds it")
-  endif()
   return()
 endif()
 
-FIND_LIBRARY(APLCON_LIBRARIES NAMES aplcon++
-  PATHS ${APLCON_SEARCH_PATHS}/build
+find_library(APLCON_LIBRARIES NAMES aplcon++
+  PATHS ${APLCON_SEARCH_PATHS}
+  PATH_SUFFIXES build
   NO_DEFAULT_PATH
   )
+if(NOT APLCON_LIBRARIES)
+  Message(STATUS "Looking for APLCON... - aplcon++ library not found.")
+endif()
 
-set(APLCON_FOUND TRUE)
-Message(STATUS "Looking for APLCON... - Found ${APLCON_LIBRARIES}")
+
+if(APLCON_INCLUDE_DIR AND APLCON_LIBRARIES)
+  set(APLCON_FOUND TRUE)
+  Message(STATUS "Looking for APLCON... - Found ${APLCON_LIBRARIES}")
+endif()
+
+if(NOT APLCON_FOUND AND APLCON_FIND_REQUIRED)
+  message(FATAL_ERROR "APLCON is required, exit.")
+endif()
---
+++ cmake/settings.cmake
@@ -0,0 +1,29 @@
+# every subdirectory has its own bin/lib path
+# this should be changed to one "global" directory...
+if(NOT DEFINED EXECUTABLE_OUTPUT_PATH)
+	set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin")
+endif()
+
+if(NOT DEFINED LIBRARY_OUTPUT_PATH)
+	set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/lib")
+endif()
+
+# we check for empty string here, since the variable
+# is indeed defined to an empty string
+if(NOT CMAKE_BUILD_TYPE)
+  # this also reflects this default in the GUI
+	SET(CMAKE_BUILD_TYPE Release CACHE STRING
+    "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
+    FORCE)
+endif()
+
+# really no optimization in debug mode
+if(CMAKE_COMPILER_IS_GNUCXX)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -Wall")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
+else()
+  message(FATAL_ERROR "Non-gnu compiler not supported at the moment")
+endif()
+
+string(TOUPPER ${CMAKE_BUILD_TYPE} BUILD_TYPE)
+set(DEFAULT_COMPILE_FLAGS ${CMAKE_CXX_FLAGS_${BUILD_TYPE}})
