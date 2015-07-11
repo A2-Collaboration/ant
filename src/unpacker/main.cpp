eb759106c655fd9fe3b1c563701a0fc43eb8dcdd
Andreas Neiser <andreas.neiser@gmail.com>
2015-07-11 23:28:10 +0200

    Add ROOT file writing in main.cpp for performance tests

--- src/unpacker/main.cpp
+++ src/unpacker/main.cpp
@@ -7,14 +7,21 @@
 #include <cstring>
 #include "Unpacker.h"
 #include "expconfig/ExpConfig.h"
+
 #include "tree/THeaderInfo.h"
+#include "tree/TUnpackerMessage.h"
+#include "tree/TSlowControl.h"
 #include "tree/TDetectorRead.h"
+
 #include "unpacker/RawFileReader.h"
 
 #include "base/Logger.h"
 #include "base/Format.h"
 
 #include <chrono>
+
+#include "TFile.h"
+#include "TTree.h"
 
 using namespace std;
 using namespace ant;
@@ -25,47 +32,67 @@ int main(int argc, char* argv[]) {
   std::chrono::time_point<std::chrono::system_clock> start, end;
   start = std::chrono::system_clock::now();
 
-  //std::vector<double> v{2,3};
-  //string str = fmt::format("Bla {}", v);
-  //cout << str << endl;
-
   //auto unpacker = Unpacker::Get("scratch/CBTaggTAPS_9227.dat");
   //auto unpacker = Unpacker::Get("scratch/CBTaggTAPS_7892.dat");
   auto unpacker = Unpacker::Get("scratch/CBTaggTAPS_5711.dat.xz");
-//  auto unpacker = Unpacker::Get("scratch/oneevent-small.dat");
+  //auto unpacker = Unpacker::Get("scratch/oneevent-small.dat");
   unsigned nReads = 0;
+
+  // write some stuff to a ROOT tree
+  TFile* file = new TFile("unpacker_test_out.root","RECREATE");
+
+  TTree* treeHeaderInfo = new TTree("treeHeaderInfo", "treeHeaderInfo");
+  THeaderInfo* HeaderInfo = new THeaderInfo();
+  treeHeaderInfo->Branch("THeaderInfo", "ant::THeaderInfo", &HeaderInfo);
+
+  TTree* treeUnpackerMessage = new TTree("treeUnpackerMessage", "treeUnpackerMessage");
+  TUnpackerMessage* UnpackerMessage = new TUnpackerMessage();
+  treeUnpackerMessage->Branch("UnpackerMessage", "ant::TUnpackerMessage", &UnpackerMessage);
+
+  TTree* treeSlowControl = new TTree("treeSlowControl", "treeSlowControl");
+  TSlowControl* SlowControl = new TSlowControl();
+  treeSlowControl->Branch("SlowControl", "ant::TSlowControl", &SlowControl);
+
+  TTree* treeDetectorRead = new TTree("treeDetectorRead", "treeDetectorRead");
+  TDetectorRead* DetectorRead = new TDetectorRead();
+  treeDetectorRead->Branch("DetectorRead", "ant::TDetectorRead", &DetectorRead);
+
 
   while(auto item = unpacker->NextItem()) {
     //cout << *item << endl;
-    const TDetectorRead* read = dynamic_cast<const TDetectorRead*>(item.get());
-    if(read == nullptr)
+    HeaderInfo = dynamic_cast<THeaderInfo*>(item.get());
+    if(HeaderInfo != nullptr) {
+      treeHeaderInfo->Fill();
       continue;
-    //cout << read << endl;
-    nReads++;
-    if(nReads % 10000 == 0) {
-      VLOG(5) << "Unpacked " << nReads << " detector reads";
+    }
+    UnpackerMessage = dynamic_cast<TUnpackerMessage*>(item.get());
+    if(UnpackerMessage != nullptr) {
+      treeUnpackerMessage->Fill();
+      continue;
+    }
+    SlowControl = dynamic_cast<TSlowControl*>(item.get());
+    if(SlowControl != nullptr) {
+      treeSlowControl->Fill();
+      continue;
+    }
+    DetectorRead = dynamic_cast<TDetectorRead*>(item.get());
+    if(DetectorRead != nullptr) {
+      treeDetectorRead->Fill();
+      nReads++;
+      if(nReads % 10000 == 0) {
+        VLOG(5) << "Unpacked " << nReads << " detector reads";
+      }
+      continue;
     }
   }
+
+  file->Write();
+  file->Close();
+  delete file;
 
   end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
   cout << "Analyzed " << nReads/elapsed_seconds.count() << " Reads/s" << endl;
-
-//  THeaderInfo header(TDataRecord::ID_t(0,0), 0, "", 0);
-//  auto config = shared_ptr<ExpConfig::Module>(ExpConfig::Get(header));
-//  LOG(INFO) << "Got config: " << config << endl;
-
-  //cout << unpacker->OpenFile("scratch/CBTaggTAPS_7892.dat.xz") << endl;
-
-//  cout << unpacker->NextItem() << endl;
-
-//  vector<int> v{1,2,3,4};
-
-//  LOG(INFO) << v;
-
-//  for(size_t i=0;i<100;i++) {
-//    LOG_N_TIMES(10,WARNING) << "OHOH";
-//  }
 
   return EXIT_SUCCESS;
 }
