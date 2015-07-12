fe97349c9005ac1edafbc9f8c6fe51627fee1a52
Andreas Neiser <andreas.neiser@gmail.com>
2015-07-12 09:13:37 +0200

    Expconfig: Extract Detector_t to separate file

--- src/expconfig/ExpConfig.cc
+++ src/expconfig/ExpConfig.cc
@@ -1,84 +1,16 @@
 #include "ExpConfig.h"
 
-#include "base/Logger.h"
+#include "setups/Setup_2014_EtaPrime.h"
 
 #include "unpacker/tree/THeaderInfo.h"
 #include "unpacker/UnpackerAcqu.h"
 
-#include "setups/Setup_2014_EtaPrime.h"
+#include "base/Logger.h"
 
 #include <type_traits>
 #include <list>
-#include <cxxabi.h>
-#include <stdexcept>
 
 using namespace std;
-using namespace ant;
-
-bool Channel_t::IsIntegral(const Channel_t::Type_t& t) {
-  switch(t) {
-  case Type_t::Integral:
-  case Type_t::IntegralShort:
-  case Type_t::IntegralAlternate:
-  case Type_t::IntegralShortAlternate:
-    return true;
-  default:
-    return false;
-  }
-}
-
-string Channel_t::ToString(const Type_t& type)
-{
-  switch(type) {
-  case Channel_t::Type_t::BitPattern:
-    return "BitPattern";
-  case Channel_t::Type_t::Counter:
-    return "Counter";
-  case Channel_t::Type_t::Integral:
-    return "Integral";
-  case Channel_t::Type_t::IntegralAlternate:
-    return "IntegralAlternate";
-  case Channel_t::Type_t::IntegralShort:
-    return "IntegralShort";
-  case Channel_t::Type_t::IntegralShortAlternate:
-    return "IntegralShortAlternate";
-  case Channel_t::Type_t::Scaler:
-    return "Scaler";
-  case Channel_t::Type_t::Timing:
-    return "Timing";
-  }
-  throw runtime_error("Not implemented");
-}
-
-string Detector_t::ToString(const Type_t &type)
-{
-  switch(type) {
-  case Detector_t::Type_t::CB :
-    return "CB";
-  case Detector_t::Type_t::Cherenkov:
-    return "Cherenkov";
-  case Detector_t::Type_t::EPT:
-    return "EPT";
-  case Detector_t::Type_t::Moeller:
-    return "Moeller";
-  case Detector_t::Type_t::MWPC0:
-    return "MWPC0";
-  case Detector_t::Type_t::MWPC1:
-    return "MWPC1";
-  case Detector_t::Type_t::PID:
-    return "PID";
-  case Detector_t::Type_t::Tagger:
-    return "Tagger";
-  case Detector_t::Type_t::TAPS:
-    return "TAPS";
-  case Detector_t::Type_t::TAPSVeto:
-    return "TAPSVeto";
-  case Detector_t::Type_t::Trigger:
-    return "Trigger";
-  }
-  throw runtime_error("Not implemented");
-}
-
 
 namespace ant { // templates need explicit namespace
 
@@ -131,10 +63,6 @@ unique_ptr< UnpackerAcquConfig >
 ExpConfig::Unpacker<UnpackerAcquConfig>::Get(const THeaderInfo& header) {
   return Get_< UnpackerAcquConfig >(header);
 }
-
-
-
-
 
 } // namespace ant
 
--- src/expconfig/ExpConfig.h
+++ src/expconfig/ExpConfig.h
@@ -6,56 +6,11 @@
 #include <cstdint>
 #include "base/printable.h"
 
+#include "Detector_t.h"
+
 namespace ant {
 
 class THeaderInfo;
-
-struct Channel_t {
-  enum class Type_t : std::uint8_t {
-    Timing,
-    Integral, IntegralShort,
-    IntegralAlternate, IntegralShortAlternate,
-    BitPattern, Scaler, Counter
-  };
-  static bool IsIntegral(const Type_t& t);
-  static std::string ToString(const Type_t& type);
-};
-
-struct Detector_t {
-  enum class Type_t : std::uint8_t {
-    Trigger, Tagger, EPT, CB, PID, MWPC0, MWPC1,
-    TAPS, TAPSVeto, Cherenkov, Moeller
-  };
-  const Type_t Type;
-  static std::string ToString(const Type_t& type);
-
-  // Element_t is the minimum information,
-  // derived classes may extend this
-  struct Element_t {
-    struct Position_t {
-      double X, Y, Z;
-    };
-    Element_t(unsigned channel, const Position_t& position) :
-      Channel(channel),
-      Position(position)
-    {}
-    unsigned Channel; // unique within Detector for all time!
-    Position_t Position;
-  };
-
-  virtual ~Detector_t() = default;
-
-protected:
-  Detector_t(const Type_t& type) :
-    Type(type) {}
-  Detector_t(const Detector_t&) = delete; // disable copy
-};
-
-struct LogicalChannel_t {
-  Detector_t::Type_t Detector;
-  Channel_t::Type_t Type;
-  unsigned Channel;
-};
 
 class ExpConfig
 {
