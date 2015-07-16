36dc2ec5d49c12a70814687251c359bfb42f1cd3
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-16 09:09:10 +0200

    base: Tree: WIP

--- src/base/Tree.h
+++ src/base/Tree.h
@@ -11,14 +11,15 @@ class Tree {
 protected:
     T data;
 
-    using node_t = std::shared_ptr<Tree>;
-    using nodelist_t = std::list<node_t>;
+    using snode_t    = std::shared_ptr<Tree>;
+    using wnode_t    = std::weak_ptr<Tree>;
+    using snodelist_t = std::list<snode_t>;
 
-    node_t partent;
-    nodelist_t daughters;
+    wnode_t partent;
+    snodelist_t daughters;
 
     void remove_daughter(Tree* t) {
-        daughters.remove_if( [t] (node_t& n) { return n.get() == t;});
+        daughters.remove_if( [t] (snode_t& n) { return n.get() == t;});
     }
 
 public:
@@ -26,16 +27,16 @@ public:
     Tree() {}
     Tree(const T& data_): data(data_) {}
 
-    static node_t makeNode(const T& data) { return std::make_shared<Tree>(data); }
+    static snode_t makeNode(const T& data) { return std::move(std::make_shared<Tree>(data)); }
 
-    static node_t makeNode(node_t p, const T& data) {
-        auto n =  std::make_shared<Tree>(data);
+    static snode_t makeNode(snode_t& p, const T& data) {
+        auto n =  makeNode(data);
         link(p,n);
         return std::move(n);
     }
 
 
-    bool isRoot() const { return partent.get() == nullptr; }
+    bool isRoot() const { return partent.expired(); }
     bool isLeaf() const { return daughters.empty(); }
 
     T& operator*() { return data; }
@@ -43,23 +44,59 @@ public:
 
     const T& get() const { return data; }
 
-    static void link( node_t& p, node_t& daughter) {
+    static void link( snode_t& p, snode_t& daughter) {
+
+        if(!p || !daughter)
+            return;
+
         if( !daughter->isRoot() ) {
             daughter->Unlink();
         }
         daughter->partent = p;
-        p->daughters.push_back(daughter);
+        p->daughters.emplace_back(daughter);
     }
 
+    snode_t GetParent() { return partent.lock(); }
+
     void Unlink() {
-        if(!isRoot()) {
-            partent->remove_daughter(this);
+        if(auto p = GetParent()) {
+            p->remove_daughter(this);
             partent.reset();
         }
     }
 
-    const nodelist_t& Daughters() const { return daughters; }
+    const snodelist_t& Daughters() const { return daughters; }
 
+    template <typename F>
+    void map(F function) const {
+        function(get());
+        for(auto& node: daughters) {
+            node->map(function);
+        }
+    }
+
+    template <typename F>
+    void maplevel(F function, size_t level=0) const {
+        function(get(), level);
+        for(auto& node: daughters) {
+            node->maplevel(function, level+1);
+        }
+    }
+
+    size_t size() const {
+        size_t s = 0;
+        map([&s] (const T&) { s++;});
+        return s;
+    }
+
+    size_t depth() const {
+        size_t d = 0;
+        maplevel(
+                    [&d] (const T&, const size_t& level) {
+            if(level > d) {d=level;}}
+    );
+        return d +1;
+    }
 
 };
 
--- test/base/TestTree.cc
+++ test/base/TestTree.cc
@@ -39,3 +39,41 @@ TEST_CASE("Tree: Parents", "[base]") {
     REQUIRE(a->Daughters().size()==2);
 
 }
+
+TEST_CASE("Tree: Unlink", "[base]") {
+    auto a = Tree<int>::makeNode(10);
+    auto b = Tree<int>::makeNode(a,30);
+    auto c = Tree<int>::makeNode(b,20);
+    auto d = Tree<int>::makeNode(b,35);
+    REQUIRE(a->isRoot() == true);
+    REQUIRE(a->isLeaf() == false);
+    REQUIRE(b->isLeaf() == false);
+    REQUIRE(b->isRoot() == false);
+    b->Unlink();
+    REQUIRE(b->isRoot() == true);
+    REQUIRE(a->isLeaf() == true);
+}
+
+TEST_CASE("Tree: map", "[base]") {
+    auto a = Tree<int>::makeNode(10);
+    auto b = Tree<int>::makeNode(a,30);
+    auto c = Tree<int>::makeNode(b,20);
+    auto d = Tree<int>::makeNode(b,35);
+    a->map([] (const int& i) { cout << i << endl;});
+}
+
+TEST_CASE("Tree: size", "[base]") {
+    auto a = Tree<int>::makeNode(10);
+    auto b = Tree<int>::makeNode(a,30);
+    auto c = Tree<int>::makeNode(b,20);
+    auto d = Tree<int>::makeNode(b,35);
+    REQUIRE(a->size() == 4);
+}
+
+TEST_CASE("Tree: depth", "[base]") {
+    auto a = Tree<int>::makeNode(10);
+    auto b = Tree<int>::makeNode(a,30);
+    auto c = Tree<int>::makeNode(b,20);
+    auto d = Tree<int>::makeNode(b,35);
+    REQUIRE(a->depth() == 3);
+}
