43b5f71a9a632a1d14bc209d2b1761b550d0c800
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-07-16 09:36:35 +0200

    Calibration: use namespaces

--- src/calibration/TestCalCB.cc
+++ src/calibration/TestCalCB.cc
@@ -3,7 +3,11 @@
 #include "analysis/data/Event.h"
 #include "analysis/utils/combinatorics.h"
 
-ant::calibration::TestCalCB::TestCalCB():
+using namespace std;
+using namespace ant;
+using namespace ant::calibration;
+
+TestCalCB::TestCalCB():
     BaseCalibrationModule("TestCalCB")
 {
     const BinSettings energybins(1000);
@@ -11,37 +15,41 @@ ant::calibration::TestCalCB::TestCalCB():
     ggIM = HistFac.makeTH1D("ggIM","2 #gamma IM [MeV]","#",energybins,"ggIM");
 }
 
-void ant::calibration::TestCalCB::ApplyTo(std::unique_ptr<TDetectorRead>&)
+
+
+void TestCalCB::ProcessEvent(const Event &event)
 {
+    const auto& photons = event.Reconstructed().Particles().Get(ParticleTypeDatabase::Photon);
 
-}
-
-void ant::calibration::TestCalCB::ProcessEvent(const ant::Event &event)
-{
-    const auto& tracks = event.Reconstructed().Particles().Get(ParticleTypeDatabase::Photon);
-
-    for( auto comb = makeCombination(tracks,2); !comb.Done(); ++comb ) {
+    for( auto comb = makeCombination(photons,2); !comb.Done(); ++comb ) {
 
         TLorentzVector gg = *comb.at(0) + *comb.at(1);
         ggIM->Fill(gg.M());
     }
 }
 
-void ant::calibration::TestCalCB::Finish()
+void TestCalCB::Finish()
 {
 
 }
 
-void ant::calibration::TestCalCB::ShowResult()
+void TestCalCB::ShowResult()
 {
 
 }
 
 
-void ant::calibration::TestCalCB::BuildRanges(std::list<ant::TDataRecord::ID_t> &ranges)
+void TestCalCB::ApplyTo(unique_ptr<TDetectorRead>&)
 {
+
 }
 
-void ant::calibration::TestCalCB::Update(const ant::TDataRecord::ID_t &id)
+void TestCalCB::BuildRanges(list<TDataRecord::ID_t> &ranges)
 {
+
+}
+
+void TestCalCB::Update(const TDataRecord::ID_t &id)
+{
+
 }
