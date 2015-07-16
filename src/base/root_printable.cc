d19e579cee77d4253e387880ff52725f20395a7c
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-16 19:16:22 +0200

    base: Fix operator<< for TVector3

--- src/base/root_printable.cc
+++ src/base/root_printable.cc
@@ -3,6 +3,6 @@
 
 std::ostream &operator<<(std::ostream &stream, const TVector3 &v)
 {
-    stream << "(" << v.X() << "," << v.Y() << "," << v.Z();
+    stream << "(" << v.X() << "," << v.Y() << "," << v.Z() << ")";
     return stream;
 }
