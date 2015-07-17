7dc2e22213f5a705e9d9e8635550fd81a6c3ae90
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-07-17 18:14:25 +0200

    Reconstruct: WIP Clustering

--- src/reconstruct/Reconstruct.cc
+++ src/reconstruct/Reconstruct.cc
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 
 using namespace std;
 using namespace ant;
@@ -35,10 +36,8 @@ Reconstruct::Reconstruct(const THeaderInfo &headerInfo)
     const auto& updateable = dynamic_pointer_cast<Updateable_traits, Detector_t>(detector);
     if(updateable != nullptr)
       updateables.push_back(updateable);
-    // ... they may be a cluster detector
-    const auto& detector_cluster = dynamic_pointer_cast<ClusterDetector_t, Detector_t>(detector);
-    if(detector_cluster != nullptr)
-      detectors_cluster.push_back(detector_cluster);
+    // ... but also are needed in DoReconstruct
+    sorted_detectors[detector->Type] = detector;
   }
 
   /// \todo build the range list from updateables
@@ -49,13 +48,13 @@ unique_ptr<TEvent> Reconstruct::DoReconstruct(TDetectorRead& detectorRead)
 
   // categorize the hits by detector type
   // this is handy for all subsequent reconstruction steps
-  map<Detector_t::Type_t, std::list< TDetectorReadHit* > > sorted_readhits;
+  map<Detector_t::Type_t, list< TDetectorReadHit* > > sorted_readhits;
   for(TDetectorReadHit& readhit : detectorRead.Hits) {
     const auto detector = static_cast<Detector_t::Type_t>(readhit.DetectorType);
     sorted_readhits[detector].push_back(addressof(readhit));
   }
 
-  // apply calibration (this alters the given detectorRead!)
+  // apply calibration (this may change the given detectorRead!)
   for(const auto& calib : calibrations) {
     calib->ApplyTo(sorted_readhits);
   }
@@ -65,11 +64,32 @@ unique_ptr<TEvent> Reconstruct::DoReconstruct(TDetectorRead& detectorRead)
 
   // the detectorRead is now calibrated as far as possible
   // lets start the hit matching, which builds the TClusterHit's
-  map<Detector_t::Type_t, std::list< TClusterHit > > sorted_clusterhits;
+  // we also extract the energy, which is always defined as a
+  // single value with type Channel_t::Type_t
 
-  auto sorted_clusterhits_hint = sorted_clusterhits.begin(); // use some hinting to speed up the map filling
+  struct HitWithEnergy_t {
+    TClusterHit Hit;
+    double Energy = numeric_limits<double>::quiet_NaN();
+    void SetEnergy(const TDetectorReadHit* readhit) {
+      if(readhit->GetChannelType() != Channel_t::Type_t::Integral)
+        return;
+      if(readhit->Values.size() != 1)
+        return;
+      Energy = readhit->Values[0];
+    }
+    HitWithEnergy_t(const TDetectorReadHit* readhit,
+                    const vector<TClusterHitDatum>&& data) :
+      Hit(readhit->Channel, data)
+    {
+      SetEnergy(readhit);
+    }
+  };
+
+  map<Detector_t::Type_t, list< HitWithEnergy_t > > sorted_clusterhits;
+  auto sorted_clusterhits_hint = sorted_clusterhits.begin();
+
   for(const auto& it_hit : sorted_readhits) {
-    std::list<TClusterHit> clusterhits;
+    list<HitWithEnergy_t> clusterhits;
     const Detector_t::Type_t detector = it_hit.first;
     const auto& readhits = it_hit.second;
 
@@ -87,21 +107,22 @@ unique_ptr<TEvent> Reconstruct::DoReconstruct(TDetectorRead& detectorRead)
                 data.begin(), do_transform);
 
       // search for a TClusterHit with same channel
-      const auto match_channel = [readhit] (const TClusterHit& hit) {
-        return hit.Channel == readhit->Channel;
+      const auto match_channel = [readhit] (const HitWithEnergy_t& hit) {
+        return hit.Hit.Channel == readhit->Channel;
       };
       const auto it_clusterhit = find_if(clusterhits.begin(),
                                          clusterhits.end(),
                                          match_channel);
       if(it_clusterhit == clusterhits.end()) {
         // not found, create new TClusterHit from readhit
-        clusterhits.emplace_back(readhit->Channel, move(data));
+        if(readhit->GetChannelType() == Channel_t::Type_t::Integral)
+          clusterhits.emplace_back(readhit, move(data));
       }
       else {
         // clusterhit with channel of readhit already exists,
         // so append TClusterHitDatum's
         move(data.begin(), data.end(),
-             back_inserter(it_clusterhit->Data));
+             back_inserter(it_clusterhit->Hit.Data));
       }
     }
 
@@ -117,11 +138,59 @@ unique_ptr<TEvent> Reconstruct::DoReconstruct(TDetectorRead& detectorRead)
                                   make_pair(detector, move(clusterhits)));
   }
 
+  // already create the event here, since TaggerHits
+  // don't need tracking and thus can be filled already
+  auto event = make_shared<TEvent>(detectorRead.ID);
 
-  // now we can do clustering on the sorted_clusterhits (sorted by detector)
-  // we build some wrapper structure to hold necessary information for clustering
+  map<Detector_t::Type_t, list< TCluster > > sorted_clusters;
+  auto sorted_clusters_hint = sorted_clusters.begin();
+
+  for(const auto& it_clusterhits : sorted_clusterhits) {
+    list<TCluster> clusters;
+    const Detector_t::Type_t detectortype = it_clusterhits.first;
+    const list<HitWithEnergy_t>& clusterhits = it_clusterhits.second;
+
+    // do we have a detector instance for this type?
+    const auto& it_detector = sorted_detectors.find(detectortype);
+    if(it_detector == sorted_detectors.end())
+      continue;
+    const shared_ptr<Detector_t>& detector = it_detector->second;
+
+    // we handle stuff a bit differently for
+    // each detector from now on: tagger, cluster, everything else
+
+    // first the tagging device, which is excluded from track matching
+    if(detectortype == Detector_t::Type_t::Tagger) {
+      // one might do some double-hit decoding here...?
+      /// \todo handle the TaggerHit stuff here, maybe include PairSpec and Moeller?
+      continue;
+    }
+
+    // check if detector can do clustering,
+    const shared_ptr<ClusterDetector_t>& clusterdetector
+        = dynamic_pointer_cast<ClusterDetector_t>(detector);
+
+    if(clusterdetector != nullptr) {
+      // clustering detector, we need more than energy and position
+
+    }
+    else {
+      // in case of no clustering,
+      // build cluster for each hit
+      for(const HitWithEnergy_t& clusterhit : clusterhits) {
+
+        clusters.emplace_back(TCluster());
+      }
+    }
 
 
+
+
+    // insert the clusterhits
+    sorted_clusters_hint =
+        sorted_clusters.insert(sorted_clusters_hint,
+                               make_pair(detectortype, move(clusters)));
+  }
 
 
 
--- src/reconstruct/Reconstruct.h
+++ src/reconstruct/Reconstruct.h
@@ -27,9 +27,12 @@ private:
   template<typename T>
   using shared_ptr_list = std::list< std::shared_ptr<T> >;
 
-  shared_ptr_list<CalibrationApply_traits> calibrations;
-  shared_ptr_list<Updateable_traits>       updateables;
-  shared_ptr_list<ClusterDetector_t>       detectors_cluster;
+  using sorted_detectors_t = std::map<Detector_t::Type_t, std::shared_ptr<Detector_t> >;
+
+
+  shared_ptr_list<CalibrationApply_traits>  calibrations;
+  shared_ptr_list<Updateable_traits>        updateables;
+  sorted_detectors_t                        sorted_detectors;
 
 };
 
