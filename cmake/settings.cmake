d2858c81a64501901b132091866480522f72c7ef
Andreas Neiser <andreas.neiser@gmail.com>
2015-07-19 12:45:16 +0200

    Travis: Need GCC 4.8

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -19,7 +19,7 @@ endif()
 
 # really no optimization in debug mode
 if(CMAKE_COMPILER_IS_GNUCXX)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -Wall -Wextra -pedantic")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wall -Wextra -pedantic")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
   set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined")
 else()
