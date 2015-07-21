4c22874ce5cac8ca4b5a48b49b7ecd920cd60355
Martin Wolfes <wolfes@kph.uni-mainz.de>
2015-07-21 12:08:28 +0200

    [Calibration] added setupID to TCalibration data

--- src/tree/TCalibrationData.h
+++ src/tree/TCalibrationData.h
@@ -2,6 +2,7 @@
 #define ANT_TCALIBRATIONDATA_H
 
 #include "TDataRecord.h"
+#include <string>
 
 #define ANT_CALIBRATION_DATA_VERSION 1
 
@@ -9,59 +10,70 @@ namespace ant {
 
 struct TCalibrationEntry
 {
-  unsigned Key;
-  double   Value;
+    unsigned Key;
+    double   Value;
 
-  TCalibrationEntry() : Key(), Value() {}
-  TCalibrationEntry(unsigned key, double value) : Key(key), Value(value) {}
+    TCalibrationEntry() : Key(), Value() {}
+    TCalibrationEntry(unsigned key, double value) : Key(key), Value(value) {}
 
-  virtual ~TCalibrationEntry(){}
-  ClassDef(TCalibrationEntry, ANT_CALIBRATION_DATA_VERSION)
+    virtual ~TCalibrationEntry(){}
+    ClassDef(TCalibrationEntry, ANT_CALIBRATION_DATA_VERSION)
 };
 
 #ifndef __CINT__
 struct TCalibrationData : printable_traits
-#else
+        #else
 struct TCalibrationData
-#endif
+        #endif
 {
 
-  TCalibrationData() : FirstID(), LastID(), Data() {}
+    TCalibrationData() : SetupID(), FirstID(), LastID(), Data() {}
 
-  TCalibrationData(const TID& first_id, const TID& last_id) :
-    FirstID(first_id),
-    LastID(last_id),
-    Data()
-  {}
-
-  #ifndef __CINT__
-  TCalibrationData(const TID& first_id, const TID& last_id, const std::vector<TCalibrationEntry>& data) :
-    FirstID(first_id),
-    LastID(last_id),
-    Data(data)
-  {}
-  #endif
-
-  virtual ~TCalibrationData() {}
-
-  TID FirstID;
-  TID LastID;
-
-  std::vector<TCalibrationEntry> Data;
+    TCalibrationData( const std::string& setupID,const TID& first_id, const TID& last_id) :
+        SetupID(setupID),
+        FirstID(first_id),
+        LastID(last_id),
+        Data()
+    {}
 
 #ifndef __CINT__
-  virtual std::ostream& Print( std::ostream& s) const override {
-    s << "TCalibrationData:\n";
-    s << "  Valid for IDs:  [" << FirstID << ", " << LastID << "]" << std::endl
-      << "  Data:" << std::endl;
-    for (auto& entry: Data){
-        s << "  " << entry.Key << "  " << entry.Value << std::endl;
-    }
-    return s;
-  }
+    TCalibrationData( const std::string& setupID, const TID& first_id, const TID& last_id, const TCalibrationEntry& data) :
+        SetupID(setupID),
+        FirstID(first_id),
+        LastID(last_id),
+        Data{data}
+    {}
+    TCalibrationData( const std::string& setupID, const TID& first_id, const TID& last_id, const std::vector<TCalibrationEntry>& data) :
+        SetupID(setupID),
+        FirstID(first_id),
+        LastID(last_id),
+        Data(data)
+    {}
 #endif
 
-  ClassDef(TCalibrationData, ANT_CALIBRATION_DATA_VERSION)
+    virtual ~TCalibrationData() {}
+
+    std::string SetupID;
+
+    TID FirstID;
+    TID LastID;
+
+    std::vector<TCalibrationEntry> Data;
+
+#ifndef __CINT__
+    virtual std::ostream& Print( std::ostream& s) const override {
+        s << "TCalibrationData" << std::endl
+          << "  SetupID:        " << SetupID << std::endl
+          << "  Valid for IDs:  [" << FirstID << ", " << LastID << "]" << std::endl
+          << "  Data:" << std::endl;
+        for (auto& entry: Data){
+            s << "  " << entry.Key << "  " << entry.Value << std::endl;
+        }
+        return s;
+    }
+#endif
+
+    ClassDef(TCalibrationData, ANT_CALIBRATION_DATA_VERSION)
 
 };
 
--- test/tree/TestTCalibrationData.cc
+++ test/tree/TestTCalibrationData.cc
@@ -15,49 +15,73 @@ using namespace std;
 void dotest();
 
 TEST_CASE("TCalibrationData: Write to TTree", "[tree]") {
-  dotest();
+    dotest();
 }
 
-void dotest() {
-  tmpfile_t tmpfile;
+void dotest()
+{
+    tmpfile_t tmpfile;
 
 
-  TFile f(tmpfile.filename.c_str(),"RECREATE");
+    TFile f(tmpfile.filename.c_str(),"RECREATE");
 
-  TTree* tree = new TTree("testtree","TCalibData Test Tree");
-  ant::TCalibrationData* cdata = new ant::TCalibrationData(ant::TID(0,0,false),ant::TID(0,1,true));
-  tree->Branch("cdata",cdata);
+    TTree* tree = new TTree("testtree","TCalibData Test Tree");
+    ant::TCalibrationData* cdata = new ant::TCalibrationData(tmpfile.filename,ant::TID(0,0,false),ant::TID(0,1,true));
+    tree->Branch("cdata",cdata);
+
+    ant::TCalibrationData* cdata2 = new ant::TCalibrationData(tmpfile.filename,ant::TID(10,11,false),
+                                                              ant::TID(12,13,true),
+                                                              {1,1.});
+    tree->Branch("cdata2",cdata2);
+
+    //test 3rd constructor
+    REQUIRE_NOTHROW(ant::TCalibrationData* cdata3 = new ant::TCalibrationData(tmpfile.filename,
+                                                              ant::TID(10,11,false),
+                                                              ant::TID(12,13,true),
+                                                              {{1,1.},
+                                                               {2,2.}});
+                   );
 
 
-  cdata->Data.push_back(ant::TCalibrationEntry(1,2.1));
-  tree->Fill();
-  cdata->Data.push_back(ant::TCalibrationEntry(2,3.2));
-  tree->Fill();
+    cdata->Data.push_back(ant::TCalibrationEntry(1,2.1));
+    tree->Fill();
+    cdata->Data.push_back(ant::TCalibrationEntry(2,3.2));
+    tree->Fill();
+
+    cout << cdata << endl;
+    cout << cdata2 << endl;
 
 
-  cout << cdata << endl;
 
-  f.Write();
-  f.Close();
 
-  tree = nullptr;
 
-  TFile f2(tmpfile.filename.c_str(),"READ");
-  REQUIRE(f2.IsOpen());
 
-  f2.GetObject("testtree",tree);
+    f.Write();
+    f.Close();
 
-  REQUIRE(tree!=nullptr);
+    tree = nullptr;
 
-  ant::TCalibrationData* readcdata = nullptr;
-  tree->SetBranchAddress("cdata",&readcdata);
+    TFile f2(tmpfile.filename.c_str(),"READ");
+    REQUIRE(f2.IsOpen());
 
-  REQUIRE(tree->GetEntries() == 2);
+    f2.GetObject("testtree",tree);
 
-  tree->GetEntry(0);
-  REQUIRE(readcdata->Data.size() == 1);
+    REQUIRE(tree!=nullptr);
 
-  tree->GetEntry(1);
-  REQUIRE(readcdata->Data.size() == 2);
+    ant::TCalibrationData* readcdata = nullptr;
+    ant::TCalibrationData* readcdata2 = nullptr;
+    tree->SetBranchAddress("cdata",&readcdata);
+    tree->SetBranchAddress("cdata2",&readcdata2);
+
+    REQUIRE(tree->GetEntries() == 2);
+
+    tree->GetEntry(0);
+    REQUIRE(readcdata->Data.size() == 1);
+
+    tree->GetEntry(1);
+    REQUIRE(readcdata->Data.size() == 2);
+
+    tree->GetEntry(2);
+    REQUIRE(readcdata2->Data.back().Value == 1);
 
 }
