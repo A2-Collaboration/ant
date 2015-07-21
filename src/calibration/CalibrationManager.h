654d5bcfd44d3c45653d4e9518a23c1219395d99
Martin Wolfes <wolfes@kph.uni-mainz.de>
2015-07-21 10:33:55 +0200

    [Calibration] added CalibrationManager

---
+++ src/calibration/CalibrationManager.h
@@ -0,0 +1,17 @@
+#ifndef ANT_CALIBRATIONMANAGER
+#define ANT_CALIBRATIONMANAGER
+
+#include <string>
+#include <sstream>
+
+namespace ant {
+
+class CalibrationManager{
+public:
+    void ReadDatabase(const std::string& filename){
+    }
+};
+
+} //namespace ant
+
+#endif // ANT_CALIBRATIONMANAGER
