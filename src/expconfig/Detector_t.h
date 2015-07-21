6c34d485dc6acebe7b3a7276a40ffa32cf98a2c3
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-07-21 13:30:26 +0200

    Expconfig: Reordering/Adding available detector types

--- src/expconfig/Detector_t.h
+++ src/expconfig/Detector_t.h
@@ -13,154 +13,158 @@ namespace ant {
  * @brief The Detector_t struct is the minimal base class for all detectors
  */
 struct Detector_t {
-  enum class Type_t : std::uint8_t {
-    Trigger, Tagger, EPT, CB, PID, MWPC0, MWPC1,
-    TAPS, TAPSVeto, Cherenkov, Moeller
-  };
-  const Type_t Type;
-  static const char* ToString(const Type_t& type);
+    // changing types here breaks the file format
+    // the only operation allowed is to add detector types!
+    enum class Type_t : std::uint8_t {
+        Trigger,
+        Tagger, TaggerMicro, EPT, Moeller, PairSpec,
+        CB, PID, MWPC0, MWPC1,
+        TAPS, TAPSVeto, Cherenkov
+    };
+    const Type_t Type;
+    static const char* ToString(const Type_t& type);
 
-  // Element_t is the minimum information,
-  // derived classes may extend this
-  struct Element_t {
-    Element_t(unsigned channel, const TVector3& position) :
-      Channel(channel),
-      Position(position)
-    {}
-    unsigned Channel; // unique within Detector for all time!
-    TVector3 Position;
-  };
+    // Element_t is the minimum information,
+    // derived classes may extend this
+    struct Element_t {
+        Element_t(unsigned channel, const TVector3& position) :
+            Channel(channel),
+            Position(position)
+        {}
+        unsigned Channel; // unique within Detector for all time!
+        TVector3 Position;
+    };
 
-  virtual TVector3 GetPosition(unsigned channel) const = 0;
+    virtual TVector3 GetPosition(unsigned channel) const = 0;
 
-  virtual ~Detector_t() = default;
+    virtual ~Detector_t() = default;
 
 protected:
-  Detector_t(const Type_t& type) :
-    Type(type) {}
-  Detector_t(const Detector_t&) = delete; // disable copy
+    Detector_t(const Type_t& type) :
+        Type(type) {}
+    Detector_t(const Detector_t&) = delete; // disable copy
 };
 
 /**
  * @brief The Channel_t struct enumerates the different channel types
  */
 struct Channel_t {
-  enum class Type_t : std::uint8_t {
-    Timing,
-    Integral, IntegralShort,
-    IntegralAlternate, IntegralShortAlternate,
-    BitPattern, Scaler, Pedestal, Counter
-  };
-  static bool IsIntegral(const Type_t& t);
-  static const char* ToString(const Type_t& type);
+    enum class Type_t : std::uint8_t {
+        Timing,
+        Integral, IntegralShort,
+        IntegralAlternate, IntegralShortAlternate,
+        BitPattern, Scaler, Pedestal, Counter
+    };
+    static bool IsIntegral(const Type_t& t);
+    static const char* ToString(const Type_t& type);
 };
 
 /**
  * @brief The LogicalChannel_t struct uniquely identifies detector element within setup
  */
 struct LogicalChannel_t {
-  Detector_t::Type_t DetectorType;
-  Channel_t::Type_t ChannelType;
-  unsigned Channel;
+    Detector_t::Type_t DetectorType;
+    Channel_t::Type_t ChannelType;
+    unsigned Channel;
 };
 
 struct ClusterDetector_t : Detector_t {
-  struct Element_t : Detector_t::Element_t {
-    Element_t(
-        unsigned channel,
-        const TVector3& position,
-        const std::vector<unsigned>& neighbours,
-        double moliereRadius
-        ) :
-      Detector_t::Element_t(channel, position),
-      Neighbours(neighbours),
-      MoliereRadius(moliereRadius)
-    {}
-    std::vector<unsigned> Neighbours;
-    double MoliereRadius;
-  };
+    struct Element_t : Detector_t::Element_t {
+        Element_t(
+                unsigned channel,
+                const TVector3& position,
+                const std::vector<unsigned>& neighbours,
+                double moliereRadius
+                ) :
+            Detector_t::Element_t(channel, position),
+            Neighbours(neighbours),
+            MoliereRadius(moliereRadius)
+        {}
+        std::vector<unsigned> Neighbours;
+        double MoliereRadius;
+    };
 
-  virtual const Element_t* GetClusterElement(unsigned channel) const = 0;
+    virtual const Element_t* GetClusterElement(unsigned channel) const = 0;
 
 protected:
-  ClusterDetector_t(const Type_t& type) :
-    Detector_t(type) {}
+    ClusterDetector_t(const Type_t& type) :
+        Detector_t(type) {}
 };
 
 struct TaggerDetector_t : Detector_t {
 
-  virtual double GetPhotonEnergy(unsigned channel) const = 0;
+    virtual double GetPhotonEnergy(unsigned channel) const = 0;
 
 protected:
-  TaggerDetector_t(const Type_t& type) :
-    Detector_t(type) {}
+    TaggerDetector_t(const Type_t& type) :
+        Detector_t(type) {}
 };
 
 
 inline const char* Channel_t::ToString(const Type_t& type)
 {
-  switch(type) {
-  case Channel_t::Type_t::BitPattern:
-    return "BitPattern";
-  case Channel_t::Type_t::Counter:
-    return "Counter";
-  case Channel_t::Type_t::Integral:
-    return "Integral";
-  case Channel_t::Type_t::IntegralAlternate:
-    return "IntegralAlternate";
-  case Channel_t::Type_t::IntegralShort:
-    return "IntegralShort";
-  case Channel_t::Type_t::IntegralShortAlternate:
-    return "IntegralShortAlternate";
-  case Channel_t::Type_t::Scaler:
-    return "Scaler";
-  case Channel_t::Type_t::Timing:
-    return "Timing";
-  case Channel_t::Type_t::Pedestal:
-    return "Pedestal";
-  }
-  throw std::runtime_error("Not implemented");
+    switch(type) {
+    case Channel_t::Type_t::BitPattern:
+        return "BitPattern";
+    case Channel_t::Type_t::Counter:
+        return "Counter";
+    case Channel_t::Type_t::Integral:
+        return "Integral";
+    case Channel_t::Type_t::IntegralAlternate:
+        return "IntegralAlternate";
+    case Channel_t::Type_t::IntegralShort:
+        return "IntegralShort";
+    case Channel_t::Type_t::IntegralShortAlternate:
+        return "IntegralShortAlternate";
+    case Channel_t::Type_t::Scaler:
+        return "Scaler";
+    case Channel_t::Type_t::Timing:
+        return "Timing";
+    case Channel_t::Type_t::Pedestal:
+        return "Pedestal";
+    }
+    throw std::runtime_error("Not implemented");
 }
 
 inline const char* Detector_t::ToString(const Type_t &type)
 {
-  switch(type) {
-  case Detector_t::Type_t::CB :
-    return "CB";
-  case Detector_t::Type_t::Cherenkov:
-    return "Cherenkov";
-  case Detector_t::Type_t::EPT:
-    return "EPT";
-  case Detector_t::Type_t::Moeller:
-    return "Moeller";
-  case Detector_t::Type_t::MWPC0:
-    return "MWPC0";
-  case Detector_t::Type_t::MWPC1:
-    return "MWPC1";
-  case Detector_t::Type_t::PID:
-    return "PID";
-  case Detector_t::Type_t::Tagger:
-    return "Tagger";
-  case Detector_t::Type_t::TAPS:
-    return "TAPS";
-  case Detector_t::Type_t::TAPSVeto:
-    return "TAPSVeto";
-  case Detector_t::Type_t::Trigger:
-    return "Trigger";
-  }
-  throw std::runtime_error("Not implemented");
+    switch(type) {
+    case Detector_t::Type_t::CB :
+        return "CB";
+    case Detector_t::Type_t::Cherenkov:
+        return "Cherenkov";
+    case Detector_t::Type_t::EPT:
+        return "EPT";
+    case Detector_t::Type_t::Moeller:
+        return "Moeller";
+    case Detector_t::Type_t::MWPC0:
+        return "MWPC0";
+    case Detector_t::Type_t::MWPC1:
+        return "MWPC1";
+    case Detector_t::Type_t::PID:
+        return "PID";
+    case Detector_t::Type_t::Tagger:
+        return "Tagger";
+    case Detector_t::Type_t::TAPS:
+        return "TAPS";
+    case Detector_t::Type_t::TAPSVeto:
+        return "TAPSVeto";
+    case Detector_t::Type_t::Trigger:
+        return "Trigger";
+    }
+    throw std::runtime_error("Not implemented");
 }
 
 inline bool Channel_t::IsIntegral(const Channel_t::Type_t& t) {
-  switch(t) {
-  case Type_t::Integral:
-  case Type_t::IntegralShort:
-  case Type_t::IntegralAlternate:
-  case Type_t::IntegralShortAlternate:
-    return true;
-  default:
-    return false;
-  }
+    switch(t) {
+    case Type_t::Integral:
+    case Type_t::IntegralShort:
+    case Type_t::IntegralAlternate:
+    case Type_t::IntegralShortAlternate:
+        return true;
+    default:
+        return false;
+    }
 }
 
 } // namespace ant
