d7377ed7d6d055b3a545e3dd7ef6e3e70c4774b2
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-22 16:46:11 +0200

    Analysios: added test for AntReader

--- test/analysis/TestAntReader.cc
+++ test/analysis/TestAntReader.cc
@@ -1,27 +1,84 @@
 #include "catch.hpp"
 #include "analysis/input/ant/AntReader.h"
 #include "analysis/data/Event.h"
+#include "base/Logger.h"
+#include "base/TFileWrapper.h"
 #include <string>
 #include <iostream>
+#include "unpacker/Unpacker.h"
+#include "expconfig/ExpConfig.h"
+
+#include "tree/THeaderInfo.h"
+#include "tree/TUnpackerMessage.h"
+#include "tree/TSlowControl.h"
+#include "tree/TDetectorRead.h"
+#include "tree/TEvent.h"
+#include "TTree.h"
+#include "unpacker/RawFileReader.h"
+#include "catch_config.h"
+#include "base/tmpfile_t.h"
+
+#include "reconstruct/Reconstruct.h"
+
 
 using namespace std;
 using namespace ant;
 using namespace ant::input;
 
+void generateInputFile(const std::string& filename);
+
 TEST_CASE("AntInput", "[analysis]") {
+    SetupLogger();
+    el::Loggers::setVerboseLevel(9);
 
-//    ant::input::AntReader reader;
+    tmpfile_t tmp;
+    generateInputFile(tmp.filename);
+
+    ant::input::AntReader reader;
+
+    reader.AddInputFile(tmp.filename);
+
+    reader.Initialize();
+
+    unsigned int n = 0;
+    while(reader.hasData() && (n++ < 10)) {
+        auto event = reader.ReadNextEvent();
+        cout << *event << endl;
+    }
+
+}
 
 
-//    reader.AddInputFile();
+void generateInputFile(const string& filename) {
 
-//    g.Initialize();
+    auto unpacker = Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-big.dat.xz");
 
-//    unsigned int n = 0;
-//    while(g.hasData() && (n++ < 10)) {
-//        auto event = g.ReadNextEvent();
-//        cout << *event << endl;
-//    }
+    // write some stuff to a ROOT tree
+    TFileWrapper file(filename);
 
-//    return 0;
+    TTree* treeEvent = new TTree("treeEvent", "treeEvent");
+    TEvent* Event = new TEvent();
+    treeEvent->Branch("Event", "ant::TEvent", &Event);
+
+
+    unique_ptr<Reconstruct> reconstruct;
+
+    while(auto item = unpacker->NextItem()) {
+
+        auto HeaderInfo = dynamic_cast<THeaderInfo*>(item.get());
+        if(HeaderInfo != nullptr) {
+            reconstruct = std_ext::make_unique<Reconstruct>(*HeaderInfo);
+            continue;
+        }
+
+        auto DetectorRead = dynamic_cast<TDetectorRead*>(item.get());
+        if(DetectorRead != nullptr) {
+
+            if(reconstruct) {
+                auto event_ptr = reconstruct->DoReconstruct(*DetectorRead);
+                Event = event_ptr.get();
+                treeEvent->Fill();
+            }
+        }
+    }
 }
