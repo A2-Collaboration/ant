1c4dcbbbb5ea0f889945d42afd7f422e123cc89f
Andreas Neiser <andreas.neiser@gmail.com>
2015-07-22 21:17:43 +0200

    Travis: Use correct gcc in aftersuccess

---
+++ .travis.aftersuccess.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+CACHE=$HOME/cache
+
+export PATH=$CACHE/gcc/bin:$PATH
+export LIBRARY_PATH=$CACHE/gcc/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACHE/gcc/lib64:$LD_LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=$CACHE/gcc/include/c++/4.8.2:$CPLUS_INCLUDE_PATH
+
+pushd $PWD
+cd build && make coveralls
+popd
+bash <(curl -s https://codecov.io/bash)
