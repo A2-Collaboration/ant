39c13e1b88098b613f5640805a69823adc3445db
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-22 15:51:18 +0200

    Added Antreader test WIP

---
+++ test/analysis/TestAntReader.cc
@@ -0,0 +1,27 @@
+#include "catch.hpp"
+#include "analysis/input/ant/AntReader.h"
+#include "analysis/data/Event.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace ant;
+using namespace ant::input;
+
+TEST_CASE("AntInput", "[analysis]") {
+
+//    ant::input::AntReader reader;
+
+
+//    reader.AddInputFile();
+
+//    g.Initialize();
+
+//    unsigned int n = 0;
+//    while(g.hasData() && (n++ < 10)) {
+//        auto event = g.ReadNextEvent();
+//        cout << *event << endl;
+//    }
+
+//    return 0;
+}
