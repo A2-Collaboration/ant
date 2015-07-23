a3d3b19fac94574a71382bcbb2f73571421ab712
Martin Wolfes <wolfes@kph.uni-mainz.de>
2015-07-23 09:58:03 +0200

    [Calibration] added main functions for TCalibrationManager

--- src/calibration/CalibrationManager.h
+++ src/calibration/CalibrationManager.h
@@ -1,15 +1,63 @@
 #ifndef ANT_CALIBRATIONMANAGER
 #define ANT_CALIBRATIONMANAGER
 
+//ant
+#include "tree/TCalibrationData.h"
+#include "tree/TDataRecord.h"
+
+//ROOT
+#include "TFile.h"
+#include "TTree.h"
+
+//std
+#include <vector>
 #include <string>
 #include <sstream>
+#include <iostream>
 
-namespace ant {
+namespace ant
+{
 
-class CalibrationManager{
+
+class CalibrationManager
+{
+private:
+    TFile* dataFile;
+    TTree* dataSets;
+
 public:
-    void ReadDatabase(const std::string& filename){
+    CalibrationManager(const std::string& dataFileName)
+    {
+
+        dataFile = new TFile(dataFileName.c_str(),"UPDATE");
+        if ( !dataFile->IsOpen() )
+            throw false;
+
+        dataSets = dataFile->GetObject("dataSets", dataSets);
+        if (dataSets == nullptr)
+            dataSets = new TTree("dataSets");
+
     }
+
+    void Add(const TCalibrationData& data)
+    {
+        /// TODO: Sanity checks like eg.
+        ///  valid setupID?
+        ///  only one specific detector per beamtime
+
+    }
+
+    const TCalibrationData& GetData(const std::string& setupID, const TID& eventID) const
+    {
+        std::cout << "TODO: add data" << endl;
+        return TCalibrationData();
+    }
+
+    const std::vector<TID> GetChangePoints(const std::string& setupID) const
+    {
+        return {TID()};
+    }
+
 };
 
 } //namespace ant
