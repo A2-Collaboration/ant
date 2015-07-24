fa64a5803edf0809e2525059c296d1f9d29d1583
Andreas Neiser <andreas.neiser@gmail.com>
2015-07-24 07:03:05 +0200

    Base: inline root_printable

--- src/base/root_printable.cc
+++
@@ -1,8 +0,0 @@
-#include "root_printable.h"
-#include "TVector3.h"
-
-std::ostream &operator<<(std::ostream &stream, const TVector3 &v)
-{
-    stream << "(" << v.X() << "," << v.Y() << "," << v.Z() << ")";
-    return stream;
-}
--- src/base/root_printable.h
+++ src/base/root_printable.h
@@ -3,6 +3,9 @@
 #include <ostream>
 #include "TVector3.h"
 
-std::ostream &operator<<(std::ostream& stream, const TVector3& v);
+inline std::ostream &operator<<(std::ostream& stream, const TVector3& v) {
+    stream << "(" << v.X() << "," << v.Y() << "," << v.Z() << ")";
+    return stream;
+}
 
 
