7951c1df9e70bc5e40df5bfdfc231c3faf82965e
Martin Wolfes <wolfes@kph.uni-mainz.de>
2015-07-24 13:37:40 +0200

    [CalibrationManager] Added finish-method saving dataBase to tree

--- src/calibration/CalibrationManager.h
+++ src/calibration/CalibrationManager.h
@@ -4,11 +4,15 @@
 #include "tree/TCalibrationData.h"
 #include "tree/TDataRecord.h"
 
+//
+#include "base/Logger.h"
+
 //ROOT
 #include "TFile.h"
 #include "TTree.h"
 
 //std
+#include <memory>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -21,33 +25,68 @@ namespace ant
 class CalibrationManager
 {
 private:
+    const std::string cm_treename;
+    const std::string cm_branchname;
     std::string dataFileName;
     std::vector<TCalibrationData> dataBase;
 
+
+    void finish() const
+    {
+        std::unique_ptr<TFile> dataFile(new TFile);
+        TTree* cmTree = new TTree(cm_treename.c_str(),cm_treename.c_str());
+
+        const TCalibrationData* cdata = nullptr;
+        cmTree->Branch(cm_branchname.c_str(),&cdata);
+
+        for( auto& entry: dataBase)
+        {
+            cdata = std::addressof(entry);
+            cmTree->Fill();
+        }
+    }
+
 public:
     CalibrationManager(const std::string& DataFileName):
-    dataFileName(DataFileName)
+        cm_treename("antCalibration"),
+        cm_branchname("dataSets"),
+        dataFileName(DataFileName)
     {
-        TFile* dataFile;
-        TTree* dataSets;
 
-        dataFile = new TFile(dataFileName.c_str(),"READ");
+        std::unique_ptr<TFile> dataFile(new TFile);
+        TTree* cmTree = nullptr;
+
         if ( dataFile->IsOpen() )
         {
-            dataFile->GetObject("dataSets", dataSets);
-            if (dataSets != nullptr){
-                TCalibrationData* cdata;
-                dataSets->SetBranchAddress("dataset",&cdata);
-                for ( Long64_t entry = 0; entry < dataSets->GetEntries(); ++entry){
-                    dataSets->GetEntry(entry);
+            dataFile->GetObject(cm_treename.c_str(), cmTree);
+            if (cmTree != nullptr)
+            {
+                TCalibrationData* cdata = nullptr;
+                cmTree->SetBranchAddress(cm_branchname.c_str(),&cdata);
+
+                for (Long64_t entry = 0 ; entry < cmTree->GetEntries() ; ++entry)
+                {
+                    cmTree->GetEntry(entry);
                     Add(*cdata);
+                    VLOG(9) << "[CalibrationManager]: Adding CalibrationData "
+                            << cdata;
                 }
+
+                dataFile->Close();
+            }
+            else
+            {
+                VLOG(5) << "[CalibratioinManager:  file " << DataFileName
+                        << " doesn't contain " << cm_treename << ", new will be generated.";
             }
 
             dataFile->Close();
-            delete(dataFile);
-            delete(dataSets);
         }
+    }
+
+    ~CalibrationManager()
+    {
+        finish();
     }
 
     void Add(const TCalibrationData& data)
