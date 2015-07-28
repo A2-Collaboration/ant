8821fe3a8203fc57baf15e262d1963524a386a13
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-28 08:45:34 +0200

    Analysis: detector_t: added some tests

--- test/analysis/TestDetectorConvert.cc
+++ test/analysis/TestDetectorConvert.cc
@@ -10,6 +10,9 @@ using namespace std;
 using namespace ant;
 
 void dotest();
+void dotest_patterns();
+
+const auto CBTAPS = detector_t::CB | detector_t::TAPS;
 
 TEST_CASE("DetectorConvert", "[analysis]") {
     dotest();
@@ -20,5 +23,25 @@ void dotest() {
     REQUIRE((detector_t(Detector_t::ToBitfield(Detector_t::Type_t::CB)) & detector_t::CB));
     REQUIRE((detector_t(Detector_t::ToBitfield(Detector_t::Type_t::TAPS)) & detector_t::TAPS));
     REQUIRE_FALSE((detector_t(Detector_t::ToBitfield(Detector_t::Type_t::TAPS)) & detector_t::CB));
+
+}
+
+TEST_CASE("Patterns","[analysis]") {
+    dotest_patterns();
+}
+
+detector_t cb_taps_test(const detector_t& a, const detector_t& b) {
+    return (a & CBTAPS) ^ (b & CBTAPS);
+}
+
+void dotest_patterns()
+{
+    // req. 1 cb and 1 taps
+    REQUIRE((cb_taps_test(detector_t::CB, detector_t::TAPS) & CBTAPS));
+    REQUIRE((cb_taps_test(detector_t::TAPS, detector_t::CB) & CBTAPS));
+    REQUIRE_FALSE((cb_taps_test(detector_t::CB, detector_t::CB) & CBTAPS));
+    REQUIRE_FALSE((cb_taps_test(detector_t::TAPS, detector_t::TAPS) & CBTAPS));
+
+    REQUIRE((cb_taps_test(detector_t::CB|detector_t::PID, detector_t::TAPS) & CBTAPS));
 
 }
