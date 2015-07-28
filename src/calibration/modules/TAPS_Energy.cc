1bc9b6f9697eb1dd54e49fc0e6cadc5185738f0d
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-07-28 08:44:20 +0200

    Calibration: TAPS_Energy: Implemented 1CB-1TAPS invariant mass plot

--- src/calibration/modules/TAPS_Energy.cc
+++ src/calibration/modules/TAPS_Energy.cc
@@ -13,9 +13,9 @@ using namespace ant;
 using namespace ant::calibration;
 
 TAPS_Energy::TAPS_Energy(Calibration::Converter::ptr_t converter,
-                               const double defaultPedestal,
-                               const double defaultGain,
-                               const double defaultThreshold):
+                         const double defaultPedestal,
+                         const double defaultGain,
+                         const double defaultThreshold):
     Energy(Detector_t::Type_t::TAPS,
            converter,
            defaultPedestal,
@@ -28,36 +28,40 @@ TAPS_Energy::TAPS_Energy(Calibration::Converter::ptr_t converter,
 TAPS_Energy::ThePhysics::ThePhysics(const string& name):
     Physics(name)
 {
-    const BinSettings cb_channels(720);
+    const BinSettings cb_channels(438);
     const BinSettings energybins(1000);
 
-    ggIM = HistFac.makeTH2D("2 neutral IM (CB,CB)", "IM [MeV]", "#", energybins, cb_channels, "ggIM");
+    ggIM = HistFac.makeTH2D("2 neutral IM (TAPS,CB)", "IM [MeV]", "#", energybins, cb_channels, "ggIM");
 }
 
 void TAPS_Energy::ThePhysics::ProcessEvent(const Event& event)
 {
     const auto& cands = event.Reconstructed().Candidates();
 
+    const auto CBTAPS = detector_t::CB | detector_t::TAPS;
+
     for( auto comb = makeCombination(cands,2); !comb.Done(); ++comb ) {
         const CandidatePtr& p1 = comb.at(0);
         const CandidatePtr& p2 = comb.at(1);
 
-        if(p1->VetoEnergy()==0 && p2->VetoEnergy()==0
-           && (p1->Detector() & detector_t::CB)
-           && (p2->Detector() & detector_t::CB)) {
-            const Particle a(ParticleTypeDatabase::Photon,comb.at(0));
-            const Particle b(ParticleTypeDatabase::Photon,comb.at(1));
-            const TLorentzVector gg = a + b;
+        if(p1->VetoEnergy()==0 && p2->VetoEnergy()==0) {
 
-            auto cl1 = p1->FindCaloCluster();
-            if(cl1)
-                ggIM->Fill(gg.M(),cl1->CentralElement);
+            //require exactly 1 CB and 1 TAPS
+            const detector_t dets = (p1->Detector() & CBTAPS) ^ (p2->Detector() & CBTAPS);
 
-            auto cl2 = p2->FindCaloCluster();
-            if(cl2)
-                ggIM->Fill(gg.M(),cl2->CentralElement);
+            if(dets & CBTAPS) {
+                const Particle a(ParticleTypeDatabase::Photon,comb.at(0));
+                const Particle b(ParticleTypeDatabase::Photon,comb.at(1));
+                const TLorentzVector gg = a + b;
+
+                // Find the one that was in TAPS
+                auto cl = p1->Detector() & detector_t::TAPS ? p1->FindCaloCluster() : p2->FindCaloCluster();
+                if(cl)
+                    ggIM->Fill(gg.M(),cl->CentralElement);
+            }
         }
     }
+
 }
 
 void TAPS_Energy::ThePhysics::Finish()
