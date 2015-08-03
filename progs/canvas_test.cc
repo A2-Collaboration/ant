267dfe83f247c82a5a310f7f8965d9fde479167f
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-03 10:19:46 +0200

    canvas_test: added cmd line options to set a histogram in a root file

--- progs/canvas_test.cc
+++ progs/canvas_test.cc
@@ -7,26 +7,42 @@
 #include "calibration/gui/FitCanvas.h"
 #include "calibration/gui/FitFunction.h"
 #include "base/Logger.h"
+#include "base/CmdLine.h"
 
 #include "calibration/gui/FitGausPol3.h"
 
 #include "TRint.h"
 #include "TH1D.h"
+#include "TFile.h"
 
 using namespace std;
+using namespace ant;
 using namespace ant::calibration::gui;
 
 
 int main(int argc, char** argv) {
     SetupLogger();
+
+    TCLAP::CmdLine cmd("canvas_test", ' ', "0.1");
+
+    auto filename = cmd.add<TCLAP::ValueArg<string>>("f","file","ROOT File to open",true,"","inputfile");
+    auto histname = cmd.add<TCLAP::ValueArg<string>>("H","hist","ROOT Histogram name in file",true,"","histogram");
+
+    cmd.parse(argc, argv);
+
+//    if(!filename->isSet() || !histname->isSet())
+//        return 1;
+
     el::Loggers::setVerboseLevel(9);
 
-    TRint app("omega",&argc,argv);
+    int i=0;
+    TRint app("omega",&i,nullptr);
 
     CalCanvas* c = new CalCanvas("test");
-    TH1D* h = new TH1D("h","h",100,-10,10);
-    h->FillRandom("gaus",1000);
 
+    TFile* f = new TFile(filename->getValue().c_str(),"READ");
+    TH1D* h = nullptr;
+    f->GetObject(histname->getValue().c_str(), h);
     auto f1 = std::make_shared<FitGausPol3>();
     c->Show(h,f1);
     app.Run(kFALSE);
