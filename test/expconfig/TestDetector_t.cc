b6777e7f5985b4da39b573eb8896fbcac14a74ae
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-07 15:01:12 +0200

    Fix test for Detector_t

--- test/expconfig/TestDetector_t.cc
+++ test/expconfig/TestDetector_t.cc
@@ -22,36 +22,36 @@ Detector_t::Any_t cb_taps_test(const Detector_t::Any_t& a, const Detector_t::Any
 void dotest() {
 
     // test printable
-    cout << Detector_t::Any_t::CB() << endl;
+    cout << Detector_t::Any_t::CB << endl;
 
     // test easy usage in if-statements
-    if(Detector_t::Any_t::CB() & Detector_t::Type_t::CB) {
+    if(Detector_t::Any_t::CB & Detector_t::Type_t::CB) {
         REQUIRE(true);
     }
     else {
         REQUIRE(false);
     }
 
-    REQUIRE((Detector_t::Any_t::CB() & Detector_t::Type_t::CB));
-    REQUIRE((Detector_t::Type_t::MWPC0 & Detector_t::Any_t::CB()));
-    REQUIRE((Detector_t::Type_t::MWPC1 & Detector_t::Any_t::CB()));
-    REQUIRE((Detector_t::Type_t::PID & Detector_t::Any_t::CB()));
-    REQUIRE_FALSE((Detector_t::Type_t::TAPS & Detector_t::Any_t::CB()));
-    REQUIRE_FALSE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::CB()));
+    REQUIRE((Detector_t::Any_t::CB & Detector_t::Type_t::CB));
+    REQUIRE((Detector_t::Type_t::MWPC0 & Detector_t::Any_t::CB));
+    REQUIRE((Detector_t::Type_t::MWPC1 & Detector_t::Any_t::CB));
+    REQUIRE((Detector_t::Type_t::PID & Detector_t::Any_t::CB));
+    REQUIRE_FALSE((Detector_t::Type_t::TAPS & Detector_t::Any_t::CB));
+    REQUIRE_FALSE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::CB));
 
-    REQUIRE((Detector_t::Type_t::TAPS & Detector_t::Any_t::TAPS()));
-    REQUIRE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::TAPS()));
+    REQUIRE((Detector_t::Type_t::TAPS & Detector_t::Any_t::TAPS));
+    REQUIRE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::TAPS));
 
-    REQUIRE((Detector_t::Type_t::PID & Detector_t::Any_t::Veto()));
-    REQUIRE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::Veto()));
+    REQUIRE((Detector_t::Type_t::PID & Detector_t::Any_t::Veto));
+    REQUIRE((Detector_t::Type_t::TAPSVeto & Detector_t::Any_t::Veto));
 
-    REQUIRE(Detector_t::Any_t::CB() == Detector_t::Any_t::CB());
-    REQUIRE(Detector_t::Any_t::CB() != Detector_t::Any_t::TAPS());
+    REQUIRE(Detector_t::Any_t::CB == Detector_t::Any_t::CB);
+    REQUIRE(Detector_t::Any_t::CB != Detector_t::Any_t::TAPS);
 
-    Detector_t::Any_t detector1 = Detector_t::Any_t::None();
+    Detector_t::Any_t detector1 = Detector_t::Any_t::None;
     detector1 |= Detector_t::Type_t::CB;
 
-    REQUIRE((detector1 & Detector_t::Any_t::CB()));
+    REQUIRE((detector1 & Detector_t::Any_t::CB));
     REQUIRE(detector1 == Detector_t::Type_t::CB);
     REQUIRE(detector1 != Detector_t::Type_t::TAPS);
     REQUIRE(detector1 != Detector_t::Type_t::MWPC0);
