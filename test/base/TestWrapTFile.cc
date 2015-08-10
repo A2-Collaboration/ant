1e4e372e4ba61f825e3cac6ad06ff57e6b954b77
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-10 17:23:56 +0200

    started Test for WrapTFile (WIP)

---
+++ test/base/TestWrapTFile.cc
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+#include "catch_config.h"
+
+#include "base/WrapTFile.h"
+#include "base/tmpfile_t.h"
+
+#include <iostream>
+
+using namespace std;
+
+void writeFile(const std::string& filename);
+void readFile(const std::string& filename);
+
+
+TEST_CASE("Write WrapTfile", "[base]") {
+
+}
+
+TEST_CASE("Read WrapTfile", "[base]") {
+
+}
+
+void writeFile(const std::string& filename) {
+
+}
+
+void readFile(const std::string& filename) {
+
+}
