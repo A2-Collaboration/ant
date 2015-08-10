c53085f411cda7d6c7b6146cb9d1020f8e2c58f2
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-10 14:47:14 +0200

    CMake: added note for -Wsuggest-override.
    Add this to flags for gcc >5.1

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -19,9 +19,12 @@ endif()
 
 # really no optimization in debug mode
 if(CMAKE_COMPILER_IS_GNUCXX)
+
+# for GCC >5.1 add -Wsuggest-override
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wall -Wextra -pedantic")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
   set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined")
+
 else()
   message(FATAL_ERROR "Non-gnu compiler not supported at the moment")
 endif()
