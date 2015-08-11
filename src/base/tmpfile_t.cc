fd6b419e9342b64c39146d002fcd02b4f8e1eaf7
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-11 19:02:24 +0200

    base: tempfile_t: added support for file extensions

--- src/base/tmpfile_t.cc
+++ src/base/tmpfile_t.cc
@@ -6,32 +6,36 @@
 #include <stdexcept>
 #include <iterator>
 #include <fstream>
+#include <cassert>
 
 using namespace std;
 using namespace ant;
 
-tmpfile_t::tmpfile_t() : filename(), testdata()
+tmpfile_t::tmpfile_t(const string& extension) : filename(), testdata()
 {
-  // obtain some random filename
-  char filename_[128];
-  strcpy(filename_, "anttmpfile.XXXXXX");
-  if(mkstemp(filename_) == -1)
-    throw runtime_error("Cannot create tmpfile "+filename);
-  filename = filename_;
+    string templ = "anttmpfile.XXXXXX" + extension;
+    assert(templ.size() <128);
+
+    // obtain some random filename
+    char filename_[128];
+    strcpy(filename_, templ.c_str());
+    if(mkstemps(filename_,extension.size()) == -1)
+        throw runtime_error("Cannot create tmpfile "+filename);
+    filename = filename_;
 }
 
 
 void tmpfile_t::write_testdata() {
-  ofstream outfile(filename);
-  ostream_iterator<uint8_t> outiterator(outfile);
-  copy(testdata.begin(), testdata.end(), outiterator);
-  outfile.close();
-  if(!outfile)
-    throw runtime_error("Cannot write data to "+filename);
+    ofstream outfile(filename);
+    ostream_iterator<uint8_t> outiterator(outfile);
+    copy(testdata.begin(), testdata.end(), outiterator);
+    outfile.close();
+    if(!outfile)
+        throw runtime_error("Cannot write data to "+filename);
 }
 
 
 tmpfile_t::~tmpfile_t() {
-  // do not really
-  remove(filename.c_str());
+    // do not really
+    remove(filename.c_str());
 }
