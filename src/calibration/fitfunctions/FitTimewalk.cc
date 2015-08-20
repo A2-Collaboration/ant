b395c2599f1beda34e0585ec1ce957e294c8050d
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-08-20 11:42:09 +0200

    Calibration: Add FitTimewalk (WIP)

---
+++ src/calibration/fitfunctions/FitTimewalk.cc
@@ -0,0 +1,92 @@
+#include "FitTimewalk.h"
+
+#include "base/interval.h"
+#include "base/Logger.h"
+#include "TF1Knobs.h"
+#include "BaseFunctions.h"
+
+#include "TF1.h"
+#include "TH1.h"
+
+#include <algorithm>
+
+using namespace ant;
+using namespace ant::calibration;
+using namespace ant::calibration::gui;
+
+FitTimewalk::FitTimewalk()
+{
+    func = functions::timewalk::getTF1();
+    func->SetNpx(1000);
+    AddKnob<KnobsTF1::RangeKnob>("Min", func, KnobsTF1::RangeKnob::RangeEndType::lower);
+    AddKnob<KnobsTF1::RangeKnob>("Max", func, KnobsTF1::RangeKnob::RangeEndType::upper);
+}
+
+FitTimewalk::~FitTimewalk()
+{
+}
+
+void FitTimewalk::Draw()
+{
+    func->Draw("same");
+}
+
+void FitTimewalk::Fit(TH1 *hist)
+{
+    hist->Fit(func,"RBQN");
+}
+
+void FitTimewalk::SetDefaults(TH1 *hist)
+{
+    if(hist) {
+        func->SetParameter(0,hist->GetMaximum());
+    } else {
+        func->SetParameter(0,100);
+    }
+    double max_pos = hist->GetXaxis()->GetBinCenter(hist->GetMaximumBin());
+    func->SetParameter(1,max_pos);
+    func->SetParameter(2,20);
+    SetRange({max_pos-60, max_pos+60});
+}
+
+void FitTimewalk::SetRange(ant::interval<double> i)
+{
+    setRange(func, i);
+}
+
+ant::interval<double> FitTimewalk::GetRange() const
+{
+    return getRange(func);
+}
+
+void FitTimewalk::SetPoints(int n)
+{
+    func->SetNpx(n);
+}
+
+FitFunction::SavedState_t FitTimewalk::Save() const
+{
+    std::vector<double> params;
+
+    saveTF1(func,params);
+
+    return params;
+}
+
+void FitTimewalk::Load(const SavedState_t &data)
+{
+    if(data.size() != std::size_t(2+func->GetNpar())) {
+        LOG(WARNING) << "Can't load parameters";
+        return;
+    }
+    SavedState_t::const_iterator pos = data.begin();
+    loadTF1(pos, func);
+
+    sync();
+}
+
+double FitTimewalk::GetPeakPosition() const
+{
+    return func->GetParameter(1);
+}
+
---
+++ src/calibration/fitfunctions/FitTimewalk.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "FitFunction.h"
+
+#include "base/interval.h"
+#include "calibration/gui/GUIbase.h"
+
+#include <memory>
+
+#include "TF1Knobs.h"
+
+#include <list>
+#include <string>
+#include "base/std_ext.h"
+#include "base/interval.h"
+
+#include "Rtypes.h"
+
+class TH1;
+class TF1;
+
+namespace ant {
+namespace calibration {
+namespace gui {
+
+class FitTimewalk: public FitFunction {
+protected:
+    TF1* func = nullptr;
+
+public:
+    FitTimewalk();
+
+    virtual ~FitTimewalk();
+
+    virtual void Draw() override;
+
+    virtual void Fit(TH1* hist) override;
+    virtual void SetDefaults(TH1* hist) override;
+
+    virtual void SetRange(ant::interval<double> i) override;
+    virtual ant::interval<double> GetRange() const override;
+    virtual void SetPoints(int n) override;
+
+    virtual SavedState_t Save() const override;
+    virtual void Load(const SavedState_t &data) override;
+
+    virtual double GetPeakPosition() const override;
+};
+
+}
+}
+}
+
