c840fcea3ad046acd2130a8ebdfe844ef52fa9c6
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-08-28 18:50:10 +0200

    Test: Add unpacker scaler block

---
+++ test/unpacker/TestUnpackerAcquScaler.cc
@@ -0,0 +1,52 @@
+#include "catch.hpp"
+#include "catch_config.h"
+
+#include "Unpacker.h"
+
+#include "tree/THeaderInfo.h"
+#include "tree/TDetectorRead.h"
+#include "tree/TSlowControl.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace ant;
+
+void dotest();
+
+TEST_CASE("Test UnpackerAcqu: Scaler block", "[unpacker]") {
+    dotest();
+}
+
+void dotest() {
+    auto unpacker = ant::Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Acqu_scalerblock.dat.xz");
+
+    unsigned nSlowControls = 0;
+    unsigned nReads = 0;
+    unsigned nHits = 0;
+
+    bool taggerScalerBlockFound = false;
+
+    while(auto item = unpacker->NextItem()) {
+        auto DetectorRead = dynamic_cast<TDetectorRead*>(item.get());
+        if(DetectorRead != nullptr) {
+            nReads++;
+            nHits += DetectorRead->Hits.size();
+        }
+        auto SlowControl = dynamic_cast<TSlowControl*>(item.get());
+        if(SlowControl != nullptr) {
+            nSlowControls++;
+            if(SlowControl->Name == "TaggerScalers") {
+                taggerScalerBlockFound = true;
+                // we know the file is extracted from an EPT run...
+                REQUIRE(SlowControl->Payload_Int.size() == 47);
+            }
+        }
+    }
+
+    REQUIRE(nSlowControls == 15);
+    REQUIRE(nReads == 211);
+    REQUIRE(nHits == 27904);
+    REQUIRE(taggerScalerBlockFound);
+}
