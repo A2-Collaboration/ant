18ef468fc93f336600f315c1f64e7f4e26600f7d
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-08-28 13:20:00 +0200

    CI: Just revert, did not show branches on codecov.io

--- ci/upload-coverage.sh
+++ ci/upload-coverage.sh
@@ -9,4 +9,4 @@ export LIBRARY_PATH=$CACHE/gcc/lib64:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$CACHE/gcc/lib64:$LD_LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=$CACHE/gcc/include/c++/4.8.2:$CPLUS_INCLUDE_PATH
 
-bash <(curl -s https://codecov.io/bash) -a "-b"
+bash <(curl -s https://codecov.io/bash)
