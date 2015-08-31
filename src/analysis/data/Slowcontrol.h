51b20372cfb0a5342179d673e585bafd7ef746fc
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-31 12:41:06 +0200

    Analysis: data/Slowcontrol: initialize double and int64 slc-vars with nan or lowest()

--- src/analysis/data/Slowcontrol.h
+++ src/analysis/data/Slowcontrol.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <cstdint>
 
 namespace ant {
 namespace analysis {
@@ -18,10 +19,14 @@ class ReqestableVariable: public SlowcontrolRequestable {
 protected:
     T data;
 public:
+    ReqestableVariable(): data({}) {}
 
     const T& operator()() const { return data; }
           T& operator()() { return data; }
 };
+
+template <> ReqestableVariable<double>::ReqestableVariable();
+template <> ReqestableVariable<std::int64_t>::ReqestableVariable();
 
 
 class Slowcontrol {
