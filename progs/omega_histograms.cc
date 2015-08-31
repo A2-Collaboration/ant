bc4c876c88472a525dc6eb8c2b20d0e110c2289a
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-08-31 17:08:54 +0200

    Added omega_histograms.cc prog. Build with -DOmegaTools (default=off)

---
+++ progs/omega_histograms.cc
@@ -0,0 +1,101 @@
+#include "base/WrapTFile.h"
+#include "base/Logger.h"
+
+#include "TRint.h"
+#include "analysis/plot/root_draw.h"
+#include <string>
+#include <list>
+#include "TH1D.h"
+#include "base/CmdLine.h"
+#include "TDirectory.h"
+#include "base/iterators.h"
+
+using namespace ant;
+using namespace std;
+
+
+template<class T, typename Func>
+std::list<T*> GetListOf(TDirectory* dir, Func func=[] (const string& name){return true;})
+{
+    std::list<T*> theList;
+
+    TList* keys = dir->GetListOfKeys();
+
+    if(!keys)
+        return {};
+
+    T* objectPtr = nullptr;
+    TKey* key = nullptr;
+    TIter nextk(keys);
+
+    while((key = (TKey*)nextk()))
+    {
+        const auto& name = key->GetName();
+        if(func(name)) {
+            objectPtr = dynamic_cast<T*>(key->ReadObj());
+            if ( !objectPtr )
+                continue;
+            theList.push_back(objectPtr);
+        }
+    }
+    return theList;
+}
+
+
+inline bool ends_with(std::string const & value, std::string const & ending)
+{
+    if (ending.size() > value.size()) return false;
+    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
+int main(int argc, char** argv) {
+    SetupLogger();
+
+    TCLAP::CmdLine cmd("Ant", ' ', "0.1");
+
+    auto cmd_files = cmd.add<TCLAP::MultiArg<string>>("i","input","Input files",true,"filename");
+
+    cmd.parse(argc, argv);
+
+
+    int fake_argc=0;
+    char** fake_argv=nullptr;
+    TRint app("Ant",&fake_argc,fake_argv,nullptr,0,true);
+
+
+    WrapTFileInput infiles;
+
+    for(const auto& f : cmd_files->getValue()) {
+        infiles.OpenFile(f);
+    }
+
+    TDirectory* dir = nullptr;
+
+    infiles.GetObject("OmegaEtaG",dir);
+
+    auto hists = GetListOf<TH1D>(dir, [] (const string& name) { return ends_with(name, "__gg");});
+
+    hists.sort([] (const TH1* a, const TH1* b) { return a->GetEntries() > b->GetEntries();});
+
+    hstack gg_stack("2#gamma IM");
+
+    const std::vector<Color_t> cols = {kRed, kGreen, kBlue, kYellow, kMagenta, kCyan, kOrange, kPink+9, kSpring+10, kGray};
+    auto cit = getCirculatIterator(cols.begin(), cols.end());
+
+    int i=0;
+    for(auto& h : hists) {
+
+        h->SetFillColor(*(cit++));
+
+        gg_stack << h;
+        if(i++>9)
+            break;
+    }
+
+    canvas("gg im") << gg_stack << endc;
+
+    app.Run();
+
+
+    return 0;
+}
