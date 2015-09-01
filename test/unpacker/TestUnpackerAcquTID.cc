6998a36b8ea3146edd037e47f6c313c7f01fb924
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-09-01 15:20:01 +0200

    Test: extend unpacker TID

--- test/unpacker/TestUnpackerAcquTID.cc
+++ test/unpacker/TestUnpackerAcquTID.cc
@@ -32,14 +32,26 @@ THeaderInfo GetHeaderInfo(const string& filename) {
 }
 
 void dotest() {
+    THeaderInfo earlier = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-big.dat.xz");
 
     THeaderInfo info_1 = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-METMEST_1.dat.xz");
     THeaderInfo info_2 = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-METMEST_2.dat.xz");
     THeaderInfo info_3 = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-METMEST_3.dat.xz");
     THeaderInfo info_4 = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-METMEST_4.dat.xz");
 
+    THeaderInfo later = GetHeaderInfo(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-small.dat.xz");
+
+    cout << earlier << endl;
+    cout << info_1 << endl;
+    cout << info_2 << endl;
+    cout << info_3 << endl;
+    cout << info_4 << endl;
+    cout << later << endl;
+
+    REQUIRE(earlier.ID < info_1.ID);
     REQUIRE(info_1.ID < info_2.ID);
     REQUIRE(info_2.ID < info_3.ID);
     REQUIRE(info_3.ID < info_4.ID);
+    REQUIRE(info_4.ID < later.ID);
 
 }
