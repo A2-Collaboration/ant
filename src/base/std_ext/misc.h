e6ee5a2e65066e5243878482f1f05109947e6397
Andreas Neiser <andreas.neiser@gmail.com>
2015-09-01 19:28:26 +0200

    Base: Add std_ext::begins_with, works with any container

--- src/base/std_ext/misc.h
+++ src/base/std_ext/misc.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <algorithm>
 #include <cxxabi.h>
+
 
 namespace ant {
 namespace std_ext {
+
+using namespace std;
+
+
+template<class TContainer>
+bool begins_with(const TContainer& input, const TContainer& match)
+{
+    return input.size() >= match.size()
+        && equal(match.begin(), match.end(), input.begin());
+}
 
 template<typename T>
 std::string getTypeAsString() {
