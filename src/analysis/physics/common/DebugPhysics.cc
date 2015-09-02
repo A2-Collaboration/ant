ead007eaf662eceb9a9d60277f193cada4a72b9b
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-09-02 22:33:47 +0200

    Analysis: Added PID Debug Class

--- src/analysis/physics/common/DebugPhysics.cc
+++ src/analysis/physics/common/DebugPhysics.cc
@@ -1,6 +1,7 @@
 #include "DebugPhysics.h"
 
 #include "base/Logger.h"
+#include "expconfig/ExpConfig.h"
 
 using namespace std;
 using namespace ant;
@@ -31,4 +32,42 @@ void DebugPhysics::Initialize(data::Slowcontrol& slowcontrol)
     slowcontrol.FaradayCup.Request();
 }
 
+
+
+DebugPIDAlignment::DebugPIDAlignment(PhysOptPtr opts):
+    Physics("DebugPIDAlignment", opts)
+{
+    const BinSettings bins(360,-180,180);
+    angles = HistFac.makeTH2D("PID Phi angles","MCTrue #phi","Rec #phi",bins,bins);
+}
+
+DebugPIDAlignment::~DebugPIDAlignment()
+{
+
+}
+
+void DebugPIDAlignment::ProcessEvent(const data::Event& event)
+{
+    if(event.MCTrue().Particles().GetAll().size() == 1) {
+        const auto mctrue_phi = event.MCTrue().Particles().GetAll().front()->Phi() * TMath::RadToDeg();
+
+        for(const data::CandidatePtr& c : event.Reconstructed().Candidates()) {
+            angles->Fill(mctrue_phi, c->Phi()* TMath::RadToDeg());
+        }
+
+        for(const data::Cluster& c : event.Reconstructed().InsaneClusters()) {
+            if(c.Detector == Detector_t::Type_t::PID) {
+                angles->Fill(mctrue_phi, c.pos.Phi()* TMath::RadToDeg());
+            }
+        }
+    }
+}
+
+void DebugPIDAlignment::ShowResult()
+{
+    canvas("PID angles") << drawoption("colz") << angles << endc;
+
+}
+
 AUTO_REGISTER_PHYSICS(DebugPhysics, "DebugPhysics")
+AUTO_REGISTER_PHYSICS(DebugPIDAlignment, "DebugPIDAlignment")
--- src/analysis/physics/common/DebugPhysics.h
+++ src/analysis/physics/common/DebugPhysics.h
@@ -18,4 +18,15 @@ public:
     virtual void Initialize(data::Slowcontrol &slowcontrol) override;
 };
 
+class DebugPIDAlignment: public Physics {
+protected:
+    TH2D* angles = nullptr;
+public:
+    DebugPIDAlignment(PhysOptPtr opts=nullptr);
+    virtual ~DebugPIDAlignment();
+
+    virtual void ProcessEvent(const data::Event& event) override;
+    virtual void ShowResult() override;
+};
+
 }}} // namespace ant::analysis::physics
