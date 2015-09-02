d306368e13aebcb1ae04bf63f88ddd7a5ce1a469
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-09-02 14:06:26 +0200

    Calibration: Improve documentation

--- src/calibration/DataManager.h
+++ src/calibration/DataManager.h
@@ -19,17 +19,26 @@ namespace calibration
 class DataAccess
 {
 public:
-   /**
+    /**
+     * @brief Add the given calibration data to the database
+     * @param cdata
+     */
+    virtual void Add(const TCalibrationData& cdata) = 0;
+
+    /**
     *  \brief GetData Query the calibration database for specific TID
     *  \param calibrationID Calibration ID
     *  \param eventID event ID
     *  \param cdata   Reference to a TCalibrationData, data will be writter here
     *  \return true if valid data was found
     */
-    virtual void Add(const TCalibrationData& cdata) = 0;
-
     virtual bool GetData(const std::string& calibrationID, const TID& eventID, TCalibrationData& cdata) = 0;
 
+    /**
+     * @brief GetChangePoints obtains the IDs where the data should be changed
+     * @param calibrationID the calibration identifier
+     * @return list of change points (possibly unsorted)
+     */
     virtual const std::list<TID> GetChangePoints(const std::string& calibrationID) = 0;
 };
 
