dc55e7303025a294210b4efb9413a7ddb600e9c6
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-09-04 15:21:47 +0200

    base: added emumfield template and test
    A bitfield based on enum class

---
+++ src/base/enumfield.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace ant {
+
+template <typename EnumClass, typename FieldType=unsigned>
+struct enumfield {
+private:
+    FieldType field = 0;
+
+    constexpr enumfield(const FieldType f) noexcept: field(f) {}
+public:
+
+    using EnumType = typename std::underlying_type<EnumClass>::type;
+
+    constexpr enumfield(EnumClass e) noexcept: field(bit(e)) {}
+    constexpr enumfield() noexcept {}
+
+    enumfield(const enumfield&) = default;
+    enumfield& operator=(const enumfield&) = default;
+
+    enumfield(enumfield&&) = default;
+    enumfield& operator=(enumfield&&) = default;
+
+    ~enumfield() = default;
+
+    constexpr bool isChecked(const EnumClass e) const noexcept {
+        return field & bit(e);
+    }
+
+    static constexpr FieldType bit(const EnumClass e) noexcept {
+        return 1 << static_cast<EnumType>(e);
+    }
+
+    void Set(const EnumClass e) noexcept {
+        field |= bit(e);
+    }
+
+    void Reset(const EnumClass e) noexcept {
+        field &= ~bit(e);
+    }
+
+    enumfield& operator&= (const enumfield e) {
+        field &= e.field;
+        return *this;
+    }
+
+    enumfield& operator|= (const enumfield e) {
+        field |= e.field;
+        return *this;
+    }
+
+    enumfield& operator^= (const enumfield e) {
+        field ^= e.field;
+        return *this;
+    }
+
+    constexpr bool operator== (const EnumClass e) const noexcept {
+        return field == bit(e);
+    }
+
+    constexpr bool operator== (const enumfield& rhs) const noexcept {
+        return field == rhs.field;
+    }
+
+    constexpr enumfield operator&(const enumfield& r) const noexcept {
+        return enumfield(field & r.field);
+    }
+
+    constexpr enumfield operator|(const enumfield& r) const noexcept {
+        return enumfield(field | r.field);
+    }
+
+    constexpr enumfield operator^(const enumfield& r) const noexcept {
+        return enumfield(field ^ r.field);
+    }
+
+};
+
+}
---
+++ test/base/TestEnumfield.cc
@@ -0,0 +1,57 @@
+#include "catch.hpp"
+#include "catch_config.h"
+
+#include "base/enumfield.h"
+
+using namespace std;
+using namespace ant;
+
+void dotest();
+
+TEST_CASE("Enumfield", "[base]") {
+    dotest();
+}
+
+enum class MyEnum {
+    F1,F2
+};
+
+void dotest() {
+    enumfield<MyEnum> field;
+
+    REQUIRE_FALSE(field.isChecked(MyEnum::F1));
+    REQUIRE_FALSE(field.isChecked(MyEnum::F2));
+
+    REQUIRE_NOTHROW(field.Set(MyEnum::F2));
+    REQUIRE(field.isChecked(MyEnum::F2));
+    REQUIRE_FALSE(field.isChecked(MyEnum::F1));
+
+    REQUIRE_NOTHROW(field.Reset(MyEnum::F2));
+    REQUIRE_FALSE(field.isChecked(MyEnum::F2));
+    REQUIRE_FALSE(field.isChecked(MyEnum::F1));
+
+    REQUIRE_NOTHROW(field = MyEnum::F1);
+    REQUIRE(field.isChecked(MyEnum::F1));
+    REQUIRE(field == MyEnum::F1);
+    const auto field2 = field;
+
+    REQUIRE(field2==field);
+
+    constexpr enumfield<MyEnum> cf1(MyEnum::F1);
+    constexpr enumfield<MyEnum> cf2(MyEnum::F2);
+
+    REQUIRE(cf1.isChecked(MyEnum::F1));
+
+    constexpr auto cf3 = cf1 | cf2;
+    REQUIRE(cf3.isChecked(MyEnum::F1));
+    REQUIRE(cf3.isChecked(MyEnum::F2));
+
+    static_assert(cf3.isChecked(MyEnum::F1),"constexpr isChecked");
+
+    static_assert(std::is_default_constructible<enumfield<MyEnum>>::value,"");
+    static_assert(std::is_move_assignable<enumfield<MyEnum>>::value,"");
+    static_assert(std::is_move_constructible<enumfield<MyEnum>>::value,"");
+
+
+
+}
