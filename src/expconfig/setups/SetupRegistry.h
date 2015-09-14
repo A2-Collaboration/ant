67d080dccbd137f575b85e314e1353b1135d9822
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-09-14 16:37:14 +0200

    Expconfig: Cleanup SetupRegistry

--- src/expconfig/setups/SetupRegistry.h
+++ src/expconfig/setups/SetupRegistry.h
@@ -9,42 +9,41 @@ namespace expconfig {
 
 class Setup;
 
-// stuff for semiauto-registering the setups
-// don't forget to use AUTO_REGISTER_SETUP
-// note that linking order is important to get this registry
-// properly working, see CMakeLists.txt
-
-template<class T>
-std::shared_ptr<Setup> setup_factory()
-{
-    return std::move(std::make_shared<T>());
-}
-
-using setup_creator = std::function<std::shared_ptr<Setup>()>;
-
+/**
+ * @brief The SetupRegistry class semi-automatically registers Setups
+ *
+ * \note don't forget to use AUTO_REGISTER_SETUP
+ * \note linking order is important to get this registry properly working, see CMakeLists.txt
+ */
 class SetupRegistry
 {
+friend class SetupRegistration;
+
 private:
-    using setup_creators_t = std::list<setup_creator>;
+    using Creator = std::function<std::shared_ptr<Setup>()>;
+    using setup_creators_t = std::list<Creator>;
     using setups_t = std::list< std::shared_ptr<Setup> >;
     setup_creators_t setup_creators;
     setups_t setups;
     void init_setups();
 public:
     static SetupRegistry& get();
-    void add(setup_creator);
+    void add(Creator);
     setups_t::iterator begin();
     setups_t::iterator end();
     void destroy();
 };
 
+/**
+ * @brief The SetupRegistration class is instantiated for each setup
+ */
 class SetupRegistration
 {
 public:
-    SetupRegistration(setup_creator);
+    SetupRegistration(SetupRegistry::Creator);
 };
 
 #define AUTO_REGISTER_SETUP(setup) \
-    SetupRegistration _setup_registration_ ## setup(setup_factory<setup>);
+    SetupRegistration _setup_registration_ ## setup(std::make_shared<setup>);
 
 }} // namespace ant::expconfig
