6f47a0dedca7cba8582a9808a6bdfecd60fa3603
Andreas Neiser <andreas.neiser@gmail.com>
2015-09-16 17:37:52 +0200

    Test: Merge WrapTFile tests

--- test/base/TestWrapTFile.cc
+++ test/base/TestWrapTFile.cc
@@ -2,51 +2,81 @@
 #include "catch_config.h"
 
 #include "base/WrapTFile.h"
+#include "base/tmpfile_t.h"
 #include "base/std_ext/memory.h"
 
-#include <cstdio>
-#include <memory>
+#include "tree/THeaderInfo.h"
 
-#include "TCutG.h"
+#include "unpacker/Unpacker.h"
 
+#include "TH1D.h"
+#include "TTree.h"
 
 using namespace std;
 using namespace ant;
 
-void writeFile(const std::string& filename);
-void readFile(const std::string& filename);
+void dotest_rw();
+void dotest_r();
+void generateInputFile(const std::string& filename);
 
-TEST_CASE("Write WrapTfile", "[base]") {
-    writeFile("anttmp.root");
+
+TEST_CASE("WrapTFileInput", "[base]") {
+    dotest_r();
 }
 
-TEST_CASE("Read WrapTfile", "[base]") {
-    readFile("anttmp.root");
-    remove("anttmp.root");
-    ///@todo Need a tmpfile_t that creates file names that end in .root
+TEST_CASE("WrapTFileOutput", "[base]") {
+    dotest_rw();
 }
 
-void writeFile(const std::string& filename) {
+void dotest_r() {
+    ant::tmpfile_t tmp;
+    ant::WrapTFileInput m;
 
-    TCutG* cut = new TCutG("test",3);
-    cut->SetPoint(0,1,1);
-    cut->SetPoint(1,2,1);
-    cut->SetPoint(2,3,1);
-    cut->SaveAs(filename.c_str()); // filename must end in .root
+    generateInputFile(tmp.filename);
+
+    m.OpenFile(tmp.filename);
+
+    TTree* tree = nullptr;
+
+    REQUIRE( m.GetObject("treeHeaderInfo", tree));
+    REQUIRE(tree != nullptr);
+    REQUIRE(tree->GetEntries() == 1);
 }
 
-/**
- * @brief readFile test: shows that we can read objects from a root file and close the file, object still valid
- * @param filename File to open
- */
-void readFile(const std::string& filename) {
-    std::unique_ptr<WrapTFileInput> file = ant::std_ext::make_unique<ant::WrapTFileInput>();
-    std::shared_ptr<TCutG> cut;
 
-    REQUIRE_NOTHROW( file->OpenFile(filename));
-    REQUIRE_NOTHROW( cut = file->GetSharedClone<TCutG>("test"));
-    REQUIRE( (cut != nullptr) );
-    REQUIRE_NOTHROW(file=nullptr);
-    REQUIRE(cut->GetN() == 3);
+
+
+void dotest_rw() {
+    tmpfile_t tmp;
+
+    auto outfile = std_ext::make_unique<WrapTFileOutput>(tmp.filename);
+    auto h2 = outfile->CreateInside<TH1D>("b","B",10,0,10);
+    auto h3 = outfile->CreateInside<TH1D>("c","C",10,0,10);
+    h2->Fill(3);
+    h3->Fill(2);
+    outfile = nullptr;
+
+    WrapTFileInput infile(tmp.filename);
+    REQUIRE(infile.GetListOf<TH1D>().size() == 2);
+}
+
+void generateInputFile(const string& filename) {
+
+    auto unpacker = ant::Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-small.dat.xz");
+
+    // write some stuff to a ROOT tree
+    ant::WrapTFileOutput file(filename);
+    TTree* treeHeaderInfo = file.CreateInside<TTree>("treeHeaderInfo", "treeHeaderInfo");
+    ant::THeaderInfo* HeaderInfo = file.CreateInside<ant::THeaderInfo>();
+
+    treeHeaderInfo->Branch("THeaderInfo", "ant::THeaderInfo", &HeaderInfo);
+
+    while(auto item = unpacker->NextItem()) {
+        HeaderInfo = dynamic_cast<ant::THeaderInfo*>(item.get());
+        if(HeaderInfo != nullptr) {
+            treeHeaderInfo->Fill();
+            break;
+        }
+    }
 }
 
