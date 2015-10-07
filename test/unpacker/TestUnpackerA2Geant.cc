5e305d2be87c5d8c2033cf652ca0aededf607fc2
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-10-07 16:14:34 +0200

    Test: Add UnpackerA2Geant open

---
+++ test/unpacker/TestUnpackerA2Geant.cc
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+#include "catch_config.h"
+
+#include "Unpacker.h"
+#include "expconfig/ExpConfig.h"
+
+#include <string>
+
+using namespace std;
+
+TEST_CASE("Test UnpackerA2Geant", "[unpacker]") {
+    // try to open the file, but we need some setup name for this...
+    ant::ExpConfig::ManualSetupName = "Setup_2014-07_EPT_Prod";
+    REQUIRE_NOTHROW(ant::Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Geant_with_TID.root"));
+}
+
