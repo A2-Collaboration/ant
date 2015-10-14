1c2cfd5c1d17470ed5f347e0ac237a8226775684
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-10-14 15:31:36 +0200

    Progs: Add usage to Ant-addTID

--- progs/Ant-addTID.cc
+++ progs/Ant-addTID.cc
@@ -1,6 +1,10 @@
 #include "simulation/mc/utils/PlutoTID.h"
 #include "base/Logger.h"
 
+#include <iostream>
+
+using namespace std;
+
 int main (int argc, char** argv)
 {
     SetupLogger();
@@ -9,6 +13,9 @@ int main (int argc, char** argv)
         ant::simulation::mc::utils::PlutoTID::AddTID(std::string(argv[1]));
         return EXIT_SUCCESS ;
     }
+
+    cout << "Add TID Tree to given pluto output file. Will be passed through a2geant (if patched version is used)." << endl;
+    cout << "Usage: Ant-addTID <filename>" << endl;
 
     return EXIT_FAILURE;
 }
