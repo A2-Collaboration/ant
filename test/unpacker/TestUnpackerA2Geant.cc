60c867793e4b85c50f266f76a4cbb3bac3f62b9f
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-10-21 12:42:02 +0200

    Test: Use new Setup names with _ only (no dashes in names anymore)

--- test/unpacker/TestUnpackerA2Geant.cc
+++ test/unpacker/TestUnpackerA2Geant.cc
@@ -10,7 +10,7 @@ using namespace std;
 
 TEST_CASE("Test UnpackerA2Geant", "[unpacker]") {
     // try to open the file, but we need some setup name for this...
-    ant::ExpConfig::ManualSetupName = "Setup_2014-07_EPT_Prod";
+    ant::ExpConfig::ManualSetupName = "Setup_2014_07_EPT_Prod";
     REQUIRE_NOTHROW(ant::Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Geant_with_TID.root"));
 }
 
