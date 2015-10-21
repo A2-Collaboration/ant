15d2bc26b7c3f4a3130aaac31f246ea13e984697
Andreas Neiser <andreas.neiser@gmail.com>
2015-10-21 21:02:02 +0200

    Test: Fix compile problems with shared_ptr/nullptr on blaster args

--- test/expconfig/TestExpConfig.cc
+++ test/expconfig/TestExpConfig.cc
@@ -28,25 +28,25 @@ void getdetector() {
     REQUIRE_NOTHROW(Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-big.dat.xz"));
 
     auto tagger = ExpConfig::Setup::GetDetector<TaggerDetector_t>();
-    REQUIRE(tagger != nullptr);
+    REQUIRE(tagger.get() != nullptr);
     auto tagger_fromtype = ExpConfig::Setup::GetDetector(Detector_t::Type_t::EPT);
     REQUIRE(tagger == tagger_fromtype);
 
     auto cb = ExpConfig::Setup::GetDetector<expconfig::detector::CB>();
-    REQUIRE(cb != nullptr);
+    REQUIRE(cb.get() != nullptr);
     REQUIRE(cb->GetNChannels() == 720);
 
     auto ladder = ExpConfig::Setup::GetDetector(Detector_t::Type_t::Tagger);
-    REQUIRE(ladder == nullptr);
+    REQUIRE(ladder.get() == nullptr);
 
     // switch to another setup
     REQUIRE_NOTHROW(Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-small.dat.xz"));
     auto tagger_notthere = ExpConfig::Setup::GetDetector<TaggerDetector_t>();
-    REQUIRE(tagger_notthere == nullptr);
+    REQUIRE(tagger_notthere.get() == nullptr);
 }
 
 void getlastfound() {
-    REQUIRE(ExpConfig::Setup::GetLastFound() == nullptr);
+    REQUIRE(ExpConfig::Setup::GetLastFound().get() == nullptr);
     ExpConfig::Setup::ManualName = "Setup_2014_07_EPT_Prod";
-    REQUIRE(ExpConfig::Setup::GetLastFound() != nullptr);
+    REQUIRE(ExpConfig::Setup::GetLastFound().get() != nullptr);
 }
