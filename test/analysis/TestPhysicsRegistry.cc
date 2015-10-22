65251b2d0f24536decf432d2bb4308824b948228
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-10-22 13:43:11 +0200

    Test: Check for duplicate directories (only obvious when output file present)

--- test/analysis/TestPhysicsRegistry.cc
+++ test/analysis/TestPhysicsRegistry.cc
@@ -2,6 +2,8 @@
 
 #include "analysis/physics/Physics.h"
 #include "base/OptionsList.h"
+#include "base/WrapTFile.h"
+#include "base/tmpfile_t.h"
 
 #include "TError.h"
 
@@ -18,20 +20,38 @@ TEST_CASE("PhysicsRegistry: Create all physics classes", "[analysis]") {
 }
 
 bool histogram_overwrite_detected = false;
+bool duplicate_mkdir_detected = false;
+
 
 void dotest() {
+    // some errors only appear when some outfiles are present
+    tmpfile_t tmpfile;
+    auto outfile = std_ext::make_unique<WrapTFileOutput>(tmpfile.filename,
+                            WrapTFileOutput::mode_t::recreate,
+                            true);
+    // overwrite ROOT's error handler to detect some warnings
     SetErrorHandler([] (
                     int level, Bool_t abort, const char *location,
                     const char *msg) {
+        // those tests are specific enough...
         if(string(location) == "TDirectory::Append")
             histogram_overwrite_detected = true;
+        if(string(location) == "TDirectoryFile::mkdir")
+            duplicate_mkdir_detected = true;
         DefaultErrorHandler(level, abort, location, msg);
     });
+
+    // create all available physics classes
     std::shared_ptr<OptionsList> popts = make_shared<OptionsList>();
     for(auto name : PhysicsRegistry::GetList()) {
         histogram_overwrite_detected = false;
+        duplicate_mkdir_detected = false;
         INFO(name);
         REQUIRE_NOTHROW(PhysicsRegistry::Create(name, popts));
         REQUIRE_FALSE(histogram_overwrite_detected);
+        REQUIRE_FALSE(duplicate_mkdir_detected);
     }
+
+    // write the file
+    REQUIRE_NOTHROW(outfile = nullptr);
 }
