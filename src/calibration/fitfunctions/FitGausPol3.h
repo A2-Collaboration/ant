6888098f4301b16777b012bcfed7775e7533ed95
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-10-28 11:59:19 +0100

    Calibration: FitGausPol3: marked methods override

--- src/calibration/fitfunctions/FitGausPol3.h
+++ src/calibration/fitfunctions/FitGausPol3.h
@@ -19,15 +19,15 @@ public:
     FitGausPol3();
     virtual ~FitGausPol3();
 
-    void Draw();
-    void Fit(TH1* hist);
+    void Draw() override;
+    void Fit(TH1* hist) override;
     virtual void SetDefaults(TH1 *hist) override;
-    void SetRange(ant::interval<double> i);
-    ant::interval<double> GetRange() const;
+    void SetRange(ant::interval<double> i) override;
+    ant::interval<double> GetRange() const override;
     virtual void Sync() override;
 
-    std::vector<double> Save() const;
-    void Load(const std::vector<double> &data);
+    std::vector<double> Save() const override;
+    void Load(const std::vector<double> &data) override;
 
     virtual double GetPeakPosition() const override;
     virtual double GetPeakWidth() const override;
