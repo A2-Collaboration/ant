49ba43961f4e5db4ae4be6656a38180227aeaa5d
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-12-01 11:59:41 +0100

    Ant-calib-regedit: convert mode works

--- progs/Ant-calib-regedit.cc
+++ progs/Ant-calib-regedit.cc
@@ -3,13 +3,17 @@
 
 #include "base/std_ext/system.h"
 #include "base/std_ext/string.h"
+#include "base/WrapTFile.h"
+#include "tree/TCalibrationData.h"
+
+#include "TTree.h"
 
 #include <iostream>
 
 using namespace ant;
+using namespace ant::std_ext;
 using namespace std;
 
-void create(vector<string> inputfiles);
 void convert(string setupfolder);
 
 int main(int argc, char** argv) {
@@ -19,9 +23,7 @@ int main(int argc, char** argv) {
     TCLAP::CmdLine cmd("Ant-calib-regedit - manage calib database on-disk format", ' ', "0.1");
     auto cmd_verbose = cmd.add<TCLAP::ValueArg<int>>("v","verbose","Verbosity level (0..9)", false, 0,"level");
 
-    TCLAP::SwitchArg cmd_mode_create("","create","Create the database skeleton from given files", false);
-    TCLAP::SwitchArg cmd_mode_convert("","convert","Convert old database in given setup folders (needs already created structure)", false);
-    cmd.xorAdd(cmd_mode_convert, cmd_mode_create);
+    auto cmd_mode_convert = cmd.add<TCLAP::SwitchArg>("","convert","Convert old database in given setup folders (needs already created structure)", false);
 
     auto cmd_givenstrings  = cmd.add<TCLAP::UnlabeledMultiArg<string>>("inputfiles","Ant files with histograms",true,"inputfiles");
 
@@ -30,23 +32,80 @@ int main(int argc, char** argv) {
     if(cmd_verbose->isSet())
         el::Loggers::setVerboseLevel(cmd_verbose->getValue());
 
-    if(cmd_mode_create.isSet())
-        create(cmd_givenstrings->getValue());
-    else if(cmd_mode_convert.isSet()) {
+    if(cmd_mode_convert->isSet()) {
+        if(cmd_givenstrings->getValue().empty())
+            LOG(ERROR) << "No input folders given!";
+
         for(auto setupfolder : cmd_givenstrings->getValue())
             convert(setupfolder);
     }
 }
 
-void convert(string setupfolder) {
-    LOG(INFO) << "Converting " << setupfolder;
-    for(auto treefile : std_ext::system::lsFiles(setupfolder+"/calibration")) {
-        if(!std_ext::string_ends_with(treefile, ".root"))
-            continue;
-        LOG(INFO) << "Opening " << treefile;
+string OutFilename(const Long64_t n) {
+    return formatter() << setw(4) << setfill('0') << n << ".root";
+}
+
+void SplitTree(TTree* tree, const string& path) {
+
+
+    TCalibrationData* data = nullptr;
+
+    tree->SetBranchAddress("cdata", &data);
+
+    Long64_t n = 0;
+
+    for( n=0; n<tree->GetEntries(); ++n) {
+
+        const string current_file(path+"/"+OutFilename(n));
+        WrapTFileOutput of(current_file);
+
+        tree->GetEntry(n);
+
+        auto nbytes = of.WriteObject(data, "cdata");
+        LOG(INFO) << "Written " << nbytes << " calibration data to file";
+    }
+
+    --n;
+
+    if(n >= 0) {
+        system::exec(formatter() << "cd " << path << " && ln -s -T -f " << OutFilename(n) << " current");
     }
 }
 
-void create(vector<string> inputfiles) {
-    // ensure that nothing is there already
+void convert(string setupfolder) {
+    LOG(INFO) << "Converting " << setupfolder;
+    const string base_dir(setupfolder+"/calibration");
+
+    for(auto treefile : std_ext::system::lsFiles(base_dir)) {
+
+        if(!std_ext::string_ends_with(treefile, ".root"))
+            continue;
+
+        LOG(INFO) << "Opening " << treefile;
+
+        WrapTFileInput infile(treefile);
+
+        auto trees = infile.GetListOf<TTree>();
+
+        for(auto tree : trees) {
+            string name = tree->GetName();
+            LOG(INFO) << "Found TTree " << name;
+            if(string_starts_with(name, "calibration-")) {
+                auto calibname = name;
+                removesubstr(calibname, "calibration-");
+                LOG(INFO) << "Calibration: " << calibname;
+
+                std_ext::replace(calibname, "/", "_");
+
+                const string path = base_dir + "/" + calibname + "/DataDefault";
+
+                system::exec(formatter() << "mkdir -p " << path );
+                LOG(INFO) << "mkdir " << path;
+
+                SplitTree(tree, path);
+
+            }
+        }
+
+    }
 }
