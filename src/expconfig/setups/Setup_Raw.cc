b2630508812eb381f22c5a8546f53d2e6e084fde
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-12-04 17:15:40 +0100

    Expconfig: Implement Setup_Raw

--- src/expconfig/setups/Setup_Raw.cc
+++ src/expconfig/setups/Setup_Raw.cc
@@ -1,7 +1,7 @@
 #include "Setup.h"
 
-
-
+#include "base/Logger.h"
+#include "base/piecewise_interval.h"
 
 #include <limits>
 
@@ -19,8 +19,11 @@ class Setup_Raw :
 {
 public:
 
-    Setup_Raw(const std::string& name, SetupOptPtr opt) : Setup(name, opt) {
-
+    Setup_Raw(const std::string& name, SetupOptPtr opt) : Setup(name, opt)
+    {
+        ADC_ranges = Options->Get<decltype(ADC_ranges)>("AcquADC");
+        if(ADC_ranges.empty())
+            LOG(ERROR) << "No Acqu ADC numbers supplied. Won't write anything.";
     }
 
     virtual double GetElectronBeamEnergy() const override {
@@ -34,12 +37,30 @@ public:
     }
 
     void BuildMappings(std::vector<hit_mapping_t>& hit_mappings,
-                       std::vector<scaler_mapping_t>& scaler_mappings) const override
+                       std::vector<scaler_mapping_t>&) const override
     {
+        // create a 1:1 mapping to LogicalChannels
+        // the Detector is set to "Raw"
+
+        for(interval<unsigned> ADC_range : ADC_ranges) {
+            if(!ADC_range.IsSane()) {
+                LOG(WARNING) << "Skipping invalid ADC range " << ADC_range;
+                continue;
+            }
+            for(unsigned adc = ADC_range.Start(); adc <= ADC_range.Stop(); adc++) {
+                hit_mappings.emplace_back(
+                            Detector_t::Type_t::Raw,
+                            Channel_t::Type_t::Raw,
+                            adc,
+                            adc
+                            );
+            }
+        }
 
     }
 protected:
 
+    PiecewiseInterval<unsigned> ADC_ranges;
 };
 
 // if you forget this, your setup is never found....
