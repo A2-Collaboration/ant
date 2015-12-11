1330a700bcbf7e93baa037c8706c57d4a949c0ca
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-12-11 18:18:07 +0100

    Analysis-codes: ExtractResolutions: Energy dep. phi

--- src/root-addons/analysis_codes/ExtractResolutions.cc
+++ src/root-addons/analysis_codes/ExtractResolutions.cc
@@ -1,6 +1,7 @@
 #include "ExtractResolutions.h"
 
 #include "TreeTools.h"
+#include "analysis/plot/Histogram.h"
 #include "base/std_ext/string.h"
 #include "base/interval.h"
 
@@ -9,14 +10,18 @@
 #include "TCanvas.h"
 #include "TF1.h"
 #include "TH2.h"
+#include "TH3.h"
+#include "TDirectory.h"
 #include "root-addons/cbtaps_display/TH2CB.h"
 #include "base/std_ext/math.h"
 #include "TF1.h"
 #include <cmath>
+#include "analysis/plot/root_draw.h"
 
 using namespace std;
 using namespace ant;
 using namespace ant::std_ext;
+using namespace ant::analysis;
 
 double LogNormalPDF(const double* X, const double* p) {
 
@@ -69,40 +74,111 @@ void ExtractResolutions::AnalyseThetaCB(TTree* tree)
 
 void ExtractResolutions::AnalysePhiCB(TTree* tree)
 {
-    const interval<double> range = {-.2,.2};
+
     const unsigned nElements = 720;
+    const BinSettings xbins(nElements, 0, nElements);
+    const BinSettings ybins(16,     0.0, 1600.0);
+    const BinSettings zbins(80,    -0.2,    0.2);
 
-    new TCanvas();
-    auto h = Draw(tree, "Phi:Element",TCut(""), int(nElements), 0, nElements, 80, range.Start(), range.Stop());
+
+    TCanvas* c = new TCanvas();
+    auto h = Draw(tree, "Phi:tE:Element", TCut(""), xbins, ybins, zbins);
     h->SetXTitle("Element");
-    h->SetYTitle("#Delta #phi [rad]");
-    h->SetZTitle("# Particles");
+    h->SetZTitle("#Delta #phi [rad]");
+    h->SetYTitle("E [MeV]");
+    h->SetName("CB_phi");
 
-    TH2CB* cb = new TH2CB("phi_sigmas", "phi sigmas");
-    cb->SetZTitle("#sigma_{#phi} [rad]");
+    TF1* f = new TF1("f", "gaus", zbins.Start(), zbins.Stop());
+    f->SetParameter(0, 1);
+    f->SetParameter(1, 0);
+    f->SetParameter(2,.1);
 
-    TF1* f = new TF1("f", "gaus", range.Start(), range.Stop());
 
-    new TCanvas();
-    for(unsigned i=0; i<nElements; ++i) {
 
-        f->SetParameter(0, 1);
-        f->SetParameter(1, 0);
-        f->SetParameter(2,.1);
-        TH1* pr;
-        pr = h->ProjectionY(Form("Phi_Element_%d",i), i, i+1);
-        pr->SetTitle(Form("Element %d", i));
-        pr->SetXTitle("#Delta #phi [rad]");
-        pr->Draw();
-        pr->Fit(f, "RQ");
-        cb->SetElement(i, f->GetParameter(2));
+    TCanvas* fits = new TCanvas();
+    bool first = true;
+    c->cd();
 
-//        gPad->SaveAs(Form("Phi_cb_%d.png",i));
-//        gPad->SaveAs(Form("Phi_cb_%d.pdf",i));
+
+    TH1D* h_global_sigma = new TH1D("cb_phi_global_sigma", "Global #Delta #phi, cb", nElements, 0, nElements);
+    TH1D* h_phi_fit_p0   = new TH1D("cb_phi_fit_p0", "Fit #sigma #phi, p0, cb", nElements, 0, nElements);
+    TH1D* h_phi_fit_p1   = new TH1D("cb_phi_fit_p1", "Fit #sigma #phi, p1, cb", nElements, 0, nElements);
+    TH1D* h_phi_fit_p2   = new TH1D("cb_phi_fit_p2", "Fit #sigma #phi, p2, cb", nElements, 0, nElements);
+
+    for(int e=0; e<int(nElements); ++e) {
+        h->GetXaxis()->SetRange(e,e+1);
+
+        const string projcmd = to_string(e)+"_zy";
+
+        TH2* proj = dynamic_cast<TH2*>(h->Project3D(projcmd.c_str()));
+        proj->SetTitle(Form("#Delta #phi, CB, Element %d",e));
+        proj->Draw("colz");
+
+        proj->FitSlicesY(f,0,-1,0,"QNR");
+
+        TH1D* h_sigma = nullptr;
+
+        gDirectory->GetObject(Form("CB_phi_%d_zy_2",e), h_sigma);
+        h_sigma->SetTitle(Form("#Delta #phi, fitted, CB, Element %d",e));
+
+        TF1* phiE = new TF1(Form("phiE_%d",e),"expo+pol0(2)", 0, 1600);
+        h_sigma->Fit(phiE,"MRQ");
+
+        TH1* p1 = proj->ProjectionY(Form("CB_phi_%d_proj",e),0,-1);
+        TF1* p1f = new TF1("", "gaus", zbins.Start(), zbins.Stop());
+        p1f->SetParameter(0, 1);
+        p1f->SetParameter(1, 0);
+        p1f->SetParameter(2,.1);
+        p1->SetTitle(Form("#Delta #phi, global, CB, Element %d",e));
+        p1->Fit(p1f,"QNR");
+
+        cout << "Element " << e << ":  Global sigma="<< p1f->GetParameter(2) <<", E-Fitted: "
+             << phiE->GetParameter(0) << " "
+             << phiE->GetParameter(1) << " "
+             << phiE->GetParameter(2)
+             << " Chi2/dof=" << phiE->GetChisquare()/phiE->GetNDF() << endl;
+
+        h_global_sigma->SetBinContent(e+1,p1f->GetParameter(2));
+        h_phi_fit_p0->SetBinContent(e+1, phiE->GetParameter(0));
+        h_phi_fit_p1->SetBinContent(e+1, phiE->GetParameter(1));
+        h_phi_fit_p2->SetBinContent(e+1, phiE->GetParameter(2));
+
+        fits->cd();
+        if(first) {
+            phiE->Draw();
+            first=false;
+        } else {
+            phiE->Draw("same");
+        }
+        c->cd();
+
+
     }
 
-    new TCanvas();
-    cb->Draw("colz");
+
+    TH2CB* cb = new TH2CB("phi_sigmas", "#sigma #phi, global, CB");
+    cb->SetZTitle("#sigma_{#phi} [rad]");
+    cb->SetElements(*h_global_sigma);
+
+    TH2CB* cba = new TH2CB("phi_sigma_p0", "#sigma #phi P0");
+    cba->SetZTitle("p0");
+    cba->SetElements(*h_phi_fit_p0);
+
+    TH2CB* cbb = new TH2CB("phi_sigma_p1", "#sigma #phi P1");
+    cbb->SetZTitle("p1");
+    cbb->SetElements(*h_phi_fit_p1);
+
+    TH2CB* cbc = new TH2CB("phi_sigma_p2", "#sigma #phi P2");
+    cbc->SetZTitle("p2");
+    cbc->SetElements(*h_phi_fit_p2);
+
+    canvas("Sigma Phi CB")
+            << cb
+            << cba
+            << cbb
+            << cbc
+            << endc;
+
 }
 
 void ExtractResolutions::AnalyseECB(TTree* tree)
