8963cfbf8a529d40b82e4543e36884ff8bd76ecb
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-12-16 11:35:45 +0100

    Calibration: TAPSVeto/PID_Energy: Use finer binning

--- src/analysis/physics/calibration/PID_Energy.cc
+++ src/analysis/physics/calibration/PID_Energy.cc
@@ -32,7 +32,7 @@ PID_Energy::PID_Energy(const string& name, analysis::PhysOptPtr opts) :
                 "PID Energy / MeV",
                 "Channel",
                 BinSettings(400,0,800),
-                BinSettings(100,0,30),
+                BinSettings(200,0,15),
                 pid_channels,
                 "Bananas"
                 );
