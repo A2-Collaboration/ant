a85300b0c08b7323b6fae947b1f4424f802aa378
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-12-25 12:57:20 +0100

    Setup 2014_10_EPT_Prod: Ignore uncalibratable TAPS channels

--- src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
@@ -15,7 +15,7 @@ public:
     {
         // see https://wwwa2.kph.uni-mainz.de/intern/daqwiki/analysis/beamtimes/2014-10-14
         IgnoreDetectorChannels(Detector_t::Type_t::CB, {17,125,189,265,267,418,456,547,549,557,565,582,586,597,602,672,677,678,696});
-        IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {127,137,138,145,218,346,356,357,364});
+        IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {2, 73, 127, 137, 138, 144, 145, 146, 147, 200, 218, 220, 222, 283, 291, 346, 353, 356, 357, 363, 364, 368, 437});
         IgnoreDetectorChannels(Detector_t::Type_t::TAPSVeto, {6,64,128,192,256,263,287,321,337,349});
     }
 
