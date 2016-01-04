f0a73de8bcee026b9c39f7105a865c0fff9fbad9
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-01-04 18:04:57 +0100

    Analysis: JustPi0: Prompt-random IM plots without ParticleID dependence

--- src/analysis/physics/pi0/JustPi0.cc
+++ src/analysis/physics/pi0/JustPi0.cc
@@ -1,5 +1,12 @@
 #include "JustPi0.h"
 
+#include "utils/particle_tools.h"
+
+#include "TH1D.h"
+
+#include <memory>
+#include <cassert>
+
 using namespace ant;
 using namespace ant::analysis;
 using namespace ant::analysis::physics;
@@ -9,19 +16,94 @@ using namespace std;
 
 
 JustPi0::JustPi0(const string& name, PhysOptPtr opts) :
-    Physics(name, opts)
+    Physics(name, opts),
+    h_missingmass(promptrandom),
+    IM_2g(promptrandom)
 {
+    promptrandom.AddPromptRange({-2.5,2.5});
+    promptrandom.AddRandomRange({-50,-5});
+    promptrandom.AddRandomRange({  5,50});
+
+    steps = HistFac.makeTH1D("Steps","","#",BinSettings(10),"steps");
+
+    h_missingmass.MakeHistograms(HistFac, "h_missingmass","Missing Mass",BinSettings(400,400, 1400),"MM / MeV","#");
+    IM_2g.MakeHistograms(HistFac, "IM_2g","Invariant Mass 2#gamma",BinSettings(500,0,700),"IM / MeV","#");
 
 }
 
 void JustPi0::ProcessEvent(const Event& event)
 {
+    const auto& data = event.Reconstructed;
+
+    const auto nPhotons_expected = 2;
+
+    steps->Fill("Seen",1);
+
+    // cut on energy sum and number of candidates
+
+    if(data.Trigger.CBEnergySum <= 550)
+        return;
+    steps->Fill("CBESum>550MeV",1);
+
+    const auto& cands = data.Candidates;
+    const auto nCandidates = cands.size();
+    const auto nCandidates_expected = nPhotons_expected+1;
+    if(nCandidates != nCandidates_expected)
+        return;
+    std::string nCandidates_cutstr = std_ext::formatter() << "nCandidates==" << nCandidates_expected;
+    steps->Fill(nCandidates_cutstr.c_str(),1);
+
+
+    // use any candidate as proton, and do the analysis (ignore ParticleID stuff)
+
+    for(auto i_proton=cands.begin();i_proton!=cands.end();i_proton++) {
+
+        const auto proton = std::make_shared<Particle>(ParticleTypeDatabase::Proton, *i_proton);
+        std::vector<ParticlePtr> photons;
+        for(auto i_photon=cands.begin();i_photon!=cands.end();i_photon++) {
+            if(i_photon == i_proton)
+                continue;
+            photons.emplace_back(make_shared<Particle>(ParticleTypeDatabase::Photon, *i_photon));
+        }
+        assert(photons.size() == nPhotons_expected);
+
+
+        TLorentzVector photon_sum(0,0,0,0);
+        for(const auto& p : photons) {
+            photon_sum += *p;
+        }
+
+        for(const TaggerHit& taggerhit : data.TaggerHits) {
+            promptrandom.SetTaggerHit(taggerhit.Time);
+            if(promptrandom.State() == PromptRandom::Case::Outside)
+                continue;
+
+            const TLorentzVector beam_target = taggerhit.GetPhotonBeam() + TLorentzVector(0, 0, 0, ParticleTypeDatabase::Proton.Mass());
+            const TLorentzVector v_mm = beam_target - photon_sum;
+            const double mm = v_mm.M();
+            h_missingmass.Fill(mm);
+
+            if(mm>850 && mm<1000) {
+                steps->Fill("MM",1.0);
+                utils::ParticleTools::FillIMCombinations([this] (double x) {IM_2g.Fill(x);},  2, photons);
+
+            }
+
+        }
+
+
+    }
+
 
 }
 
 void JustPi0::ShowResult()
 {
-
+    canvas(GetName())
+            << steps
+            << h_missingmass.subtracted
+            << IM_2g.subtracted
+            << endc;
 }
 
 
--- src/analysis/physics/pi0/JustPi0.h
+++ src/analysis/physics/pi0/JustPi0.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "physics/Physics.h"
+#include "plot/PromptRandomHist.h"
+
+class TH1D;
 
 namespace ant {
 namespace analysis {
@@ -8,13 +11,20 @@ namespace physics {
 
 class JustPi0 : public Physics {
 protected:
+    TH1D* steps;
+
+
+    PromptRandom::Switch promptrandom;
+    PromptRandom::Hist1  h_missingmass;
+    PromptRandom::Hist1  IM_2g;
+
 
 
 public:
     JustPi0(const std::string& name, PhysOptPtr opts);
 
-    virtual void ProcessEvent(const data::Event& event);
-    virtual void ShowResult();
+    virtual void ProcessEvent(const data::Event& event) override;
+    virtual void ShowResult() override;
 };
 
 }}}
