4b9de4939c24f784bad28c78816d09bfad0820c3
Oliver Steffen <steffen@kph.uni-mainz.de>
2016-01-04 12:11:32 +0100

    Setup 2014_10_EPT_Prod: Ignore more TAPS channels for run by run calibration

--- src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
@@ -16,6 +16,10 @@ public:
         // see https://wwwa2.kph.uni-mainz.de/intern/daqwiki/analysis/beamtimes/2014-10-14
         IgnoreDetectorChannels(Detector_t::Type_t::CB, {17,125,189,265,267,418,456,547,549,557,565,582,586,597,602,672,677,678,696});
         IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {2, 73, 127, 137, 138, 144, 145, 146, 147, 200, 218, 220, 222, 283, 291, 346, 353, 356, 357, 363, 364, 368, 437});
+
+        // Not enough statistics for runbyrun "-a 20"
+        IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {0,64,72,75,139,217,358,419});
+
         IgnoreDetectorChannels(Detector_t::Type_t::TAPSVeto, {6,64,128,192,256,263,287,321,337,349});
     }
 
