86590b4f804b7ae82bb235092b5ca4549e42ab85
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-01-12 15:36:14 +0100

    Analysis: Add ProtonTAPS with simple but effective proton finding?

---
+++ src/analysis/physics/common/ProtonTAPS.cc
@@ -0,0 +1,81 @@
+#include "ProtonTAPS.h"
+#include "base/std_ext/math.h"
+
+#include "TTree.h"
+
+using namespace ant;
+using namespace ant::std_ext;
+using namespace ant::analysis;
+using namespace ant::analysis::data;
+using namespace ant::analysis::physics;
+using namespace std;
+
+
+ProtonTAPS::ProtonTAPS(const string& name, PhysOptPtr opts):
+    Physics(name, opts)
+{
+
+    tree = HistFac.makeTTree("tree");
+
+
+    tree->Branch("nCB",    &b_nCB);
+    tree->Branch("nTAPS",  &b_nTAPS);
+    tree->Branch("CBAvgTime", &b_CBAvgTime);
+    tree->Branch("CBAvgVetoE", &b_CBAvgVetoE);
+
+    tree->Branch("Proton", &b_Proton);
+
+}
+
+template <typename T>
+double TimeAverage(const T& cands) {
+    double time   = 0.0;
+    double energy = 0.0;
+    for(const auto& c : cands) {
+        time += c->Time * c->CaloEnergy;
+        energy += c->CaloEnergy;
+    }
+    return time / energy;
+}
+
+void ProtonTAPS::ProcessEvent(const data::Event& event)
+{
+
+    data::CandidateList cands_taps;
+    data::CandidateList cands_cb;
+
+    b_CBAvgVetoE = 0;
+    for(const auto& p : event.Reconstructed.Candidates) {
+        if(p->GetDetector() & Detector_t::Any_t::TAPS_Apparatus) {
+            cands_taps.emplace_back(p);
+        }
+        else if(p->GetDetector() & Detector_t::Any_t::CB_Apparatus) {
+            cands_cb.emplace_back(p);
+            b_CBAvgVetoE += p->VetoEnergy;
+        }
+    }
+    b_nTAPS = cands_taps.size();
+    if(b_nTAPS == 0)
+        return;
+
+    b_nCB = cands_cb.size();
+    b_CBAvgTime = TimeAverage(cands_cb);
+    b_CBAvgVetoE /= b_nCB;
+
+    // find the proton in TAPS
+    b_Proton.Time = numeric_limits<double>::quiet_NaN();
+    for(const CandidatePtr& cand_proton : cands_taps) {
+        if(!isfinite(b_Proton.Time) || b_Proton.Time < cand_proton->Time) {
+            b_Proton = *cand_proton;
+        }
+    }
+
+    tree->Fill();
+}
+
+void ProtonTAPS::ShowResult()
+{
+    tree->Draw("Proton.CaloEnergy:(Proton.Time-CBAvgTime)/cos(Proton.Theta)","nCB>2 && CBAvgVetoE < 0.25","colz");
+}
+
+AUTO_REGISTER_PHYSICS(ProtonTAPS)
---
+++ src/analysis/physics/common/ProtonTAPS.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "analysis/physics/Physics.h"
+#include "base/interval.h"
+
+class TH1D;
+class TH2D;
+
+namespace ant {
+namespace analysis {
+namespace physics {
+
+class ProtonTAPS : public Physics {
+public:
+
+    TTree* tree = nullptr;
+
+    unsigned  b_nCB = 0;
+    unsigned  b_nTAPS = 0;
+    double    b_CBAvgTime  = 0.0;
+    double    b_CBAvgVetoE  = 0.0;
+
+    TCandidate b_Proton;
+
+
+    ProtonTAPS(const std::string& name, PhysOptPtr opts);
+
+    void ProcessEvent(const data::Event &event) override;
+    void ShowResult() override;
+};
+
+}
+}
+}
