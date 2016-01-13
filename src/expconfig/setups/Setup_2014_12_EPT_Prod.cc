e20d8e26f416e5fa2ab435cb00cbd186c3a08267
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-01-13 17:33:34 +0100

    setup_dec_2014: ignored elements

--- src/expconfig/setups/Setup_2014_12_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_12_EPT_Prod.cc
@@ -13,7 +13,22 @@ public:
     Setup_2014_12_EPT_Prod(const std::string& name, SetupOptPtr opt)
         : Setup_2014_EPT(name, opt)
     {
-        /// \todo add ignored elements
+        IgnoreDetectorChannel(Detector_t::Type_t::CB,  17); /// odd time [walk]
+        IgnoreDetectorChannel(Detector_t::Type_t::CB,  26); /// empty
+        IgnoreDetectorChannel(Detector_t::Type_t::CB,  41); /// odd time walk and energy
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 125); /// odd time [walk] and energy
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 265); /// few entries
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 418); /// odd time spectrum
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 549); /// no entries
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 557); /// double peak in time
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 582); /// odd time [walk]
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 586); /// few entries
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 602); /// odd time walk
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 672); /// odd time walk
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 677); /// few entries
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 678); /// inverted/odd spectra
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 679); /// no entries
+        IgnoreDetectorChannel(Detector_t::Type_t::CB, 696); /// odd time walk
     }
 
 
