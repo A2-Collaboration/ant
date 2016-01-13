f5ae41d39008a34853554dc293b35a143b208145
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-01-13 19:14:58 +0100

    Expconfig: Detectors: Fix indendation in TAPSVeto

--- src/expconfig/detectors/TAPSVeto.cc
+++ src/expconfig/detectors/TAPSVeto.cc
@@ -12,49 +12,51 @@ using namespace ant;
 using namespace ant::expconfig::detector;
 
 void TAPSVeto::BuildMappings(vector<UnpackerAcquConfig::hit_mapping_t> &hit_mappings,
-                       vector<UnpackerAcquConfig::scaler_mapping_t>&) const {
+                             vector<UnpackerAcquConfig::scaler_mapping_t>&) const {
     for(const BaF2_Element_t& element : BaF2_elements)  {
 
-      // TAC provides timing information
 
-      hit_mappings.emplace_back(Type,
-                                Channel_t::Type_t::Timing,
-                                element.Channel,
-                                element.TAC);
 
-      // the flag UseSensitiveChannels swaps the mapping
-      // of LGS/LG to type Integral/IntegralAlternate
-      // only Integral is used in the clustering
+        // TAC provides timing information
 
-      hit_mappings.emplace_back(Type,
-                                Channel_t::Type_t::Integral,
-                                element.Channel,
-                                element.LGS
-                                                       );
+        hit_mappings.emplace_back(Type,
+                                  Channel_t::Type_t::Timing,
+                                  element.Channel,
+                                  element.TAC);
+
+        // the flag UseSensitiveChannels swaps the mapping
+        // of LGS/LG to type Integral/IntegralAlternate
+        // only Integral is used in the clustering
+
+        hit_mappings.emplace_back(Type,
+                                  Channel_t::Type_t::Integral,
+                                  element.Channel,
+                                  element.LGS
+                                  );
 
     }
 
     // the PbWO4 are a bit simpler
 
     for(const PbWO4_Element_t& element : PbWO4_elements)  {
-      hit_mappings.emplace_back(Type,
-                                Channel_t::Type_t::Timing,
-                                element.Channel,
-                                element.TDC);
+        hit_mappings.emplace_back(Type,
+                                  Channel_t::Type_t::Timing,
+                                  element.Channel,
+                                  element.TDC);
 
-      /// \todo Provide switch to use sensitive/non-sensitive?
+        /// \todo Provide switch to use sensitive/non-sensitive?
 
-      hit_mappings.emplace_back(Type,
-                                Channel_t::Type_t::Integral,
-                                element.Channel,
-                                element.QDCH
-                                                       );
+        hit_mappings.emplace_back(Type,
+                                  Channel_t::Type_t::Integral,
+                                  element.Channel,
+                                  element.QDCH
+                                  );
 
-      hit_mappings.emplace_back(Type,
-                                Channel_t::Type_t::IntegralAlternate,
-                                element.Channel,
-                                element.QDCH
-                                );
+        hit_mappings.emplace_back(Type,
+                                  Channel_t::Type_t::IntegralAlternate,
+                                  element.Channel,
+                                  element.QDCH
+                                  );
     }
 }
 
@@ -67,12 +69,12 @@ void TAPSVeto::InitElements()
     const double zpos = CherenkovInstalled ? 174.2 : 145.7;
     /// \bug check z-position, Acqu config is inconsistent
     for(auto& baf2 : BaF2_elements) {
-      baf2.Position.SetZ(zpos);
-      nElements++;
+        baf2.Position.SetZ(zpos);
+        nElements++;
     }
     for(auto& pbwo4 : PbWO4_elements) {
-      pbwo4.Position.SetZ(zpos);
-      nElements++;
+        pbwo4.Position.SetZ(zpos);
+        nElements++;
     }
 
     assert(nElements == 384);
