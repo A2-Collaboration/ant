842b686822f23e3cce7b62845478b81d4a66c6ac
Andreas Neiser <andreas.neiser@gmail.com>
2016-01-30 16:20:53 +0100

    Test: Fix UnpackerA2Geant interface usage

--- test/unpacker/TestUnpackerA2Geant.cc
+++ test/unpacker/TestUnpackerA2Geant.cc
@@ -20,7 +20,7 @@ void dotest() {
     // try to open the file, but we need some setup name for this...
     ant::ExpConfig::Setup::ManualName = "Setup_Test";
 
-    std::unique_ptr<Unpacker::Reader> unpacker;
+    std::unique_ptr<Unpacker::Module> unpacker;
     REQUIRE_NOTHROW(unpacker = ant::Unpacker::Get(string(TEST_BLOBS_DIRECTORY)+"/Geant_with_TID.root"));
     REQUIRE(unpacker != nullptr);
 
