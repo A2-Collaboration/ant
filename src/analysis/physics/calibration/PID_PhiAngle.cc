3c1dbe4d6bf56fada8b50b30eb03f0353d1e0669
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-02-12 16:21:48 +0100

    Analysis: Calibration: Simplify PID_PhiAngle, according to DebugPIDAlignment findings

--- src/analysis/physics/calibration/PID_PhiAngle.cc
+++ src/analysis/physics/calibration/PID_PhiAngle.cc
@@ -21,59 +21,31 @@ PID_PhiAngle::PID_PhiAngle(const string& name, OptionsPtr opts) :
 
 void PID_PhiAngle::ProcessEvent(const TEvent& event, manager_t&)
 {
-    const auto& cands = event.Reconstructed->Candidates;
 
     // search for events with
     // one cluster in CB, one cluster in PID
-    // ignore the matched candidates, since this is what
-    // we want to calibrate
 
-    TClusterPtr cluster_pid = nullptr;
-    double phi_cb = numeric_limits<double>::quiet_NaN();
-
-    for(const auto& cand : cands) {
-
-        if(!theta_range.Contains(cand->Theta))
-            continue;
-
-        auto cl_cb_  = cand->FindFirstCluster(Detector_t::Type_t::CB);
-
-        if(cl_cb_ != nullptr) {
-            // found more than one CB cluster
-            if(isfinite(phi_cb))
-                return;
-            phi_cb = cand->Phi;
-        }
-
-        auto cl_pid_ = cand->FindFirstCluster(Detector_t::Type_t::PID);
-
-        if(cl_pid_ != nullptr) {
-            // found more than one pid cluster
-            if(cluster_pid != nullptr)
-                return;
-            cluster_pid = cl_pid_;
-        }
-    }
-
-    /// \todo search all clusters, leave candidates alone
+    TClusterPtr cluster_pid;
+    TClusterPtr cluster_cb;
     for(const TClusterPtr& cl : event.Reconstructed->Clusters) {
-        if(cl->DetectorType != Detector_t::Type_t::PID)
-            continue;
-        if(!isfinite(cl->Energy) || !isfinite(cl->Time))
-            continue;
-        // found more than one PID cluster
-        if(cluster_pid != nullptr)
-            return;
-        cluster_pid = cl;
+        if(cl->DetectorType == Detector_t::Type_t::PID) {
+            if(cluster_pid)
+                return;
+            cluster_pid = cl;
+        }
+        else if(cl->DetectorType == Detector_t::Type_t::CB) {
+            if(cluster_cb)
+                return;
+            cluster_cb = cl;
+        }
     }
 
-    if(!isfinite(phi_cb) || cluster_pid == nullptr)
-        return;
+    if(cluster_pid && cluster_cb) {
+        const double phi_cb_degrees = std_ext::radian_to_degree(cluster_cb->Position.Phi());
 
-    const double phi_cb_degrees = std_ext::radian_to_degree(phi_cb);
-
-    pid_cb_phi_corr->Fill(phi_cb_degrees,     cluster_pid->CentralElement);
-    pid_cb_phi_corr->Fill(phi_cb_degrees+360, cluster_pid->CentralElement);
+        pid_cb_phi_corr->Fill(phi_cb_degrees,     cluster_pid->CentralElement);
+        pid_cb_phi_corr->Fill(phi_cb_degrees+360, cluster_pid->CentralElement);
+    }
 }
 
 void PID_PhiAngle::ShowResult()
