b5ab7e62c1d20adc81114c81fdbe2942d10bd82e
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-03-03 18:34:33 +0100

    [processEtap3pi0] combinations for nucleons resonances

--- progs/processEtapTree.cc
+++ progs/processEtapTree.cc
@@ -33,6 +33,7 @@ using namespace ant::analysis::utils;
 
 int main( int argc, char** argv )
 {
+    // ===================== INIT  ==============================================================================
     SetupLogger();
 
     TCLAP::CmdLine cmd("process analysis tree for physics class Etap3Pi0", ' ', "0.1");
@@ -49,18 +50,31 @@ int main( int argc, char** argv )
     auto app = new TRint("Ant-calib",&fake_argc,fake_argv);
 
 
+
+    // ===================== HIST  ==============================================================================
     HistogramFactory HistFac("hists");
 
-    BinSettings singlePion(250,0,700);
+    BinSettings singlePion(360,0,700);
     BinSettings doublePion(360,0,1000);
+    BinSettings PionP(360,900,1900);
+    BinSettings doublePionP(360,1000,2000);
 
-    TH2D* all      = HistFac.makeTH2D("All events",                   "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"all");
-    TH2D* signal   = HistFac.makeTH2D("Signal selection",             "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"signal");
-    TH2D* ref      = HistFac.makeTH2D("Reference selection",          "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"ref");
-    TH2D* mcSignal = HistFac.makeTH2D("Signal mc true",               "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mcSignal");
-    TH2D* mcRef    = HistFac.makeTH2D("Reference mc true",            "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mcRef");
-    TH2D* mc3pi0   = HistFac.makeTH2D("3 #pi^{0} production mc true", "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mc3pi0");
+    std::map<std::string,std::map<std::string,TH2D*>> hists2d;
 
+    string cat("pionsID");
+    hists2d[cat]["all"]      = HistFac.makeTH2D("All events",                   "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"all");
+    hists2d[cat]["signal"]   = HistFac.makeTH2D("Signal selection",             "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"signal");
+    hists2d[cat]["ref"]      = HistFac.makeTH2D("Reference selection",          "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"ref");
+    cat = "pionsMC";
+    hists2d[cat]["mcSignal"] = HistFac.makeTH2D("Signal mc true",               "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mcSignal");
+    hists2d[cat]["mcRef"]    = HistFac.makeTH2D("Reference mc true",            "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mcRef");
+    hists2d[cat]["mc3pi0"]   = HistFac.makeTH2D("3 #pi^{0} production mc true", "m(2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePion,singlePion,"mc3pi0");
+    cat = "NucleonRes";
+    hists2d[cat]["1pi"]      = HistFac.makeTH2D("Invariant Mass",               "m(p #pi^{0}) [MeV]","m(2 #pi^{0}) [MeV]",PionP,doublePion,"1pi");
+    hists2d[cat]["2pi"]      = HistFac.makeTH2D("Invariant Mass",               "m(p 2 #pi^{0}) [MeV]","m(#pi^{0}) [MeV]",doublePionP,singlePion,"2pi");
+
+
+    // ===================== TREE  ==============================================================================
     WrapTFileInput file(cmd_infile->getValue());
     TTree* tree;
     file.GetObject("Etap3pi0/tree",tree);
@@ -68,10 +82,10 @@ int main( int argc, char** argv )
     double taggW;
     int type;
     int truetype;
-    vector<TLorentzVector> pions(3);
-    vector<TLorentzVector> gammas(6);
-    TLorentzVector sixG;
-    TLorentzVector proton;
+    vector<TLorentzVector>* pions = new vector<TLorentzVector>(3);
+    vector<TLorentzVector>* gammas = new vector<TLorentzVector>(6);
+    TLorentzVector* sixG = new TLorentzVector();
+    TLorentzVector* proton = new TLorentzVector();
 
     tree->SetBranchAddress("taggWeight", &taggW);
     tree->SetBranchAddress("type",&type);
@@ -81,33 +95,54 @@ int main( int argc, char** argv )
     tree->SetBranchAddress("kf_6g", &sixG);
     tree->SetBranchAddress("kf_p", &proton);
 
+    // ===================== PLOT  ==============================================================================
     for (std::int64_t ind_entry = 0 ; ind_entry < tree->GetEntries() ; ++ind_entry)
     {
         tree->GetEntry(ind_entry);
         for ( unsigned i = 0 ; i < 3 ; ++i)
         {
-//            const auto pM  = gammas[i]->M();
-//            const auto ppM = (*gammas.at((i + 2) % 3) + *gammas.at((i + 1) % 3)).M();
-            /*
-            all->Fill(ppM, pM, taggW);
+            const auto pM  = pions->at(i).M();
+            const auto ppM = (pions->at((i + 2) % 3) + pions->at((i + 1) % 3)).M();
+            hists2d.at("pionsID").at("all")->Fill(ppM,pM,taggW);
             if ( type == 0 )
-                signal->Fill(ppM, pM, taggW);
+                hists2d.at("pionsID").at("signal")->Fill(ppM, pM, taggW);
             if ( type == 1 )
-                ref->Fill(ppM, pM, taggW);
+                hists2d.at("pionsID").at("ref")->Fill(ppM, pM, taggW);
             if ( truetype == 0 )
-                mcSignal->Fill(ppM, pM, taggW);
+                hists2d.at("pionsMC").at("mcSignal")->Fill(ppM, pM, taggW);
             if ( truetype == 1 )
-                mcRef->Fill(ppM, pM, taggW);
+                hists2d.at("pionsMC").at("mcRef")->Fill(ppM, pM, taggW);
             if ( truetype == 2 )
-                mc3pi0->Fill(ppM, pM, taggW);
-                */
+                hists2d.at("pionsMC").at("mc3pi0")->Fill(ppM, pM, taggW);
+        }
+        for(auto comb=utils::makeCombination(*pions, 3); !comb.Done(); ++comb)
+        {
+            const auto N    = comb.at(0) + *(proton);
+            const auto pipi = comb.at(1) + comb.at(2);
+            hists2d.at("NucleonRes").at("1pi")->Fill(N.M(),pipi.M(),taggW);
+        }
+        for(auto comb=utils::makeCombination(*pions, 2); !comb.Done(); ++comb)
+        {
+            const auto N  = comb.at(0) + comb.at(1) + *(proton);
+            const auto pi = *comb.begin_not();
+            hists2d.at("NucleonRes").at("2pi")->Fill(N.M(),pi.M(),taggW);
         }
     }
 
-    gStyle->SetOptStat(kFALSE);
-    canvas("identified") << drawoption("col") << all << signal << ref << endc;
-    canvas("mctrue") << drawoption("col") << mcSignal << mcRef << mc3pi0 << endc;
 
+
+    // ===================== DRAW  ==============================================================================
+    gStyle->SetOptStat(kFALSE);
+
+    vector<string> cats2d = {"pionsID","pionsMC","NucleonRes"};
+    for (auto& category: cats2d)
+    {
+        canvas c(category);
+        c << drawoption("colz");
+        for (auto h: hists2d.at(category))
+            c << h.second;
+        c << endc;
+    }
     app->Run(kTRUE);
 
 }
