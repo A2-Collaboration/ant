450c1dfe22ae90417dfdcb280aeee7eb6278b930
Jennifer Wettig <wettig@kph.uni-mainz.de>
2016-03-14 22:39:22 +0100

    Analysis: nicer cuts and plots for RarePion analysis class

--- src/analysis/physics/pi0/RarePion.cc
+++ src/analysis/physics/pi0/RarePion.cc
@@ -1,16 +1,16 @@
 #include "RarePion.h"
 
 #include "base/ParticleType.h"
-#include "utils/combinatorics.h"
-#include "TH1D.h"
-#include <memory>
-#include <iostream>
 #include "plot/HistogramFactories.h"
-#include "TCanvas.h"
-
 #include "plot/root_draw.h"
-
+#include "utils/combinatorics.h"
 #include "utils/particle_tools.h"
+
+#include "TCanvas.h"
+#include "TH1D.h"
+
+#include <iostream>
+#include <memory>
 
 /**
  * this is mainly a copy of the class TestPhysics and to be changed that it fits the analysis
@@ -34,17 +34,17 @@ RarePion::RarePion(const std::string& name, OptionsPtr opts):
     gggim    = HistFac.makeTH1D("3 #gamma IM",       "M_{#gamma #gamma #gamma} [MeV]",  "#", im_binning,    "IM_3g");
     ggggim   = HistFac.makeTH1D("4 #gamma IM",       "M_{4#gamma} [MeV]",               "#", im_binning,    "IM_4g");
     im_3g    = HistFac.makeTH1D("IM of exact 3#gamma", "M_{3#gamma}",                   "#", im_binning,    "IM_3g_exact");
+    im_2g    = HistFac.makeTH1D("IM of exact 2#gamma", "M_{2#gamma}",                   "#", im_binning,    "IM_2g_exact");
 
     nphotons = HistFac.makeTH1D("Number of photons", "N",                               "#", npart_binning, "N_g");
-    nphotons_anglecut = HistFac.makeTH1D("Number of photons after cut on angle", "N",   "#", npart_binning, "N_g_cut");
+    nphotons_anglecut = HistFac.makeTH1D("Number of photons after cut on angle > 40#circ", "N",   "#", npart_binning, "N_g_cut");
 
     theta_vs_En = HistFac.makeTH2D("Theta vs Energy","E_{kin} [MeV]","#Theta [#circ]",energy_binning,theta_binning,"EnTheta");
+    splitoffenergy = HistFac.makeTH2D("Energy of 2 nearby photons (out of exact 3)","E_{kin} of one #gamma [MeV]","E_{kin} of the other #gamma [MeV]",energy_binning,energy_binning,"EnSplit");
 
-    minAngle = HistFac.makeTH1D("angle between any 2 #gamma",       "#alpha [#circ]","#",theta_binning,"alpha");
-    minAngle2g = HistFac.makeTH1D("angle between exact 2 #gamma",   "#alpha [#circ]","#",theta_binning,"alpha2g");
-    minAngle3g = HistFac.makeTH1D("angle between 2 #gamma out of 3","#alpha [#circ]","#",theta_binning,"alpha3g");
-    splitoffangle = HistFac.makeTH1D("smallest angle between 2 #gamma","#alpha [#circ]","#",theta_binning,"alphamin");
-
+    minAngle = HistFac.makeTH1D("angle between any 2 #gamma of 3 clusters",       "#alpha [#circ]","#",theta_binning,"alpha");
+    minAngle2g = HistFac.makeTH1D("angle between exact 2 #gamma (2 clusters)",   "#alpha [#circ]","#",theta_binning,"alpha2g");
+    splitoffangle = HistFac.makeTH1D("smallest angle between 2 #gamma (3 clusters)","#alpha [#circ]","#",theta_binning,"alphamin");
 
     // Build a map of ParticleType -> Histogram, and fill it
 //    for( auto& type : ParticleTypeDatabase() ) {
@@ -72,71 +72,93 @@ void RarePion::ProcessEvent(const TEvent& event, manager_t&)
 
     nphotons->Fill(photons.size());
 
-    auto combinations2 = utils::makeCombination(photons,2);
-    do {
-        TLorentzVector v;
-        for( auto& i: combinations2 ) {
-            v += *i;
-        }
-
-        ggim->Fill(v.M());
-
-    } while(combinations2.next());
-
-    auto combinations3 = utils::makeCombination(photons,3);
-    do {
-        TLorentzVector v;
-        for( auto& i: combinations3 ) {
-            v += *i;
-        }
-
-        gggim->Fill(v.M());
-
-    } while(combinations3.next());
-
-    auto combinations4 = utils::makeCombination(photons,4);
-    do {
-        TLorentzVector v;
-        for( auto& i: combinations4 ) {
-            v += *i;
-        }
-
-        ggggim->Fill(v.M());
-
-    } while(combinations4.next());
-
+    Bool_t checkCB = true;
     for( auto& p: photons ) {
-        theta_vs_En->Fill(p->Ek(),p->Theta()*TMath::RadToDeg());
-    }
-
-    for( auto comb = utils::makeCombination(photons,2); !comb.Done(); ++comb) {
-        minAngle->Fill(comb.at(0)->Angle(comb.at(1)->Vect())*TMath::RadToDeg());
-    }
-
-    if(photons.size() == 2) { // cut already on # of clusters
-        for( auto comb = utils::makeCombination(photons,2); !comb.Done(); ++comb) {
-            minAngle2g->Fill(comb.at(0)->Angle(comb.at(1)->Vect())*TMath::RadToDeg());
+        if (! (p->Candidate->Detector & Detector_t::Type_t::CB)){
+            checkCB = false;
         }
     }
 
-    if(photons.size() == 3) { // cut already on # of clusters
-        const TParticle pi0 ( ParticleTypeDatabase::Pi0, *photons.at(0) + *photons.at(1) + *photons.at(2));
-        im_3g->Fill(pi0.M());
-        Double_t anglemin = 180;
+    if ( checkCB == true ){ // only continue when all photons are in CB
+        for( auto& p: photons ) {
+            theta_vs_En->Fill(p->Ek(),p->Theta()*TMath::RadToDeg());
+        }
+
+        auto combinations2 = utils::makeCombination(photons,2);
+        do {
+            TLorentzVector v;
+            for( auto& i: combinations2 ) {
+                v += *i;
+            }
+
+            ggim->Fill(v.M());
+
+        } while(combinations2.next());
+
+        auto combinations3 = utils::makeCombination(photons,3);
+        do {
+            TLorentzVector v;
+            for( auto& i: combinations3 ) {
+                v += *i;
+            }
+
+            gggim->Fill(v.M());
+
+        } while(combinations3.next());
+
+        auto combinations4 = utils::makeCombination(photons,4);
+        do {
+            TLorentzVector v;
+            for( auto& i: combinations4 ) {
+                v += *i;
+            }
+
+            ggggim->Fill(v.M());
+
+        } while(combinations4.next());
+
         for( auto comb = utils::makeCombination(photons,2); !comb.Done(); ++comb) {
-            Double_t angle = comb.at(0)->Angle(comb.at(1)->Vect())*TMath::RadToDeg();
-            //cout << "angle: " << angle << "\n";
-            minAngle3g->Fill(angle);
-            if(anglemin > angle) {
-                anglemin = angle;
+            minAngle->Fill(comb.at(0)->Angle(comb.at(1)->Vect())*TMath::RadToDeg());
+        }
+
+        if(photons.size() == 2) { // cut already on # of clusters
+            const TParticle pi02g ( ParticleTypeDatabase::Pi0, *photons.at(0) + *photons.at(1));
+            im_2g->Fill(pi02g.M());
+            for( auto comb = utils::makeCombination(photons,2); !comb.Done(); ++comb) {
+                minAngle2g->Fill(comb.at(0)->Angle(comb.at(1)->Vect())*TMath::RadToDeg());
             }
         }
-        splitoffangle->Fill(anglemin);
-        if (anglemin > 30) {
-            nphotons_anglecut->Fill(photons.size());
+
+        if(photons.size() == 3) { // cut already on # of clusters
+            const TParticle pi03g ( ParticleTypeDatabase::Pi0, *photons.at(0) + *photons.at(1) + *photons.at(2));
+            im_3g->Fill(pi03g.M());
+            Double_t anglemin = 180;
+            angle01 = photons.at(0)->Angle(photons.at(1)->Vect())*TMath::RadToDeg();
+            angle02 = photons.at(0)->Angle(photons.at(2)->Vect())*TMath::RadToDeg();
+            angle12 = photons.at(1)->Angle(photons.at(2)->Vect())*TMath::RadToDeg();
+            if (angle01 < angle02){
+                if (angle01 < angle12){
+                    //Fall1, vectors 01
+                    splitoffenergy->Fill(photons.at(0)->Ek(),photons.at(1)->Ek());
+                    anglemin = angle01;
+                }
+                else {
+                    //Fall2, vectors 12
+                    splitoffenergy->Fill(photons.at(1)->Ek(),photons.at(2)->Ek());
+                    anglemin = angle12;
+                }
+            }
+            else  if (angle02 < angle12){
+                //Fall3, vectors 02
+                splitoffenergy->Fill(photons.at(0)->Ek(),photons.at(2)->Ek());
+                anglemin = angle02;
+            }
+            splitoffangle->Fill(anglemin);
+            if (anglemin > 40) {
+                nphotons_anglecut->Fill(photons.size());
+            }
         }
     }
-
 }
 
 void RarePion::Finish()
@@ -154,13 +176,11 @@ void RarePion::ShowResult()
 //          nphotons <<
 //          //nprotons <<
 //          endc;
-
 //    canvas cc2("RarePion2");
 //    for( auto& e : EHists) {
 //        cc2 << e.second;
 //    }
 //    cc2 << endc;
-
 
 }
 
--- src/analysis/physics/pi0/RarePion.h
+++ src/analysis/physics/pi0/RarePion.h
@@ -18,13 +18,19 @@ protected:
     TH1D* nphotons;
     TH1D* nphotons_anglecut;
     TH1D* im_3g;
+    TH1D* im_2g;
     TH1D* minAngle;
     TH1D* minAngle2g;
-    TH1D* minAngle3g;
     TH1D* splitoffangle;
 
     TH2D* theta_vs_En;
+    TH2D* splitoffenergy;
 
+    Double_t angle01;
+    Double_t angle02;
+    Double_t angle12;
+
+    interval<double> theta_cut = {25,155};
 
     //std::map<const ant::ParticleTypeDatabase::Type*, TH1D*> EHists;
 
