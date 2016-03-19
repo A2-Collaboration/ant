735685d5fd6de6696abaa6690f359a35d7f1a855
Andreas Neiser <andreas.neiser@gmail.com>
2016-03-19 17:24:45 +0100

    Reconstruct: Clustering: Little code refactoring

--- src/reconstruct/Clustering.cc
+++ src/reconstruct/Clustering.cc
@@ -45,55 +45,48 @@ void Clustering::Build(const shared_ptr<ClusterDetector_t>& clusterdetector,
     for(const clustering::cluster_t& cluster : crystal_clusters) {
         const double cluster_energy = clustering::calc_total_energy(cluster);
 
-        vec3   weightedPosition(0,0,0);
-        double weightedSum = 0;
+        clusters.emplace_back(
+                    vec3(0,0,0),
+                    cluster_energy,
+                    std_ext::NaN,
+                    clusterdetector->Type,
+                    0
+                    );
+        auto& the_cluster = clusters.back();
 
-        double   cluster_time        = numeric_limits<double>::quiet_NaN();
-        double   cluster_maxenergy   = 0;
-        unsigned cluster_max_channel = 0;
-        double   cluster_shortenergy = numeric_limits<double>::quiet_NaN();
-        bool     cluster_toucheshole = false;
-
-        std::vector<TClusterHit> clusterhits;
+        auto& clusterhits = the_cluster.Hits;
         clusterhits.reserve(cluster.size());
 
+        double weightedSum = 0;
+        double cluster_maxenergy   = 0;
         for(const clustering::crystal_t& crystal : cluster) {
-            double wgtE = clustering::calc_energy_weight(crystal.Energy, cluster_energy);
-            weightedPosition += crystal.Element->Position * wgtE;
-            weightedSum += wgtE;
+
             clusterhits.emplace_back(*crystal.Hit);
+
+            double wgtE = clustering::calc_energy_weight(crystal.Energy, cluster_energy);
+            the_cluster.Position += crystal.Element->Position * wgtE;
+            weightedSum += wgtE;
+
             if(cluster_maxenergy<=crystal.Energy) {
-                cluster_toucheshole = crystal.Element->TouchesHole;
-                cluster_time = crystal.Hit->Time;
                 cluster_maxenergy = crystal.Energy;
-                cluster_max_channel = crystal.Element->Channel;
+
+                the_cluster.SetFlag(TCluster::Flags_t::TouchesHole, crystal.Element->TouchesHole);
+                the_cluster.Time = crystal.Hit->Time;
+                the_cluster.CentralElement = crystal.Element->Channel;
                 // search for short energy
                 for(const TClusterHit::Datum& datum : crystal.Hit->Data) {
                     if(datum.Type == Channel_t::Type_t::IntegralShort) {
-                        cluster_shortenergy = datum.Value;
+                        the_cluster.ShortEnergy = datum.Value;
                         break;
                     }
                 }
             }
         }
-        weightedPosition *= 1.0/weightedSum;
+        the_cluster.Position *= 1.0/weightedSum;
 
-
-        clusters.emplace_back(
-                    weightedPosition,
-                    cluster_energy,
-                    cluster_time,
-                    clusterdetector->Type,
-                    cluster_max_channel,
-                    clusterhits
-                    );
-        auto& the_cluster = clusters.back();
-        if(isfinite(cluster_shortenergy))
-            the_cluster.ShortEnergy = cluster_shortenergy;
         if(cluster.Split)
             the_cluster.SetFlag(TCluster::Flags_t::Split);
-        if(cluster_toucheshole)
-            the_cluster.SetFlag(TCluster::Flags_t::TouchesHole);
+
     }
 }
 
