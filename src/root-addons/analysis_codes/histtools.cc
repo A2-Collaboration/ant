16a4bea664775db07220769f0a5e3d0956247140
Oliver Steffen <steffen@kph.uni-mainz.de>
2016-03-30 17:33:58 +0200

    root-addons: histtoos: added PlotSignificance(TH1, TH1)

--- src/root-addons/analysis_codes/histtools.cc
+++ src/root-addons/analysis_codes/histtools.cc
@@ -36,3 +36,31 @@ TH1* histtools::CutPosPlot(const TH1* const h1, const TH1* const h2)
     }
     return h;
 }
+
+TH1*histtools::PlotSignificance(const TH1* const h1, const TH1* const h2)
+{
+    if(h1->GetNbinsX() != h2->GetNbinsX()) {
+        cerr << "ERROR: Bin count mismatch!" << endl;
+    }
+
+    auto h = new TH1D("","",h1->GetNbinsX(), h1->GetXaxis()->GetXmin(), h1->GetXaxis()->GetXmax());
+    h->SetXTitle("Cut Pos");
+    h->SetYTitle("Significance");
+
+    if(!h)
+        cerr << "Error cloning histogram!" << endl;
+
+    double sum1 = 0.0;
+    double sum2 = 0.0;
+    for(int i=1; i<=h1->GetNbinsX(); ++i) {
+        sum1 += h1->GetBinContent(i);
+        sum2 += h2->GetBinContent(i);
+
+        const auto v = sum1 / sqrt(sum1+sum2);
+
+        if(isfinite(v))
+            h->SetBinContent(i, v);
+
+    }
+    return h;
+}
--- src/root-addons/analysis_codes/histtools.h
+++ src/root-addons/analysis_codes/histtools.h
@@ -14,6 +14,14 @@ struct histtools {
      * @return new histogram
      */
     static TH1* CutPosPlot(const TH1* const h1, const TH1* const h2);
+
+    /**
+     * @brief Plot Ns / sqrt (Ns / Nb)
+     * @param h1
+     * @param h2
+     * @return new histogram
+     */
+    static TH1* PlotSignificance(const TH1* const h1, const TH1* const h2);
 };
 
 }
