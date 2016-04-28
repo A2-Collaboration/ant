46af67fdd29a5587f4b1b98266465d4243a4517d
Andreas Neiser <andreas.neiser@gmail.com>
2016-04-28 18:31:03 +0200

    Base: Add std_ext::copy_if_better

--- src/base/std_ext/misc.h
+++ src/base/std_ext/misc.h
@@ -10,6 +10,20 @@ namespace std_ext {
 
 using namespace std;
 
+template<class T, class U, class Better>
+bool copy_if_better(T& dest, const U& source,
+                    Better better) {
+    if(std::isfinite(dest) && better(dest, source))
+        return false;
+    dest = source;
+    return true;
+}
+
+template<class T, class U>
+bool copy_if_greater(T& dest, const U& source) {
+    return copy_if_better(dest, source, std::greater<U>());
+}
+
 
 template<class TContainer>
 bool begins_with(const TContainer& input, const TContainer& match)
