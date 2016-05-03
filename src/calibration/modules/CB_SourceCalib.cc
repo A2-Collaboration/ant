512858881da4a8a4fda53ab84ae4cd01a1daf32c
Felix Weber <wfelix@students.uni-mainz.de>
2016-05-03 15:16:21 +0100

    src:calibration: dependencies on Energy class removed, GUI works

--- src/calibration/modules/CB_SourceCalib.cc
+++ src/calibration/modules/CB_SourceCalib.cc
@@ -5,6 +5,7 @@
 #include "calibration/fitfunctions/FitGausexpo.h"
 
 #include "expconfig/detectors/CB.h"
+#include "tree/TDetectorReadHit.h"
 
 
 #include "root-addons/cbtaps_display/TH2CB.h"
@@ -16,71 +17,98 @@ using namespace ant::calibration;
 
 CB_SourceCalib::CB_SourceCalib(std::shared_ptr<expconfig::detector::CB> cb,
                                std::shared_ptr<DataManager> calmgr,
-                               Calibration::Converter::ptr_t converter,
-                               double defaultPedestal,
-                               double defaultGain,
-                               double defaultThreshold,
-                               double defaultRelativeGain):
-    Energy(cb->Type,
-           calmgr,
-           converter,
-           defaultPedestal,
-           defaultGain,
-           defaultThreshold,
-           defaultRelativeGain),
-    cb_detector(cb)
-{
+                               Calibration::Converter::ptr_t converter
+                               ) :
+    Calibration::PhysicsModule("CB_SourceCalib"),
+    cb_detector(cb),
+    calibrationManager(calmgr),
+    Converter(move(converter))
 
+{
 }
 
+CB_SourceCalib::~CB_SourceCalib()
+{
+}
+
+void CB_SourceCalib::ApplyTo(const readhits_t &hits)
+{
+
+    const auto& dethits = hits.get_item(Detector_t::Type_t::CB);
+    //const auto& dethits = hits.get_item(cb_detector->Type);
+
+    for(TDetectorReadHit& dethit : dethits) {
+        if(dethit.ChannelType != Channel_t::Type_t::Integral)
+            continue;
+        std::vector<double> all_values;
+        dethit.Converted = Converter->Convert(dethit.RawData);
+        for(double value : dethit.Converted){
+            all_values.push_back(value);
+        }
+
+        for (double value: all_values)
+        {
+            dethit.Values.push_back(value);
+        }
+    }
+
+
+}
+//GUI
 void CB_SourceCalib::GetGUIs(list<unique_ptr<gui::CalibModule_traits> >& guis)
 {
-    guis.emplace_back(std_ext::make_unique<GUI_Gains>(
+    guis.emplace_back(std_ext::make_unique<TheGUI>(
                           GetName(),
-                          RelativeGains,
                           calibrationManager,
                           cb_detector
                           ));
 }
 
-CB_SourceCalib::GUI_Gains::GUI_Gains(const string& basename,
-                                     CalibType& type,
-                                     const std::shared_ptr<DataManager>& calmgr,
-                                     const std::shared_ptr<Detector_t>& detector) :
-               GUI_CalibType(basename, type, calmgr, detector),
-               func(make_shared<gui::FitGausexpo>())
+CB_SourceCalib::TheGUI::TheGUI(const string &basename,
+                               const std::shared_ptr<DataManager> & calmgr,
+                               const std::shared_ptr<expconfig::detector::CB> & cb) :
+    gui::CalibModule_traits(basename),
+    calibrationManager(calmgr),
+    cb_detector(cb),
+    func(make_shared<gui::FitGausexpo>())
 {
 }
 
-
-void CB_SourceCalib::GUI_Gains::InitGUI(gui::ManagerWindow_traits* window)
+CB_SourceCalib::TheGUI::~TheGUI()
 {
-    GUI_CalibType::InitGUI(window);
 
-    window->AddNumberEntry("Chi2/NDF limit for autostop", AutoStopOnChi2);
-
-    canvas = window->AddCalCanvas();
-    h_peaks = new TH1D("h_peaks","Peak positions",GetNumberOfChannels(),0,GetNumberOfChannels());
-    h_peaks-> SetXTitle("Channel Number");
-    h_peaks-> SetYTitle("4.4Mev Peak");
 }
 
-gui::CalibModule_traits::DoFitReturn_t CB_SourceCalib::GUI_Gains::DoFit(TH1 *hist, unsigned channel)
+shared_ptr<TH1> CB_SourceCalib::TheGUI::GetHistogram(const WrapTFile &file) const
 {
-    if(detector->IsIgnored(channel))
+    return file.GetSharedHist<TH1>("CB_SourceCalib/HitsADC_Cluster");
+}
+
+unsigned CB_SourceCalib::TheGUI::GetNumberOfChannels() const
+{
+    return cb_detector->GetNChannels();
+}
+
+void CB_SourceCalib::TheGUI::InitGUI(gui::ManagerWindow_traits * window)
+{
+    canvas = window ->AddCalCanvas();
+}
+
+void CB_SourceCalib::TheGUI::StartSlice(const interval<TID> & range)
+{
+
+}
+
+gui::CalibModule_traits::DoFitReturn_t CB_SourceCalib::TheGUI::DoFit(TH1 *hist, unsigned channel)
+{
+    if(cb_detector->IsIgnored(channel))
         return DoFitReturn_t::Skip;
 
     TH2* hist2 = dynamic_cast<TH2*>(hist);
 
     h_projection = hist2->ProjectionX("h_projection",channel+1,channel+1);
 
-    if(h_projection->GetEntries()==0)
-        return DoFitReturn_t::Display;
-
     func->SetDefaults(h_projection);
-
-    const auto it_fit_param =fitParameters.find(channel);
-
     auto fit_loop = [this] (size_t retries) {
         do {
             func->Fit(h_projection);
@@ -99,43 +127,34 @@ gui::CalibModule_traits::DoFitReturn_t CB_SourceCalib::GUI_Gains::DoFit(TH1 *his
     LOG(INFO) << "Chi2/dof = " << func->Chi2NDF();
     return DoFitReturn_t::Display;
 
-
 }
-void CB_SourceCalib::GUI_Gains::DisplayFit()
+
+void CB_SourceCalib::TheGUI::DisplayFit()
 {
-    canvas->Divide(1,1);
     canvas->Show(h_projection, func.get());
 }
 
-void CB_SourceCalib::GUI_Gains::StoreFit(unsigned channel)
+void CB_SourceCalib::TheGUI::StoreFit(unsigned channel)
 {
-    const double oldValue = previousValues[channel];
-
-
+    fitParameters[channel] = func->Save();
 }
 
-shared_ptr<TH1> CB_SourceCalib::GUI_Gains::GetHistogram(const WrapTFile &file) const
+bool CB_SourceCalib::TheGUI::FinishSlice()
 {
-    return file.GetSharedHist<TH1>("CB_SourceCalib/HitsADC_Cluster");
-}
-
-
-bool CB_SourceCalib::GUI_Gains::FinishSlice()
-{
-    canvas->Clear();
-    canvas->Divide(2,2);
-
-    canvas->cd(1);
-    h_peaks->SetStats(false);
-    h_peaks->Draw("P");
-
     return true;
 }
 
-string CB_SourceCalib::GUI_Gains::GetName() const
+void CB_SourceCalib::TheGUI::StoreFinishSlice(const interval<TID> &range)
+{
+
+}
+
+std::string CB_SourceCalib::TheGUI::GetName() const
 {
     return "bla";
 }
+
+
 
 
 
--- src/calibration/modules/CB_SourceCalib.h
+++ src/calibration/modules/CB_SourceCalib.h
@@ -20,47 +20,66 @@ namespace gui {
 class FitGausexpo;
 }
 
-class CB_SourceCalib : public Energy
+class CB_SourceCalib : public Calibration::PhysicsModule,
+                       public ReconstructHook::DetectorReadHits
+
 {
 
 public:
-    struct GUI_Gains : GUI_CalibType {
-        GUI_Gains(const std::string& basename,
-               CalibType& type,
-               const std::shared_ptr<DataManager>& calmgr,
-               const std::shared_ptr<Detector_t>& detector);
+    virtual void ApplyTo(const readhits_t &hits) override;
 
-        virtual void InitGUI(gui::ManagerWindow_traits* window) override;
-        virtual DoFitReturn_t DoFit(TH1 *hist, unsigned channel) override;
-        virtual void DisplayFit() override;
-        virtual void StoreFit(unsigned channel) override;
-        virtual std::shared_ptr<TH1>  GetHistogram(const WrapTFile &file) const;
-        virtual bool FinishSlice() override;
-        virtual std::string GetName() const override;
+
+    //GUI
+    class TheGUI : public gui::CalibModule_traits {
     protected:
+        std::shared_ptr<DataManager> calibrationManager;
+        std::shared_ptr<expconfig::detector::CB> cb_detector;
         std::shared_ptr<gui::FitGausexpo> func;
+
         gui::CalCanvas* canvas;
         TH1* h_projection = nullptr;
-        TH1D* h_peaks = nullptr;
 
+        std::map< unsigned, std::vector<double> > fitParameters;
 
         double AutoStopOnChi2 = 6;
+
+    public:
+        TheGUI(const std::string& basename,
+               const std::shared_ptr<DataManager>& calmgr,
+               const std::shared_ptr<expconfig::detector::CB>& cb);
+        virtual ~TheGUI();
+
+        virtual std::shared_ptr<TH1> GetHistogram(const WrapTFile& file) const override;
+        virtual unsigned GetNumberOfChannels() const override;
+        virtual void InitGUI(gui::ManagerWindow_traits*) override;
+
+        virtual void StartSlice(const interval<TID>& range) override;
+        virtual DoFitReturn_t DoFit(TH1* hist, unsigned channel) override;
+        virtual void DisplayFit() override;
+        virtual void StoreFit(unsigned channel) override;
+        virtual bool FinishSlice() override;
+        virtual void StoreFinishSlice(const interval<TID>& range) override;
+        virtual std::string GetName() const override;
 
     };
 
     CB_SourceCalib(
             std::shared_ptr<expconfig::detector::CB> cb,
             std::shared_ptr<DataManager> calmgr,
-            Calibration::Converter::ptr_t converter,
-            double defaultPedestal,
-            double defaultGain,
-            double defaultThreshold,
-            double defaultRelativeGain);
+            Calibration::Converter::ptr_t converter
+            );
+
+    virtual ~CB_SourceCalib();
+
 
     virtual void GetGUIs(std::list<std::unique_ptr<gui::CalibModule_traits> >& guis) override;
 
+
+
 protected:
      std::shared_ptr<expconfig::detector::CB> cb_detector;
+     std::shared_ptr<DataManager> calibrationManager;
+     const Calibration::Converter::ptr_t Converter;
 };
 
 }}
