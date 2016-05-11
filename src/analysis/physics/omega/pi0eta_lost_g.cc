f97f41386179b045630be58fcccec4c81a3f4953
Oliver Steffen <steffen@kph.uni-mainz.de>
2016-05-11 14:07:08 +0200

    Analysis: Omega: added Pi0Eta lost gamma class

---
+++ src/analysis/physics/omega/pi0eta_lost_g.cc
@@ -0,0 +1,46 @@
+﻿#include "pi0eta_lost_g.h"
+#include "base/Logger.h"
+#include <algorithm>
+#include "base/std_ext/math.h"
+
+#include "TTree.h"
+
+#include "utils/matcher.h"
+
+using namespace std;
+using namespace ant;
+using namespace ant::analysis;
+using namespace ant::analysis::physics;
+using namespace ant::std_ext;
+
+Pi0EtaLostG::Pi0EtaLostG(const std::string& name, OptionsPtr opts):
+    Physics(name, opts),
+    tree(HistFac.makeTTree("tree"))
+{
+    t.CreateBranches(tree);
+}
+
+Pi0EtaLostG::~Pi0EtaLostG()
+{
+}
+
+void Pi0EtaLostG::ProcessEvent(const TEvent& event, manager_t& manager)
+{
+
+    const auto& mc_cands = event.MCTrue().Particles.GetAll();
+    const auto& re_cands = event.Reconstructed().Candidates;
+
+    if(mc_cands.size() != 5 || re_cands.size() != 4)
+        return;
+
+    // find matched and unmatched
+}
+
+
+
+Pi0EtaLostG::Tree_t::Tree_t()
+{
+
+}
+
+AUTO_REGISTER_PHYSICS(Pi0EtaLostG)
---
+++ src/analysis/physics/omega/pi0eta_lost_g.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "analysis/physics/Physics.h"
+#include "base/Tree.h"
+#include "base/interval.h"
+#include "base/std_ext/math.h"
+#include "base/WrapTTree.h"
+
+#include "TTree.h"
+#include "TLorentzVector.h"
+#include "Rtypes.h"
+
+#include <map>
+
+
+class TH1D;
+class TH2D;
+class TH3D;
+
+namespace ant {
+
+namespace analysis {
+namespace physics {
+
+
+
+class Pi0EtaLostG : public Physics {
+public:
+    struct Tree_t : WrapTTree {
+        Tree_t();
+
+        ADD_BRANCH_T(std::vector<TLorentzVector>, photon, 3)
+        ADD_BRANCH_T(TLorentzVector, proton)
+        ADD_BRANCH_T(double, E)
+        ADD_BRANCH_T(double, theta)
+        ADD_BRANCH_T(double, phi)
+        ADD_BRANCH_T(double, MinAngle)
+    };
+
+protected:
+    TTree*  tree;
+    Tree_t t;
+
+public:
+
+    Pi0EtaLostG(const std::string& name, OptionsPtr opts);
+    virtual ~Pi0EtaLostG();
+
+    void ProcessEvent(const TEvent& event, manager_t& manager) override;
+};
+
+}
+}
+}
