b678a7a283afa1dc1f845cc2b2f82958e61f45f4
Sascha Wagner <wagners@kph.uni-mainz.de>
2016-06-16 15:49:14 +0200

    Analysis: Calibration: Use Kinfit instead of pure kinematics
    Use KinFitter to select events, added more debug histograms, included
    trees

--- src/analysis/physics/calibration/PID_Energy_etaDalitz.cc
+++ src/analysis/physics/calibration/PID_Energy_etaDalitz.cc
@@ -15,19 +15,51 @@ void PID_Energy_etaDalitz::shift_right(std::vector<T>& v)
     std::rotate(v.begin(), v.end() -1, v.end());
 }
 
-PID_Energy_etaDalitz::PID_Energy_etaDalitz(const string& name, OptionsPtr opts) :
-    Physics(name, opts)
+APLCON::Fit_Settings_t PID_Energy_etaDalitz::MakeFitSettings(unsigned max_iterations)
 {
+    auto settings = APLCON::Fit_Settings_t::Default;
+    settings.MaxIterations = max_iterations;
+    return settings;
+}
+
+PID_Energy_etaDalitz::Tree_t::Tree_t()
+{}
+
+PID_Energy_etaDalitz::PID_Energy_etaDalitz(const string& name, OptionsPtr opts) :
+    Physics(name, opts),
+    kinfit("kinfit", 3,
+           make_shared<uncertainty_model_t>(),
+           PID_Energy_etaDalitz::MakeFitSettings(20)
+           )
+{
+    promptrandom.AddPromptRange({-5, 5});
+    promptrandom.AddRandomRange({-30, -10});
+    promptrandom.AddRandomRange({10, 30});
+
     auto detector = ExpConfig::Setup::GetDetector(Detector_t::Type_t::PID);
 
-    const BinSettings cb_channels(detector->GetNChannels());
+    t.CreateBranches(HistFac.makeTTree("tree"));
+
+    const BinSettings pid_channels(detector->GetNChannels());
     const BinSettings energybins(1000, 0, 10);
 
     eegPID = HistFac.makeTH2D("PID 2 charged 1 neutral", "PID Energy [MeV]", "#",
-                              energybins, cb_channels, "eegPID");
+                              energybins, pid_channels, "eegPID");
+    steps = HistFac.makeTH1D("Accepted Events", "step", "#", BinSettings(10), "steps");
     etaIM = HistFac.makeTH1D("#eta IM all comb", "IM [MeV]", "#", BinSettings(1200), "etaIM");
+    etaIM_fit = HistFac.makeTH1D("#eta IM fitted", "IM [MeV]", "#", BinSettings(1200), "etaIM_fit");
+    etaIM_cand = HistFac.makeTH1D("#eta IM candidates", "IM [MeV]", "#", BinSettings(1200), "etaIM_cand");
+    etaIM_final = HistFac.makeTH1D("#eta IM final", "IM [MeV]", "#", BinSettings(1200), "etaIM_final");
+    MM = HistFac.makeTH1D("Missing mass proton", "MM [MeV]", "#", BinSettings(1600), "MM");
     hCopl = HistFac.makeTH1D("coplanarity #eta - proton all comb", "coplanarity [#circ]", "#", BinSettings(720, -180, 180), "hCopl");
+    hCopl_final = HistFac.makeTH1D("coplanarity #eta - proton final", "coplanarity [#circ]", "#", BinSettings(720, -180, 180), "hCopl_final");
+    pTheta = HistFac.makeTH1D("#vartheta proton candidate", "#vartheta_{p} [#circ]", "#", BinSettings(720, 0, 180), "pTheta");
     protonVeto = HistFac.makeTH1D("Veto energy identified proton", "Veto [MeV]", "#", energybins, "protonVeto");
+    hChi2 = HistFac.makeTH1D("#chi^{2}", "#chi^{2}", "#", BinSettings(500, 0, 100), "hChi2");
+    hProb = HistFac.makeTH1D("Probability", "probability", "#", BinSettings(500, 0, 1), "hProb");
+    hIter = HistFac.makeTH1D("# Iterations", "#iterations", "#", BinSettings(20), "hIter");
+    hEta = HistFac.makeTH2D("Kinematics #eta", "Energy [MeV]", "#vartheta [#circ]", BinSettings(1200), BinSettings(360, 0, 180), "hEta");
+    hProton = HistFac.makeTH2D("Kinematics p", "Energy [MeV]", "#vartheta [#circ]", BinSettings(1200), BinSettings(160, 0, 80), "hProton");
 }
 
 void PID_Energy_etaDalitz::ProcessEvent(const TEvent& event, manager_t&)
@@ -35,38 +67,180 @@ void PID_Energy_etaDalitz::ProcessEvent(const TEvent& event, manager_t&)
     //const auto& cands = event.Reconstructed().Candidates;
     const auto& data = event.Reconstructed();
     const auto& cands = data.Candidates;
-    const auto nCandidates = cands.size();
+    //const auto nCandidates = cands.size();
+    t.nCands = cands.size();
+    steps->Fill("seen", 1);
 
-    if(cands.size() != 4)
+    if (cands.size() != 4)
         return;
+    steps->Fill("#cands", 1);
 
     TLorentzVector eta;
     TLorentzVector proton;
-    const interval<double> eta_im({ETA_IM-ETA_SIGMA, ETA_IM+ETA_SIGMA});
-    const interval<double> coplanarity({-10, 10});
-    vector<TCandidatePtr> comb;
-    for( auto p : cands.get_iter() )
+    //const interval<double> eta_im({ETA_IM-ETA_SIGMA, ETA_IM+ETA_SIGMA});
+    const interval<double> coplanarity({-25, 25});
+    TCandidatePtrList comb;
+    for (auto p : cands.get_iter())
         comb.emplace_back(p);
+/* old routine w/o kinfit
     bool found;
-    for( size_t j = 0; j < cands.size(); j++ ) {  // loop to test all different combinations
+    for (size_t j = 0; j < cands.size(); j++) {  // loop to test all different combinations
+        // ensure the possible proton candidate is kinematically allowed
+        if (std_ext::radian_to_degree(comb.back()->Theta) > 60.)
+            continue;
         found = true;
         eta.SetXYZT(0,0,0,0);
-        for( size_t i = 0; i < comb.size()-1; i++ )
+        for (size_t i = 0; i < comb.size()-1; i++)
             eta += TParticle(ParticleTypeDatabase::Photon, comb.at(i));
         proton = TParticle(ParticleTypeDatabase::Proton, comb.back());
         const double copl = std_ext::radian_to_degree(abs(eta.Phi() - proton.Phi())) - 180.;
         etaIM->Fill(eta.M());
         hCopl->Fill(copl);
-        if (eta_im.Contains(eta.M()) && coplanarity.Contains(copl)
+        if (eta_im.Contains(eta.M()) && coplanarity.Contains(copl) && comb.back()->VetoEnergy
                 && std::count_if(comb.begin(), comb.end()-1, [](TCandidatePtr c){ return c->VetoEnergy; }) >= 2)
             break;
         found = false;
         shift_right(comb);
     }
-    if(!found)
+    if (!found)
+        return;
+*/
+
+    // require at least 2 candidates with PID/Veto entries
+    if (std::count_if(comb.begin(), comb.end(), [](TCandidatePtr c){ return c->VetoEnergy; }) < 2)
+        return;
+    steps->Fill("#Veto", 1);
+
+    t.CBAvgTime = event.Reconstructed().Trigger.CBTiming;
+    if (event.HasMCTrue())
+        t.CBAvgTime = 0;
+    if (!isfinite(t.CBAvgTime))
         return;
 
+    TParticleList photons;
+    TLorentzVector missing;
+    std::vector<utils::Fitter::FitParticle> fitparticles;
+    const interval<double> mm({ParticleTypeDatabase::Proton.Mass()-150., ParticleTypeDatabase::Proton.Mass()+150.});
+    double min_chi2 = std_ext::inf;
+    size_t best_comb = cands.size();
+    for (const TTaggerHit& taggerhit : data.TaggerHits) {  // loop over all tagger hits
+        promptrandom.SetTaggerHit(taggerhit.Time - t.CBAvgTime);
+        if (promptrandom.State() == PromptRandom::Case::Outside)
+            continue;
+        steps->Fill("time window", 1);
+
+        t.TaggW = promptrandom.FillWeight();
+        t.TaggE = taggerhit.PhotonEnergy;
+        t.TaggT = taggerhit.Time;
+        t.TaggCh = taggerhit.Channel;
+
+        for (size_t i = 0; i < cands.size(); i++) {  // loop to test all different combinations
+            // ensure the possible proton candidate is kinematically allowed
+            if (std_ext::radian_to_degree(comb.back()->Theta) > 60.) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("proton #vartheta", 1);
+
+            // require 2 PID entries for the eta candidate
+            if (std::count_if(comb.begin(), comb.end()-1, [](TCandidatePtr c){ return c->VetoEnergy; }) < 2) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("2 PIDs", 1);
+
+            photons.clear();
+            proton = TParticle(ParticleTypeDatabase::Proton, comb.back());
+            eta.SetXYZT(0,0,0,0);
+            for (size_t j = 0; j < comb.size()-1; j++) {
+                photons.emplace_back(make_shared<TParticle>(ParticleTypeDatabase::Photon, comb.at(j)));
+                eta += TParticle(ParticleTypeDatabase::Photon, comb.at(j));
+            }
+            t.eta = eta;
+            etaIM->Fill(eta.M(), t.TaggW);
+
+            const double copl = std_ext::radian_to_degree(abs(eta.Phi() - proton.Phi())) - 180.;
+            t.copl = copl;
+            hCopl->Fill(copl, t.TaggW);
+            if (!coplanarity.Contains(copl)) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("coplanarity", 1);
+
+            missing = taggerhit.GetPhotonBeam() + LorentzVec(0, 0, 0, ParticleTypeDatabase::Proton.Mass());
+            missing -= eta;
+            t.missing_momentum = missing;
+            MM->Fill(missing.M(), t.TaggW);
+            if (!mm.Contains(missing.M())) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("missing mass", 1);
+
+            kinfit.SetEgammaBeam(taggerhit.PhotonEnergy);
+            const auto& p = make_shared<TParticle>(ParticleTypeDatabase::Proton, comb.back());
+            kinfit.SetProton(p);
+            kinfit.SetPhotons(photons);
+
+            auto result = kinfit.DoFit();
+
+            if (result.Status != APLCON::Result_Status_t::Success) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("kinfit", 1);
+
+            const double chi2 = result.ChiSquare;
+            const double prob = result.Probability;
+            const int iterations = result.NIterations;
+            hChi2->Fill(chi2);
+            hProb->Fill(prob);
+            hIter->Fill(iterations);
+
+            if (prob < .1) {
+                shift_right(comb);
+                continue;
+            }
+            steps->Fill("probability", 1);
+
+            hEta->Fill(eta.E() - eta.M(), std_ext::radian_to_degree(eta.Theta()), t.TaggW);
+            hProton->Fill(proton.E() - proton.M(), std_ext::radian_to_degree(proton.Theta()), t.TaggW);
+
+            auto fitted_photons = kinfit.GetFittedPhotons();
+            eta.SetXYZT(0,0,0,0);
+            for (const auto& g : fitted_photons)
+                eta += *g;
+            t.eta_fit = eta;
+            etaIM_fit->Fill(eta.M(), t.TaggW);
+
+            if (chi2 < min_chi2) {
+                min_chi2 = chi2;
+                best_comb = i;
+            }
+            t.chi2 = chi2/result.NDoF;
+            t.probability = prob;
+            t.iterations = iterations;
+
+            shift_right(comb);
+        }
+    }
+
+    if (best_comb >= cands.size() || !isfinite(min_chi2))
+        return;
+    steps->Fill("best comb", 1);
+
+    // restore combinations with best chi2
+    while (best_comb-- > 0)
+        shift_right(comb);
+
+    proton = TParticle(ParticleTypeDatabase::Proton, comb.back());
+    eta.SetXYZT(0,0,0,0);
+    for (size_t i = 0; i < comb.size()-1; i++)
+        eta += TParticle(ParticleTypeDatabase::Photon, comb.at(i));
+    etaIM_cand->Fill(eta.M());
     protonVeto->Fill(comb.back()->VetoEnergy);
+    pTheta->Fill(std_ext::radian_to_degree(comb.back()->Theta));
     // at this point a possible eta Dalitz candidate was found, work only with eta final state
     comb.pop_back();
 
@@ -78,16 +252,22 @@ void PID_Energy_etaDalitz::ProcessEvent(const TEvent& event, manager_t&)
     const TCandidatePtr& l1 = comb.at(0);
     const TCandidatePtr& l2 = comb.at(1);
     // suppress conversion decays
-    if(l1->FindVetoCluster()->CentralElement == l2->FindVetoCluster()->CentralElement)
+    if (l1->FindVetoCluster()->CentralElement == l2->FindVetoCluster()->CentralElement)
         return;
+    steps->Fill("distinct PID", 1);
     const double eeIM = (TParticle(ParticleTypeDatabase::eMinus, l1) + TParticle(ParticleTypeDatabase::eMinus, l2)).M();
     // suppress pi0
-    if(eeIM > 135.)
+    if (eeIM > 130.)
         return;
+    steps->Fill("above #pi^{0}", 1);
 
-    for( const TCandidatePtr& c : comb )
-        if(c->VetoEnergy)
+    etaIM_final->Fill(eta.M());
+    hCopl_final->Fill(std_ext::radian_to_degree(abs(eta.Phi() - proton.Phi())) - 180.);
+    for (const TCandidatePtr& c : comb)
+        if (c->VetoEnergy)
             eegPID->Fill(c->VetoEnergy, c->FindVetoCluster()->CentralElement);
+            //eegPID->Fill(c->VetoEnergy*sin(c->Theta), c->FindVetoCluster()->CentralElement);
+    t.Tree->Fill();
 }
 
 void PID_Energy_etaDalitz::ShowResult()
--- src/analysis/physics/calibration/PID_Energy_etaDalitz.h
+++ src/analysis/physics/calibration/PID_Energy_etaDalitz.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "analysis/physics/Physics.h"
+#include "analysis/utils/Fitter.h"
+#include "analysis/utils/FitterUncertainties.h"
+#include "analysis/plot/PromptRandomHist.h"
+#include "base/WrapTTree.h"
 
 #include "root-addons/cbtaps_display/TH2CB.h"
 
@@ -14,11 +18,51 @@ class PID_Energy_etaDalitz : public Physics {
 
 protected:
     TH2* eegPID = nullptr;
+    TH1D* steps = nullptr;
     TH1D* etaIM = nullptr;
+    TH1D* etaIM_fit = nullptr;
+    TH1D* etaIM_cand = nullptr;
+    TH1D* etaIM_final = nullptr;
+    TH1D* MM = nullptr;
     TH1D* hCopl = nullptr;
+    TH1D* hCopl_final = nullptr;
+    TH1D* pTheta = nullptr;
     TH1D* protonVeto = nullptr;
+    TH1D* hChi2 = nullptr;
+    TH1D* hProb = nullptr;
+    TH1D* hIter = nullptr;
+    TH2* hEta = nullptr;
+    TH2* hProton = nullptr;
     static constexpr double ETA_IM = 547.853;
-    static constexpr double ETA_SIGMA = 30.;
+    static constexpr double ETA_SIGMA = 50.;
+
+    struct Tree_t : WrapTTree {
+        Tree_t();
+
+        ADD_BRANCH_T(TLorentzVector, eta)
+        ADD_BRANCH_T(TLorentzVector, eta_fit)
+        ADD_BRANCH_T(TLorentzVector, missing_momentum)
+        ADD_BRANCH_T(double, copl)
+        ADD_BRANCH_T(double, copl_final)
+
+        ADD_BRANCH_T(double, TaggW)
+        ADD_BRANCH_T(double, TaggE)
+        ADD_BRANCH_T(double, TaggT)
+        ADD_BRANCH_T(unsigned, TaggCh)
+
+        ADD_BRANCH_T(double, chi2)
+        ADD_BRANCH_T(double, probability)
+        ADD_BRANCH_T(unsigned, iterations)
+
+        ADD_BRANCH_T(unsigned, nCands)
+        ADD_BRANCH_T(double, CBSumE)
+        ADD_BRANCH_T(double, CBAvgTime)
+    };
+
+    Tree_t t;
+    PromptRandom::Switch promptrandom;
+    utils::KinFitter kinfit;
+    using uncertainty_model_t = utils::UncertaintyModels::Optimized_Oli1;
 
     template<typename T>
     void shift_right(std::vector<T>&);
@@ -26,6 +70,8 @@ protected:
 public:
 
     PID_Energy_etaDalitz(const std::string& name, OptionsPtr opts);
+
+    static APLCON::Fit_Settings_t MakeFitSettings(unsigned);
 
     virtual void ProcessEvent(const TEvent& event, manager_t& manager) override;
     virtual void ShowResult() override;
