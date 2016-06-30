4742f5315583a692f0ca33a694233ac5002c0483
Andreas Neiser <andreas.neiser@gmail.com>
2016-06-30 21:18:13 +0200

    Base: Interpolator finished

--- src/base/Interpolator.cc
+++ src/base/Interpolator.cc
@@ -7,16 +7,38 @@ extern "C" {
 using namespace std;
 using namespace ant;
 
+struct Interpolator2D::interp2d : ::interp2d {};
+struct Interpolator2D::gsl_interp_accel : ::gsl_interp_accel {};
+
+const interp2d_type* getType(Interpolator2D::Type type) {
+    switch(type) {
+    case Interpolator2D::Type::Bilinear:
+        return interp2d_bilinear;
+    case Interpolator2D::Type::Bicubic:
+        return interp2d_bicubic;
+    }
+    return nullptr;
+}
+
 
 Interpolator2D::Interpolator2D(const std::vector<double>& x,
                                const std::vector<double>& y,
-                               const std::vector<double>& z) :
-    X(x), Y(y), Z(z)
+                               const std::vector<double>& z,
+                               Type type) :
+    X(x), Y(y), Z(z),
+    interp(static_cast<interp2d*>(
+               interp2d_alloc(getType(type), X.size(), Y.size())
+               ), interp2d_free),
+    xa(static_cast<gsl_interp_accel*>(gsl_interp_accel_alloc()), gsl_interp_accel_free),
+    ya(static_cast<gsl_interp_accel*>(gsl_interp_accel_alloc()), gsl_interp_accel_free)
 {
     if(X.size()*Y.size() != Z.size())
         throw Exception("X*Y grid must match to Z values");
+    interp2d_init(interp.get(), X.data(), Y.data(), Z.data(), X.size(), Y.size());
+}
 
-    interp2d_spline* spline = interp2d_spline_alloc(interp2d_bicubic, X.size(), Y.size());
-
-    (void)spline;
+double Interpolator2D::GetPoint(double x, double y) const
+{
+    return interp2d_eval(interp.get(), X.data(), Y.data(), Z.data(), x, y,
+                         xa.get(), ya.get());
 }
--- src/base/Interpolator.h
+++ src/base/Interpolator.h
@@ -2,24 +2,42 @@
 
 #include <vector>
 #include <stdexcept>
+#include <memory>
 
 namespace ant {
 
 class Interpolator2D {
 public:
+    enum class Type {
+        Bilinear, Bicubic
+    };
+
     Interpolator2D(const std::vector<double>& x,
                    const std::vector<double>& y,
-                   const std::vector<double>& z);
+                   const std::vector<double>& z,
+                   Type type = Type::Bicubic);
 
-    class Exception : public std::runtime_error {
+    double GetPoint(double x, double y) const;
+
+    struct Exception : std::runtime_error {
         using std::runtime_error::runtime_error; // use base class constructor
     };
 
-protected:
+private:
+
     const std::vector<double> X;
     const std::vector<double> Y;
     const std::vector<double> Z;
 
+    template<typename T>
+    using deleted_unique_ptr = std::unique_ptr<T, std::function<void(T*)>>;
+
+    struct interp2d;
+    deleted_unique_ptr<interp2d> interp;
+
+    struct gsl_interp_accel;
+    deleted_unique_ptr<gsl_interp_accel> xa;
+    deleted_unique_ptr<gsl_interp_accel> ya;
 };
 
 }
