422a66d7270e67afe2e7300a6eb8853105a129c6
Andreas Neiser <andreas.neiser@gmail.com>
2016-07-21 21:29:58 +0200

    Test: Unpacker: Open Mk1/gz compressed test file

--- test/unpacker/TestUnpackerAcqu.cc
+++ test/unpacker/TestUnpackerAcqu.cc
@@ -16,7 +16,7 @@ TEST_CASE("Test UnpackerAcqu: headeronly, big record length", "[unpacker]") {
     dotest(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-big.dat.xz");
 }
 
-TEST_CASE("Test UnpackerAcqu: one event, big record length", "[unpacker]") {
+TEST_CASE("Test UnpackerAcqu: one event block, big record length", "[unpacker]") {
     dotest(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-big.dat.xz");
 }
 
@@ -24,10 +24,13 @@ TEST_CASE("Test UnpackerAcqu: headeronly, small record length", "[unpacker]") {
     dotest(string(TEST_BLOBS_DIRECTORY)+"/Acqu_headeronly-small.dat.xz");
 }
 
-TEST_CASE("Test UnpackerAcqu: one event, small record length", "[unpacker]") {
+TEST_CASE("Test UnpackerAcqu: one event block, small record length", "[unpacker]") {
     dotest(string(TEST_BLOBS_DIRECTORY)+"/Acqu_oneevent-small.dat.xz");
 }
 
+TEST_CASE("Test UnpackerAcqu: Mk1, three blocks", "[unpacker]") {
+    dotest(string(TEST_BLOBS_DIRECTORY)+"/AcquMk1_problematic.dat.gz");
+}
 
 void dotest(const string &filename) {
     ant::test::EnsureSetup();
