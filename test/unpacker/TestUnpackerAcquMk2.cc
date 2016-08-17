9b4be9d688e22abe828a2b4af8404d6ba15fc6fb
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-08-17 21:09:51 +0200

    Unpacker: fixed test for slowcontrols

--- test/unpacker/TestUnpackerAcquMk2.cc
+++ test/unpacker/TestUnpackerAcquMk2.cc
@@ -55,7 +55,7 @@ void dotest() {
         }
     }
 
-    REQUIRE(nSlowControls == 18);
+    REQUIRE(nSlowControls == 20);
     REQUIRE(nEvents == 211);
     REQUIRE(nHits == 30563);
     REQUIRE(nEmptyEvents == 0);
