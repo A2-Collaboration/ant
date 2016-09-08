3ccde66949529aa8803df045a49638be3433d023
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-09-08 17:06:44 +0200

    Add fitter business to EtapSergey from EtapOmegaG

--- src/analysis/physics/etaprime/etaprime_sergey.cc
+++ src/analysis/physics/etaprime/etaprime_sergey.cc
@@ -10,9 +10,75 @@ using namespace ant::analysis;
 using namespace ant::analysis::physics;
 using namespace std;
 
-EtapSergey::EtapSergey(const string& name, OptionsPtr opts) :
-    Physics(name, opts)
+utils::TreeFitter Make(const EtapSergey::params_t& params)
 {
+    auto setupnodes = [] (const ParticleTypeTree& t) {
+        utils::TreeFitter::nodesetup_t nodesetup;
+        // always exlude the EtaPrime
+        if(t->Get() == ParticleTypeDatabase::EtaPrime)
+            nodesetup.Excluded = true;
+        // exclude the Omega as well
+        if(t->Get() == ParticleTypeDatabase::Omega)
+            nodesetup.Excluded = true;
+        return nodesetup;
+    };
+
+    utils::TreeFitter treefitter{
+        "sig_treefitter_Pi0",
+        ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::EtaPrime_gOmega_ggPi0_4g),
+        params.Fit_uncertainty_model,
+        params.Fit_Z_vertex,
+        setupnodes
+    };
+    if(params.Fit_Z_vertex)
+        treefitter.SetZVertexSigma(params.Z_vertex_sigma);
+    return treefitter;
+}
+
+EtapSergey::EtapSergey(const string& name, OptionsPtr opts) :
+    Physics(name, opts),
+    params(// use FitterSergey as default
+           make_shared<utils::UncertaintyModels::FitterSergey>(),
+           true, // flag to enable z vertex
+           3.0 // Z_vertex_sigma, =0 means unmeasured
+           ),
+    kinfitter_sig("kinfitter_sig",4,
+                  params.Fit_uncertainty_model, params.Fit_Z_vertex
+                  ),
+    treefitter(Make(params)),
+    treefitter_Pi0Pi0("treefit_Pi0Pi0",
+                      ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::TwoPi0_4g),
+                      params.Fit_uncertainty_model, params.Fit_Z_vertex, {}
+                      ),
+    treefitter_Pi0Eta("treefit_Pi0Eta",
+                      ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::Pi0Eta_4g),
+                      params.Fit_uncertainty_model, params.Fit_Z_vertex, {}
+                      ),
+    fitted_Pi0(treefitter.GetTreeNode(ParticleTypeDatabase::Pi0)),
+    fitted_Omega(treefitter.GetTreeNode(ParticleTypeDatabase::Omega)),
+    fitted_EtaPrime(treefitter.GetTreeNode(ParticleTypeDatabase::EtaPrime))
+{
+    if(params.Fit_Z_vertex) {
+        treefitter_Pi0Pi0.SetZVertexSigma(params.Z_vertex_sigma);
+        treefitter_Pi0Eta.SetZVertexSigma(params.Z_vertex_sigma);
+    }
+
+    // search dependent gammas and remember the tree nodes in the fitter
+
+    auto find_photons = [] (utils::TreeFitter::tree_t fitted) {
+        std::vector<utils::TreeFitter::tree_t> photons;
+        for(const auto& d : fitted->Daughters())
+            if(d->Get().TypeTree->Get() == ParticleTypeDatabase::Photon)
+                photons.emplace_back(d);
+        return photons;
+    };
+
+    fitted_g1_Pi0 = find_photons(fitted_Pi0).at(0);
+    fitted_g2_Pi0 = find_photons(fitted_Pi0).at(1);
+
+    fitted_g_Omega = find_photons(fitted_Omega).at(0);
+
+    fitted_g_EtaPrime = find_photons(fitted_EtaPrime).at(0);
 
 }
 
@@ -29,6 +95,12 @@ void EtapSergey::ProcessEvent(const TEvent& event, manager_t&)
     for(auto& r : result)
         cout << r << endl;
     cout << endl;
+
+    // try to reproduce the result from Sergey
+    for(const auto& r : result) {
+
+    }
+
 }
 
 void EtapSergey::ShowResult()
--- src/analysis/physics/etaprime/etaprime_sergey.h
+++ src/analysis/physics/etaprime/etaprime_sergey.h
@@ -14,9 +14,39 @@ namespace physics {
 class EtapSergey : public Physics {
 public:
 
+    struct params_t {
+        const utils::UncertaintyModelPtr Fit_uncertainty_model;
+        const bool Fit_Z_vertex;
+        const double Z_vertex_sigma;
+        params_t(utils::UncertaintyModelPtr fit_uncertainty_model,
+                 bool fit_Z_vertex, double Z_vertex_sigma) :
+            Fit_uncertainty_model(fit_uncertainty_model),
+            Fit_Z_vertex(fit_Z_vertex),
+            Z_vertex_sigma(Z_vertex_sigma)
+        {}
+    };
+
 protected:
 
+    const params_t params;
+
     utils::FitterSergey fitter_sergey;
+
+    utils::KinFitter kinfitter_sig;
+
+    utils::TreeFitter treefitter;
+    utils::TreeFitter treefitter_Pi0Pi0;
+    utils::TreeFitter treefitter_Pi0Eta;
+
+    utils::TreeFitter::tree_t fitted_Pi0;
+    utils::TreeFitter::tree_t fitted_g1_Pi0;
+    utils::TreeFitter::tree_t fitted_g2_Pi0;
+
+    utils::TreeFitter::tree_t fitted_Omega;
+    utils::TreeFitter::tree_t fitted_g_Omega;
+
+    utils::TreeFitter::tree_t fitted_EtaPrime;
+    utils::TreeFitter::tree_t fitted_g_EtaPrime;
 
 public:
     EtapSergey(const std::string& name, OptionsPtr opts);
