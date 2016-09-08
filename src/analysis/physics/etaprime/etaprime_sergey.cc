6943a391f3f4de7acd836d4cb180d755e0e5921a
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-09-08 17:45:27 +0200

    Start comparison, WIP

--- src/analysis/physics/etaprime/etaprime_sergey.cc
+++ src/analysis/physics/etaprime/etaprime_sergey.cc
@@ -59,6 +59,7 @@ EtapSergey::EtapSergey(const string& name, OptionsPtr opts) :
     fitted_EtaPrime(treefitter.GetTreeNode(ParticleTypeDatabase::EtaPrime))
 {
     if(params.Fit_Z_vertex) {
+        kinfitter_sig.SetZVertexSigma(params.Z_vertex_sigma);
         treefitter_Pi0Pi0.SetZVertexSigma(params.Z_vertex_sigma);
         treefitter_Pi0Eta.SetZVertexSigma(params.Z_vertex_sigma);
     }
@@ -86,21 +87,60 @@ void EtapSergey::ProcessEvent(const TEvent& event, manager_t&)
 {
     const TEventData& data = event.Reconstructed();
 
-    auto result = fitter_sergey.Process(data);
+    auto results_sergey = fitter_sergey.Process(data);
 
-    if(result.empty())
+    if(results_sergey.empty())
         return;
 
     cout << ">>>> Sergey:" << endl;
-    for(auto& r : result)
+    for(auto& r : results_sergey)
         cout << r << endl;
-    cout << endl;
+
+    vector<double> photon_energies(results_sergey.size());
+    std::transform(results_sergey.begin(), results_sergey.end(), photon_energies.begin(),
+                   [] (const utils::FitterSergey::result_t& r) {
+        return r.TaggE;
+    });
+
+    vector<utils::FitterSergey::result_t> results_ant;
 
     // try to reproduce the result from Sergey
-    for(const auto& r : result) {
+    for(const auto& photon_energy : photon_energies) {
 
+        utils::FitterSergey::result_t r;
+
+        r.KinFitProb = std_ext::NaN;
+
+        for(const auto& cand_proton :  data.Candidates.get_iter()) {
+
+            auto proton = make_shared<TParticle>(ParticleTypeDatabase::Proton, cand_proton);
+
+            TParticleList photons;
+            for(const auto& cand : data.Candidates.get_iter()) {
+                if(cand == cand_proton)
+                    continue;
+                auto photon = make_shared<TParticle>(ParticleTypeDatabase::Photon, cand);
+                photons.emplace_back(photon);
+            }
+
+            kinfitter_sig.SetEgammaBeam(photon_energy);
+            kinfitter_sig.SetProton(proton);
+            kinfitter_sig.SetPhotons(photons);
+
+            auto result = kinfitter_sig.DoFit();
+
+            if(!std_ext::copy_if_greater(r.KinFitProb, result.Probability))
+                continue;
+        }
+
+        results_ant.emplace_back(move(r));
     }
 
+
+    cout << ">>>> Ant:" << endl;
+    for(auto& r : results_ant)
+        cout << r << endl;
+    cout << endl;
 }
 
 void EtapSergey::ShowResult()
