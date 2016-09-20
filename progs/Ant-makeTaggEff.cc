675f8655496dcb9831292df22cc87c09d9ca4e0c
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-09-20 18:36:30 +0200

    makeTaggEff: simpler histogramm handling

--- progs/Ant-makeTaggEff.cc
+++ progs/Ant-makeTaggEff.cc
@@ -11,6 +11,7 @@
 #include "base/std_ext/string.h"
 #include "base/std_ext/system.h"
 #include "base/std_ext/time.h"
+#include "base/WrapTFile.h"
 
 #include "calibration/DataManager.h"
 #include "calibration/modules/TaggEff.h"
@@ -48,12 +49,70 @@ const map<string,TID> startIDs({ {"Setup_2014_07_EPT_Prod", TID(1406592000)},
 
 constexpr double chi2cut_channels = 15.0;
 
-static TH1D* hist_channels(nullptr);
-static TH2D* hist_channels_2d(nullptr);
-static TH2D* hist_channels_2d_errors(nullptr);
-static TGraphErrors* graphTime(nullptr);
 
-static shared_ptr<HistogramFactory> histfac = nullptr;
+struct channelHist_t
+{
+
+    TH1D* Hist = nullptr;
+    shared_ptr<HistogramFactory> HistFac;
+    string Title;
+
+    channelHist_t(const string& title, shared_ptr<HistogramFactory> histfac):
+        HistFac(histfac),
+        Title(title) {}
+    void Fill(const vector<double>& data, const vector<double>& dataErrors)
+    {
+        if ( !Hist )
+            Hist = HistFac->makeTH1D(Title,"channel","Efficiency",BinSettings(data.size()));
+
+        for ( auto ch = 0u ; ch < data.size() ; ++ch)
+        {
+            Hist->Fill(ch,data.at(ch));
+            Hist->SetBinError(ch+1,dataErrors.at(ch)); // bin-numbering ( bin 0 is underflow ) ...
+        }
+    }
+};
+
+struct channelHistTime_t
+{
+
+    TH2D* Hist = nullptr;
+    TH2D* HistErrors = nullptr;
+    TGraphErrors* GraphMeans = nullptr;
+    shared_ptr<HistogramFactory> HistFac;
+    string Title;
+
+    channelHistTime_t(const string& title, shared_ptr<HistogramFactory> histfac):
+        HistFac(histfac),
+        Title(title) {}
+    void Fill(const vector<double>& data, const vector<double>& dataErrors, const unsigned time )
+    {
+        if ( !Hist )
+            Hist = HistFac->makeTH2D(Title,
+                                     "","channel",
+                                     BinSettings(1,0,0),BinSettings(data.size()));
+        if ( !HistErrors )
+            HistErrors = HistFac->makeTH2D(std_ext::formatter() << Title << " relative errors",
+                                           "","channel",
+                                           BinSettings(1,0,0),BinSettings(data.size()));
+        if ( !GraphMeans )
+            GraphMeans = HistFac->make<TGraphErrors>();
+
+        std_ext::RMS mte;
+        std_ext::RMS mtee;
+        for ( auto ch = 0u ; ch < data.size() ; ++ch )
+        {
+            Hist->Fill(std_ext::to_iso8601(time).c_str(), ch,data.at(ch));
+            mte.Add(data.at(ch));
+            HistErrors->Fill(std_ext::to_iso8601(time).c_str(), ch,std::abs( 1.0 * dataErrors.at(ch) / data.at(ch)));
+            mtee.Add(dataErrors.at(ch));
+        }
+        auto N = GraphMeans->GetN();
+        GraphMeans->SetPoint(N,time,mte.GetMean());
+        GraphMeans->SetPointError(N,0,mtee.GetMean());
+    }
+};
+
 
 static volatile bool interrupt = false;
 static bool noStore = false;
@@ -65,17 +124,12 @@ auto failExit = [] (const string& message)
     exit(EXIT_FAILURE);
 };
 
-void fillHistSingle( const vector<double>& data, const vector<double>& dataErrors);
-void fillHistTime( const vector<double>& data, const vector<double>& dataErrors, const unsigned time );
-
-void storeHist(const taggEff_t& result);
-void storeResult(const taggEff_t& result, shared_ptr<calibration::DataManager> manager, const string& calibrationName,
-                 const Calibration::AddMode_t& addMode = Calibration::AddMode_t::RightOpen);
-
-taggEffTriple_t* processFiles(const vector<string>& files);
-void mediateCSV(const vector<string>& csvFiles);
-void processCSV(const string& csvFile);
-void processManualData(const string& dataFile, const string& setupName);
+void storeClibrationData(const taggEff_t& result, shared_ptr<calibration::DataManager> manager, const string& calibrationName,
+                         const Calibration::AddMode_t& addMode = Calibration::AddMode_t::RightOpen);
+taggEffTriple_t* processFiles(const vector<string>& files, shared_ptr<channelHist_t> chHist);
+taggEff_t mediateCSV(const vector<string>& csvFiles);
+void processCSV(const string& csvFile, shared_ptr<channelHistTime_t> chHistTime);
+taggEff_t processManualData(const string& dataFile, const string& setupName);
 
 int main( int argc, char** argv )
 {
@@ -103,11 +157,11 @@ int main( int argc, char** argv )
 
     // other settings:
     auto cmd_setup      = cmd.add<TCLAP::ValueArg<string>>("","setup", "set setup manually",        false, "", "name");
+    auto cmd_output     = cmd.add<TCLAP::ValueArg<string>>("",  "save-hist", "Save results to a histogram", false, "","filename");
 
     //switches
     auto cmd_batchmode  = cmd.add<TCLAP::SwitchArg>("b", "batch",     "Run in batch mode (no ROOT shell afterwards)");
     auto cmd_nostore    = cmd.add<TCLAP::SwitchArg>("n", "nostore",   "don't store, only show results");
-    auto cmd_histout    = cmd.add<TCLAP::SwitchArg>("",  "save-hist", "Save results to a histogram");
 
     //files
     auto cmd_filelist   = cmd.add<TCLAP::UnlabeledMultiArg<string>>("inputfiles","inputfiles to read from",true,"inputfiles");
@@ -129,63 +183,82 @@ int main( int argc, char** argv )
 
     auto fileList = cmd_filelist->getValue();
     noStore = cmd_nostore->isSet();
-    histOut = cmd_histout->isSet();
+    histOut = cmd_output->isSet();
 
+    unique_ptr<WrapTFileOutput> masterFile;
+    if(histOut) {
+        masterFile = std_ext::make_unique<WrapTFileOutput>(cmd_output->getValue(),
+                                                    WrapTFileOutput::mode_t::recreate,
+                                                     true);
+    }
+
+    auto histfac = make_shared<HistogramFactory>("makeTaggEff");
+
+    shared_ptr<channelHistTime_t> chHistCsv = nullptr;
     if (mode_csv->isSet())
     {
-        if (cmd_histout)
         if (fileList.size() != 1)
             failExit("Provide one csv file!");
-        processCSV(fileList.at(0));
+        chHistCsv = make_shared<channelHistTime_t>(fileList.at(0),histfac);
+        processCSV(fileList.at(0),chHistCsv);
     }
 
+    shared_ptr<channelHist_t> chHistMeanCsv;
     if (mode_csv_mean->isSet())
     {
-        if (cmd_histout)
+        if (cmd_output)
         if (fileList.size() < 1)
             failExit("Provide at least one csv file!");
-        mediateCSV(fileList);
+        auto result = mediateCSV(fileList);
+        chHistMeanCsv = make_shared<channelHist_t>(result.Setup,histfac);
+        chHistMeanCsv->Fill(result.TaggEffs,result.TaggEffErrors);
+
     }
 
+    shared_ptr<channelHist_t> chHistGroup;
     taggEffTriple_t* triple_grp = nullptr;
     if (mode_group->isSet())
     {
         if (fileList.size() != 3)
             failExit("Group should contain three files in following order: 1st background, TaggEff-run, background 2.");
-        triple_grp = processFiles(fileList);
+        chHistGroup = make_shared<channelHist_t>(fileList.at(1),histfac);
+        triple_grp = processFiles(fileList,chHistGroup);
     }
 
+    shared_ptr<channelHist_t> chHistManual;
     if (mode_manual->isSet())
     {
         if (!cmd_setup->isSet())
             failExit("Must provide setup name for data.");
         if (fileList.size() != 1)
             failExit("Provide one data file!");
-        processManualData(fileList.at(0),cmd_setup->getValue());
+        auto result = processManualData(fileList.at(0),cmd_setup->getValue());
+        chHistManual = make_shared<channelHist_t>(std_ext::formatter() << result.Setup << " - manual data",histfac);
+        chHistManual->Fill(result.TaggEffs,result.TaggEffErrors);
     }
 
 
+
+    // OUPUT ==============================
 
     argc=1; // prevent TRint to parse any cmdline except prog name
     auto app = cmd_batchmode->isSet() || !std_ext::system::isInteractive()
                ? nullptr
                : std_ext::make_unique<TRint>("Ant-makeSigmas",&argc,argv,nullptr,0,true);
-
     if(app) {
-        canvas c("TaggEff");
-        if (hist_channels)
-            c << drawoption("E") << hist_channels;
-        if (hist_channels_2d)
-            c << drawoption("colz") << hist_channels_2d << endr;
-        if (hist_channels_2d_errors)
-            c << drawoption("colz") << hist_channels_2d_errors << endr;
-        c << endc;
 
-        if ( graphTime )
+        for ( auto i: {chHistMeanCsv,chHistGroup,chHistManual})
         {
-            canvas("means") << graphTime << endc;
-            graphTime->GetXaxis()->SetTimeDisplay(1);
-            graphTime->GetXaxis()->SetTimeFormat("%d.%m.%Y %F 1970-01-01 00:00:00");
+            if (i)
+                canvas("TaggEff") << drawoption("E") << i->Hist << endc;
+        }
+        if (chHistCsv)
+        {
+            canvas("TaggEff") << drawoption("colz") << chHistCsv->Hist << endr
+                              << chHistCsv->HistErrors << endr << endc;
+            canvas("means") << drawoption("AP") << chHistCsv->GraphMeans << endc;
+            chHistCsv->GraphMeans->GetXaxis()->SetTimeDisplay(1);
+            chHistCsv->GraphMeans->GetXaxis()->SetTimeFormat("%d.%m.%Y %F 1970-01-01 00:00:00");
         }
 
         if ( triple_grp )
@@ -211,14 +284,18 @@ int main( int argc, char** argv )
             }
         }
 
+        if(masterFile)
+            LOG(INFO) << "Stopped running, but close ROOT properly to write data to disk.";
         app->Run(kTRUE); // really important to return...
+        if(masterFile)
+            LOG(INFO) << "Writing output file...";
+        masterFile = nullptr;   // and to destroy the master WrapTFile before TRint is destroyed
     }
-
 
     return EXIT_SUCCESS;
 }
 
-void processManualData(const string& dataFile, const string& setupName)
+taggEff_t processManualData(const string& dataFile, const string& setupName)
 {
     ifstream fstream(dataFile);
 
@@ -252,34 +329,30 @@ void processManualData(const string& dataFile, const string& setupName)
     if (ch != nChannels)
         failExit("File contains less entries than tagger-channels in setup.");
 
-    fillHistSingle(result.TaggEffs,result.TaggEffErrors);
-
     if (!noStore)
     {
         auto manager = ExpConfig::Setup::GetLastFound()->GetCalibrationDataManager();
-        storeResult(result,manager,calibration::TaggEff::GetDataName());
+        storeClibrationData(result,manager,calibration::TaggEff::GetDataName(),Calibration::AddMode_t::AsDefault);
     }
-    if ( histOut )
-        storeHist(result);
+
+    return result;
 }
 
-taggEffTriple_t* processFiles(const vector<string>& files)
+taggEffTriple_t* processFiles(const vector<string>& files, shared_ptr<channelHist_t> chHist)
 {
     auto taggEff = new taggEffTriple_t(files.at(0),files.at(1),files.at(2));
     taggEff_t result = taggEff->GetTaggEffSubtracted();
     auto manager = ExpConfig::Setup::GetLastFound()->GetCalibrationDataManager();
 
-    fillHistSingle(result.TaggEffs,result.TaggEffErrors);
+    chHist->Fill(result.TaggEffs,result.TaggEffErrors);
 
     if (!noStore)
-        storeResult(result,manager,calibration::TaggEff::GetDataName());
-    if (histOut)
-        storeHist(result);
+        storeClibrationData(result,manager,calibration::TaggEff::GetDataName());
 
     return taggEff;
 }
 
-void processCSV(const string& csvFile)
+void processCSV(const string& csvFile, shared_ptr<channelHistTime_t> chHistTime)
 {
     shared_ptr<calibration::DataManager> manager = nullptr;
 
@@ -327,19 +400,20 @@ void processCSV(const string& csvFile)
         {
             manager = ExpConfig::Setup::GetLastFound()->GetCalibrationDataManager();
             result.FirstID = it_beamtime->second;
+
         }
         if (n_TaggEffs > 0 && result.Setup != setupName )
                 throw runtime_error("Different Setupnames within file list found!");
 
-        fillHistTime(result.TaggEffs,result.TaggEffErrors,result.FirstID.Timestamp);
+        chHistTime->Fill(result.TaggEffs,result.TaggEffErrors,result.FirstID.Timestamp);
         if (!noStore)
-            storeResult(result,manager,calibration::TaggEff::GetDataName());
+            storeClibrationData(result,manager,calibration::TaggEff::GetDataName());
         setupName = result.Setup;
         n_TaggEffs++;
     }
 }
 
-void mediateCSV(const vector<string>& csvFiles)
+taggEff_t mediateCSV(const vector<string>& csvFiles)
 {
     string setupName;
     size_t n_TaggEffs(0);
@@ -417,79 +491,12 @@ void mediateCSV(const vector<string>& csvFiles)
         tagF.TaggEffs.at(ch)      = 1.0 * vSy.at(ch) / vS.at(ch);
         tagF.TaggEffErrors.at(ch) = sqrt(1.0 / vS.at(ch));
     }
-
-
-    fillHistSingle(tagF.TaggEffs,tagF.TaggEffErrors);
     if (!noStore)
-        storeResult(tagF,manager,calibration::TaggEff::GetDataName());
+        storeClibrationData(tagF,manager,calibration::TaggEff::GetDataName(),Calibration::AddMode_t::AsDefault);
+    return tagF;
 }
 
-void fillHistSingle( const vector<double>& data, const vector<double>& dataErrors)
-{
-    if ( !hist_channels )
-    {
-        hist_channels = new TH1D("taggEff", "Generated Tagging Efficiencies", data.size(),0,data.size());
-        hist_channels->SetXTitle("channel");
-        hist_channels->SetYTitle("Efficiency");
-    }
-
-    for ( auto ch = 0u ; ch < data.size() ; ++ch)
-    {
-        hist_channels->Fill(ch,data.at(ch));
-        hist_channels->SetBinError(ch+1,dataErrors.at(ch)); // bin-numbering ( bin 0 is underflow ) ...
-    }
-}
-
-void fillHistTime( const vector<double>& data, const vector<double>& dataErrors, const unsigned time )
-{
-    if ( !hist_channels_2d )
-    {
-        hist_channels_2d = new TH2D("taggEffTime","Generated Tagging Efficiencies", 1 ,0,0,data.size(),0,data.size());
-        hist_channels_2d->SetXTitle("");
-        hist_channels_2d->SetYTitle("channel");
-    }
-    if ( !hist_channels_2d_errors )
-    {
-        hist_channels_2d_errors = new TH2D("taggEffErrTime","Tagging Efficiencies - relative errors", 1 ,0,0,data.size(),0,data.size());
-        hist_channels_2d_errors->SetXTitle("");
-        hist_channels_2d_errors->SetYTitle("channel");
-    }
-    if ( !graphTime )
-    {
-        graphTime = new TGraphErrors();
-    }
-    std_ext::RMS mte;
-    std_ext::RMS mtee;
-
-    for ( auto ch = 0u ; ch < data.size() ; ++ch )
-    {
-        hist_channels_2d->Fill(std_ext::to_iso8601(time).c_str(), ch,data.at(ch));
-        mte.Add(data.at(ch));
-        hist_channels_2d_errors->Fill(std_ext::to_iso8601(time).c_str(), ch,std::abs( 1.0 * dataErrors.at(ch) / data.at(ch)));
-        mtee.Add(dataErrors.at(ch));
-    }
-    auto N = graphTime->GetN();
-    graphTime->SetPoint(N,time,mte.GetMean());
-    graphTime->SetPointError(N,0,mtee.GetMean());
-
-}
-
-void storeHist( const taggEff_t& result)
-{
-    if (histfac == nullptr)
-        histfac = make_shared<HistogramFactory>(result.Setup);
-
-    auto nCh = result.TaggEffs.size();
-
-    auto hist = histfac->makeTH1D(result.Setup,"channel","#eta",BinSettings(nCh));
-    for ( auto ch = 0u ; ch < nCh ; ++ch )
-    {
-        hist->Fill(ch,result.TaggEffs.at(ch));
-        hist->SetBinError(ch+1,result.TaggEffErrors.at(ch));
-    }
-}
-
-void storeResult(const taggEff_t& result, shared_ptr<calibration::DataManager> manager, const string& calibrationName,
+void storeClibrationData(const taggEff_t& result, shared_ptr<calibration::DataManager> manager, const string& calibrationName,
                  const Calibration::AddMode_t& addMode)
 {
     TCalibrationData cdata(
