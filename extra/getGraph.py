2d6a749840c7b9681f3eb3b2e6d010b7a79cb6ec
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-09-30 12:04:15 +0200

    extra: getGraph.py select function

--- extra/getGraph.py
+++ extra/getGraph.py
@@ -9,18 +9,32 @@ graph = TGraph()
 #scale to taggEff:
 scaleFactor = .58 / .132
 
+yFunction = {
+        ''                    : lambda x: x,
+        'p2Lscaled'           : lambda x: scaleFactor / x,
+        'invert'              : lambda x : 1.0 / x
+}
+
 def saveFill(line):
     try:
-        graph.SetPoint(graph.GetN(),float(line[0]),scaleFactor /  float(line[1].strip('\n')))
+        graph.SetPoint(graph.GetN(),float(line[0]),yFunction['p2Lscaled'](float(line[1].strip('\n'))))
     except:
-        print line
+        print '   Skipping line: ', line
 
 
-if len(sys.argv) !=2:
-    print "need file to convert"
+if not (len(sys.argv)==2 or len(sys.argv)==3):
+    print  'usage: ', sys.argv[0] , 'filename [functionName]'
     exit(1)
 
 filename = sys.argv[1]
+function = ''
+
+if len(sys.argv) == 3: 
+    function = sys.argv[2]
+    if not function in yFunction.keys():
+        print 'Function ', function , ' not in list of functions:  ', yFunction.keys()
+        exit(2)
+
 
 with open(filename) as f:
     map(saveFill,map(lambda line: line.split('\t'),f.readlines()))
