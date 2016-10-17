ed4e7c9183c1b7984dad1fa0fde48678d6493517
Andreas Neiser <neiser@kph.uni-mainz.de>
2016-10-17 13:26:10 +0200

    Progs: Ant-hadd: Add native mode (copied from original ROOT hadd)

--- progs/Ant-hadd.cc
+++ progs/Ant-hadd.cc
@@ -15,6 +15,11 @@
 #include "TFileMergeInfo.h"
 #include "root-addons/analysis_codes/hstack.h"
 
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TInterpreter.h"
+#include "TFileMerger.h"
+
 
 #include <list>
 #include <string>
@@ -142,6 +147,64 @@ void MergeRecursive(TDirectory& target, const sources_t& sources)
     }
 }
 
+void do_nativemode(const string& outputfile, const list<string>& inputfiles) {
+    Bool_t force = kTRUE; // changed from defaults
+    Bool_t skip_errors = kFALSE;
+    Bool_t reoptimize = kFALSE;
+    Bool_t noTrees = kFALSE;
+    Int_t maxopenedfiles = 0;
+    Int_t verbosity = 99;
+    Int_t newcomp = 1; // compression level
+
+
+    gSystem->Load("libTreePlayer");
+    TClass::GetClass("ROOT::Cintex::Cintex"); // autoload Cintex if it exist.
+    if (gInterpreter->IsLoaded("libCintex")) {
+       gROOT->ProcessLine("ROOT::Cintex::Cintex::Enable();");
+    }
+
+    TFileMerger merger(kFALSE,kFALSE);
+    merger.SetMsgPrefix("Ant-hadd");
+    merger.SetPrintLevel(verbosity - 1);
+    if (maxopenedfiles > 0) {
+       merger.SetMaxOpenedFiles(maxopenedfiles);
+    }
+    if (!merger.OutputFile(outputfile.c_str(),force,newcomp) ) {
+       LOG(ERROR) << "hadd error opening target file";
+       exit(EXIT_FAILURE);
+    }
+
+    for ( auto& inputfile : inputfiles ) {
+        if( ! merger.AddFile(inputfile.c_str()) ) {
+          if ( skip_errors ) {
+             LOG(ERROR) << "hadd skipping file with error: " << inputfile << endl;
+          } else {
+             LOG(ERROR) << "hadd exiting due to error in " << inputfile << endl;
+             exit(EXIT_FAILURE);
+          }
+       }
+    }
+    if (reoptimize) {
+       merger.SetFastMethod(kFALSE);
+    } else {
+       if (merger.HasCompressionChange()) {
+          // Don't warn if the user any request re-optimization.
+          LOG(WARNING) <<"hadd Sources and Target have different compression levels"<<endl;
+          LOG(WARNING) <<"hadd merging will be slower"<<endl;
+       }
+    }
+
+    merger.SetNotrees(noTrees);
+    Bool_t status = merger.Merge();
+
+    if(!status) {
+        LOG(ERROR) << "hadd failure during the merge of " << merger.GetMergeList()->GetEntries() << " input files in " << outputfile << ".\n";
+        exit(EXIT_FAILURE);
+    }
+
+    LOG(INFO) << "hadd merged " << merger.GetMergeList()->GetEntries() << " input files in " << outputfile << ".\n";
+}
+
 //___________________________________________________________________________
 int main( int argc, char **argv )
 {
@@ -149,8 +212,13 @@ int main( int argc, char **argv )
    SetupLogger();
 
    TCLAP::CmdLine cmd("Ant-hadd - Merge ROOT objects in files", ' ', "0.1");
+   auto cmd_verbose = cmd.add<TCLAP::ValueArg<int>>("v","verbose","Verbosity level (0..9)", false, 0,"int");
+   auto cmd_nativemode = cmd.add<TCLAP::MultiSwitchArg>("","native","Run native TFileMerger, is slow on large trees",false);
    auto cmd_filenames  = cmd.add<TCLAP::UnlabeledMultiArg<string>>("files","ROOT files, first one is output",true,"ROOT files");
    cmd.parse(argc, argv);
+   if(cmd_verbose->isSet()) {
+       el::Loggers::setVerboseLevel(cmd_verbose->getValue());
+   }
 
    list<string> filenames(cmd_filenames->getValue().begin(), cmd_filenames->getValue().end());
 
@@ -167,9 +235,16 @@ int main( int argc, char **argv )
        exit(EXIT_FAILURE);
    }
 
-   auto outputfile = std_ext::make_unique<TFile>(filenames.front().c_str(), "RECREATE");
+   // important to copy the std::string here
+   const auto outputfilename = filenames.front();
    filenames.pop_front();
 
+   if(cmd_nativemode->isSet()) {
+       do_nativemode(outputfilename, filenames);
+       exit(EXIT_SUCCESS);
+   }
+
+   auto outputfile = std_ext::make_unique<TFile>(outputfilename.c_str(), "RECREATE");
    sources_t sources;
    for(const auto& filename : filenames) {
        sources.emplace_back(std_ext::make_unique<TFile>(filename.c_str(), "READ"));
