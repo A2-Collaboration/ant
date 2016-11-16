22948bff5db8829f443f19edaa3a018e05a1fb5a
Martin Wolfes <wolfes@kph.uni-mainz.de>
2016-11-16 16:34:30 +0100

    production: added comparable single-pion-class

---
+++ src/analysis/physics/production/singlePi0.cc
@@ -0,0 +1,308 @@
+#include "singlePi0.h"
+
+
+#include "expconfig/ExpConfig.h"
+
+#include "utils/combinatorics.h"
+#include "utils/particle_tools.h"
+#include "base/vec/LorentzVec.h"
+
+using namespace std;
+using namespace ant;
+using namespace ant::analysis;
+using namespace ant::analysis::physics;
+
+const singlePi0::named_channel_t singlePi0::signal =
+    {"Pi0Prod",    ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::Pi0_2g)};
+const singlePi0::named_channel_t singlePi0::mainBackground =
+    {"EtaGG",     ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::Eta_2g)};
+const std::vector<singlePi0::named_channel_t> singlePi0::otherBackgrounds =
+{
+    {"Pi0PiPi",      ParticleTypeTreeDatabase::Get(ParticleTypeTreeDatabase::Channel::Pi0_eeg)}
+};
+
+auto reducedChi2 = [](const APLCON::Result_t& ares)
+{
+    return 1. * ares.ChiSquare / ares.NDoF;
+};
+auto getLorentzSumUnfitted = [](const vector<utils::TreeFitter::tree_t>& nodes)
+{
+    vector<TLorentzVector> acc;
+    for ( const auto& node: nodes)
+    {
+        LorentzVec temp({0,0,0},0);
+        for ( const auto& ph: node->Daughters())
+        {
+            temp+=(*(ph->Get().Leave->Particle));
+        }
+        acc.push_back(temp);
+    }
+    return acc;
+};
+auto getLorentzSumFitted = [](const vector<utils::TreeFitter::tree_t>& nodes)
+{
+    vector<TLorentzVector> acc;
+    for ( const auto& node: nodes)
+    {
+        acc.push_back(node->Get().LVSum);
+    }
+    return acc;
+};
+
+singlePi0::singlePi0(const string& name, ant::OptionsPtr opts):
+    Physics(name, opts),
+    phSettings(),
+    kinFitterEMB("fitterEMB", phSettings.nPhotons,   uncertModel, true ),
+    fitterSig("fitterSig", signal.DecayTree,         uncertModel, true )
+{
+    fitterSig.SetZVertexSigma(phSettings.fitter_ZVertex);
+    kinFitterEMB.SetZVertexSigma(phSettings.fitter_ZVertex);
+
+    auto extractS = [] ( vector<utils::TreeFitter::tree_t>& nodes,
+                         const utils::TreeFitter& fitter,
+                         const ParticleTypeDatabase::Type& mother,
+                         const ParticleTypeDatabase::Type& daughterSelection )
+    {
+        const auto head = fitter.GetTreeNode(mother);
+        for (const auto& daughter: head->Daughters())
+        {
+            if (daughter->Get().TypeTree->Get() == daughterSelection)
+                nodes.emplace_back(daughter);
+        }
+    };
+
+    extractS(pionsFitterSig, fitterSig,
+             ParticleTypeDatabase::BeamProton,
+             ParticleTypeDatabase::Pi0);
+
+    const auto setup = ant::ExpConfig::Setup::GetLastFound();
+    if(!setup) {
+        throw std::runtime_error("No Setup found");
+    }
+
+    promptrandom.AddPromptRange(phSettings.Range_Prompt);
+    for ( const auto& range: phSettings.Ranges_Random)
+        promptrandom.AddRandomRange(range);
+
+    hist_steps          = HistFac.makeTH1D("steps","","# evts.",BinSettings(1,0,0),"steps");
+    hist_channels       = HistFac.makeTH1D("channels","","# evts.",BinSettings(1,0,0),"channels");
+    hist_channels_end   = HistFac.makeTH1D("channel-selected","","# evts.",BinSettings(1,0,0),"channels_end");
+
+    tree.CreateBranches(HistFac.makeTTree(phSettings.Tree_Name));
+    tree.photons().resize(phSettings.nPhotons);
+    tree.EMB_photons().resize(phSettings.nPhotons);
+}
+
+void singlePi0::ProcessEvent(const ant::TEvent& event, manager_t&)
+{
+    const auto& data   = event.Reconstructed();
+
+    FillStep("seen");
+
+    tree.CBESum = data.Trigger.CBEnergySum;
+
+    //simulate cb-esum-trigger
+    if (tree.CBESum < phSettings.Cut_CBESum )
+        return;
+    FillStep(std_ext::formatter() << "CB energy sum < " << phSettings.Cut_CBESum);
+
+//    const auto& mcTrue       = event.MCTrue();
+    auto& particleTree = event.MCTrue().ParticleTree;
+    //===================== TreeMatching   ====================================================
+    tree.MCTrue = phSettings.Index_Data;
+    string trueChannel = "Unknown/Data";
+    if (particleTree)
+    {
+        if (particleTree->IsEqual(signal.DecayTree,utils::ParticleTools::MatchByParticleName))
+        {
+            tree.MCTrue = phSettings.Index_Signal;
+            trueChannel = signal.Name;
+        }
+        else if (particleTree->IsEqual(mainBackground.DecayTree,utils::ParticleTools::MatchByParticleName))
+        {
+            tree.MCTrue = phSettings.Index_MainBkg;
+            trueChannel = mainBackground.Name;
+        }
+        else
+        {
+            auto index = phSettings.Index_Offset;
+            bool found = false;
+            for (const auto& otherChannel:otherBackgrounds)
+            {
+                if (particleTree->IsEqual(otherChannel.DecayTree,utils::ParticleTools::MatchByParticleName))
+                {
+                    tree.MCTrue = index;
+                    trueChannel = otherChannel.Name;
+                    found = true;
+                }
+                index++;
+            }
+            if (!found)
+            {
+                tree.MCTrue = phSettings.Index_Unknown;
+                trueChannel = utils::ParticleTools::GetDecayString(particleTree);
+            }
+        }
+
+    }
+    hist_channels->Fill(trueChannel.c_str(),1);
+
+    if ( data.Candidates.size() != phSettings.Cut_NCands )
+        return;
+    FillStep(std_ext::formatter() << "N candidates == " << phSettings.Cut_NCands);
+
+    unique_ptr<protonSelection_t> bestSelection;
+
+    //===================== Reconstruction ====================================================
+    tree.CBAvgTime = data.Trigger.CBTiming;
+
+    auto bestProb_EMB  = 0.;
+    for ( const auto& taggerHit: data.TaggerHits )
+    {
+        FillStep("seen taggerhits");
+
+        promptrandom.SetTaggerHit(taggerHit.Time - tree.CBAvgTime);
+        if (promptrandom.State() == PromptRandom::Case::Outside)
+            continue;
+
+        tree.Tagg_Ch = static_cast<unsigned>(taggerHit.Channel);
+        tree.Tagg_E  = taggerHit.PhotonEnergy;
+        tree.Tagg_W  = promptrandom.FillWeight();
+
+        auto temp_EMB_prob = -std_ext::inf;
+        for ( auto i_proton: data.Candidates.get_iter())
+        {
+            const protonSelection_t selection(i_proton, data.Candidates,
+                                              taggerHit.GetPhotonBeam(),
+                                              taggerHit.PhotonEnergy);
+
+            if (!phSettings.Cut_ProtonCopl.Contains(selection.Copl_pg))
+                continue;
+            FillStep(std_ext::formatter() << "proton-photons coplanarity in " << phSettings.Cut_ProtonCopl);
+
+            if ( !(phSettings.Cut_MM.Contains(selection.Proton_MM.M())))
+                continue;
+            FillStep(std_ext::formatter() << "MM(proton) in " << phSettings.Cut_MM);
+
+            if ( phSettings.Cut_MMAngle < (selection.Angle_pMM))
+                continue;
+            FillStep(std_ext::formatter() << "angle(MM,proton) > " << phSettings.Cut_MMAngle);
+
+            kinFitterEMB.SetProton(selection.Proton);
+            kinFitterEMB.SetPhotons(selection.Photons);
+            kinFitterEMB.SetEgammaBeam(selection.Tagg_E);
+
+            auto EMB_result = kinFitterEMB.DoFit();
+            if (!(EMB_result.Status == APLCON::Result_Status_t::Success))
+                continue;
+
+            if ( EMB_result.Probability > temp_EMB_prob)
+            {
+                tree.SetRaw(selection);
+                tree.SetEMB(kinFitterEMB,EMB_result);
+                bestSelection = std_ext::make_unique<protonSelection_t>(selection);
+                std_ext::copy_if_greater(bestProb_EMB,EMB_result.Probability);
+            }
+            FillStep(std_ext::formatter() << "EMB-Fit: prob. not in " << phSettings.Cut_EMB_Chi2);
+        } // proton
+
+        // cut on EMB
+        if (!(phSettings.Cut_EMB_Chi2.Contains(tree.EMB_chi2)))
+            continue;
+        // fit worked at all?
+        if (!bestSelection )
+            continue;
+        FillStep(std_ext::formatter() << "proton-selection passed");
+
+        auto applyTreeFit = [&bestSelection](utils::TreeFitter& fitter,
+                                             const std::vector<utils::TreeFitter::tree_t>& intermediates)
+        {
+            fitter.SetProton( bestSelection->Proton);
+            fitter.SetPhotons(bestSelection->Photons);
+            fitter.SetEgammaBeam(bestSelection->Tagg_E);
+            APLCON::Result_t result;
+            auto best_prob = std_ext::NaN;
+            fitRatings_t fr(0,0,0,{});
+            while(fitter.NextFit(result))
+                if (   (result.Status    == APLCON::Result_Status_t::Success)
+                       && (std_ext::copy_if_greater(best_prob,result.Probability)))
+                {
+                    fr = fitRatings_t(best_prob,reducedChi2(result),result.NIterations,
+                                      getLorentzSumUnfitted(intermediates));
+                }
+
+            return fr;
+        };
+
+        tree.SetSIG(applyTreeFit(fitterSig,pionsFitterSig));
+
+        tree.Tree->Fill();
+        hist_channels_end->Fill(trueChannel.c_str(),1);
+    } // taggerHits
+}
+
+void singlePi0::ShowResult()
+{
+    const auto colz = drawoption("colz");
+
+    canvas("summary") << hist_steps
+                      << hist_channels
+                      << hist_channels_end
+                      << TTree_drawable(tree.Tree,"IM2g")
+                      << TTree_drawable(tree.Tree,"EMB_chi2")
+                      << samepad
+                      << TTree_drawable(tree.Tree,"SIG_chi2")
+                      << endc;
+
+    auto c = canvas("SIG - cuts");
+    for (auto chi2: {1.,5.,10.,15.,20.,30.,40.})
+        c << TTree_drawable(tree.Tree,"MCTrue",(std_ext::formatter() << "SIG_chi2 < " << chi2));
+    c << colz
+      << TTree_drawable(tree.Tree,"MCTrue:SIG_chi2>>h(100,0,15,18,0,18)")
+      << endc;
+
+    canvas("pions") << colz
+                    << TTree_drawable(tree.Tree,"EMB_proton.Theta() * 180/3.1415:EMB_proton.E()-938.3>>ht(100,0,1000,100,0,80)")
+                    << TTree_drawable(tree.Tree,"SIG_pions.M()")
+                    << endc;
+}
+
+void singlePi0::PionProdTree::SetRaw(const singlePi0::protonSelection_t& selection)
+{
+    proton = *selection.Proton;
+    photons() = MakeTLorenz(selection.Photons);
+    photonSum = selection.PhotonSum;
+    IM2g = photonSum().M();
+    proton_MM =   selection.PhotonBeam
+                + LorentzVec({0, 0, 0}, ParticleTypeDatabase::Proton.Mass())
+                - selection.PhotonSum;
+    pg_copl   = std_ext::radian_to_degree(vec2::Phi_mpi_pi(selection.Proton->Phi()-selection.PhotonSum.Phi() - M_PI ));
+    pMM_angle = std_ext::radian_to_degree(proton_MM().Angle(selection.Proton->p));
+}
+
+
+
+void singlePi0::PionProdTree::SetEMB(const utils::KinFitter& kF, const APLCON::Result_t& result)
+{
+
+    EMB_proton = *(kF.GetFittedProton());
+    const auto fittedPhotons = kF.GetFittedPhotons();
+    EMB_photons = MakeTLorenz(fittedPhotons);
+    EMB_photonSum = accumulate(EMB_photons().begin(),EMB_photons().end(),TLorentzVector(0,0,0,0));
+    EMB_IM2g = EMB_photonSum().M();
+    EMB_Ebeam  = kF.GetFittedBeamE();
+    EMB_iterations = result.NIterations;
+    EMB_prob = result.Probability;
+    EMB_chi2 = reducedChi2(result);
+
+}
+
+void singlePi0::PionProdTree::SetSIG(const singlePi0::fitRatings_t& fitRating)
+{
+    SIG_prob = fitRating.Prob;
+    SIG_chi2 = fitRating.Chi2;
+    SIG_iterations = fitRating.Niter;
+    SIG_pions = fitRating.Intermediates;
+}
+
+AUTO_REGISTER_PHYSICS(singlePi0)
---
+++ src/analysis/physics/production/singlePi0.h
@@ -0,0 +1,235 @@
+#pragma once
+
+#include "analysis/utils/Fitter.h"
+#include "analysis/physics/Physics.h"
+#include "analysis/plot/PromptRandomHist.h"
+#include "utils/A2GeoAcceptance.h"
+
+#include "base/WrapTTree.h"
+
+#include "TLorentzVector.h"
+
+class TH1D;
+
+namespace ant {
+namespace analysis {
+namespace physics {
+
+struct singlePi0 :  Physics {
+
+    //===================== Settings   ========================================================
+
+
+    struct settings_t
+    {
+        const std::string Tree_Name = "tree";
+
+        const unsigned nPhotons = 2;
+
+        const double    Cut_CBESum     = 550;
+        const unsigned  Cut_NCands     = 3;
+        const IntervalD Cut_ProtonCopl = {-25,25};
+        const IntervalD Cut_MM         = {850,1026};
+        const double    Cut_MMAngle    = 20;
+        const IntervalD Cut_EMB_Chi2    = {0.,40.};
+
+        const IntervalD              Range_Prompt  =   { -5,  5};
+        const std::vector<IntervalD> Ranges_Random = { {-55,-10},
+                                                       { 10, 55}  };
+
+        const double fitter_ZVertex = 3;
+
+        const unsigned Index_Data    = 0;
+        const unsigned Index_Signal  = 1;
+        const unsigned Index_MainBkg = 2;
+        const unsigned Index_Offset  = 10;
+        const unsigned Index_Unknown = 9;
+
+    };
+
+    const settings_t phSettings;
+
+    ant::analysis::utils::A2SimpleGeometry geometry;
+
+    //===================== Channels   ========================================================
+
+    struct named_channel_t
+    {
+        const std::string Name;
+        const ParticleTypeTree DecayTree;
+        named_channel_t(const std::string& name, ParticleTypeTree tree):
+            Name(name),
+            DecayTree(tree){}
+    };
+
+    static const named_channel_t              signal;
+    static const named_channel_t              mainBackground;
+    static const std::vector<named_channel_t> otherBackgrounds;
+
+    //===================== Histograms ========================================================
+
+    TH1D* hist_steps             = nullptr;
+    TH1D* hist_channels          = nullptr;
+    TH1D* hist_channels_end      = nullptr;
+
+    //===================== KinFitting ========================================================
+
+
+    std::shared_ptr<utils::UncertaintyModel> uncertModel = std::make_shared<utils::UncertaintyModels::FitterSergey>();
+
+    utils::KinFitter kinFitterEMB;
+
+    utils::TreeFitter fitterSig;
+    std::vector<utils::TreeFitter::tree_t> pionsFitterSig;
+
+    //========================  ProptRa. ============================================================
+
+    ant::analysis::PromptRandom::Switch promptrandom;
+
+    //========================  Storage  ============================================================
+    struct protonSelection_t
+    {
+        TParticlePtr   Proton;
+        TParticleList  Photons;
+        LorentzVec     PhotonSum;
+        LorentzVec     Proton_MM;
+        LorentzVec     PhotonBeam;
+        double         Copl_pg;
+        double         Angle_pMM;
+        double         Tagg_E;
+
+        template<typename wtf_ITER>
+        protonSelection_t(const wtf_ITER& selectedProton, const TCandidateList& candidates,
+                          const LorentzVec& photonBeam,   double taggE):
+            PhotonSum({0,0,0},0),
+            PhotonBeam(photonBeam),
+            Tagg_E(taggE)
+        {
+            Proton = std::make_shared<TParticle>(ParticleTypeDatabase::Proton, selectedProton);
+            for ( auto i_photon : candidates.get_iter())
+                if (!(i_photon == selectedProton))
+                {
+                    Photons.emplace_back(std::make_shared<TParticle>(ParticleTypeDatabase::Photon, i_photon));
+                    PhotonSum += *Photons.back();
+                }
+            Proton_MM =   photonBeam
+                        + LorentzVec({0, 0, 0}, ParticleTypeDatabase::Proton.Mass())
+                        - PhotonSum;
+            Copl_pg   =   std_ext::radian_to_degree(vec2::Phi_mpi_pi(Proton->Phi()-PhotonSum.Phi() - M_PI ));
+            Angle_pMM =   std_ext::radian_to_degree(Proton_MM.Angle(Proton->p));
+        }
+    };
+
+    struct fitRatings_t
+    {
+        double Prob;
+        double Chi2;
+        int    Niter;
+        std::vector<TLorentzVector> Intermediates;
+        fitRatings_t(double prob,double chi2,int niter,
+                     const std::vector<TLorentzVector> intermediates):
+            Prob(prob),Chi2(chi2),Niter(niter),Intermediates(intermediates){}
+    };
+
+    struct PionProdTree : WrapTTree
+    {
+        struct particleStorage_t
+        {
+            TParticlePtr   Proton;
+            TParticleList  Photons;
+            LorentzVec     PhotonSum;
+            particleStorage_t():
+                Proton(std::make_shared<TParticle>(ParticleTypeDatabase::Proton,LorentzVec({0,0,0},0))),
+                Photons(TParticleList()),
+                PhotonSum({0,0,0},0){}
+            particleStorage_t(const TParticlePtr& proton,
+                              const TParticleList& photons, const LorentzVec& photonSum):
+                Proton(proton),
+                Photons(photons),PhotonSum(photonSum){}
+        };
+
+        // type: 0   data
+        //       1   signal (3pi0)
+        //       2   mainBkg(eta->3pi0)
+        //       10+ otherBkg
+        ADD_BRANCH_T(unsigned, MCTrue)
+
+        ADD_BRANCH_T(double,   Tagg_W)
+        ADD_BRANCH_T(unsigned, Tagg_Ch)
+        ADD_BRANCH_T(double,   Tagg_E)
+
+        ADD_BRANCH_T(double, CBAvgTime)
+        ADD_BRANCH_T(double, CBESum)
+
+        // best emb combination raw
+        ADD_BRANCH_T(TLorentzVector,              proton)
+        ADD_BRANCH_T(std::vector<TLorentzVector>, photons)
+        ADD_BRANCH_T(TLorentzVector,              photonSum)
+        ADD_BRANCH_T(double,                      IM2g)
+
+        ADD_BRANCH_T(TLorentzVector,              proton_MM)
+        ADD_BRANCH_T(double,                      pMM_angle)
+        ADD_BRANCH_T(double,                      pg_copl)
+        void SetRaw(const singlePi0::protonSelection_t& selection);
+
+        // best emb comb. emb-fitted
+        ADD_BRANCH_T(TLorentzVector,              EMB_proton)
+        ADD_BRANCH_T(std::vector<TLorentzVector>, EMB_photons)
+        ADD_BRANCH_T(TLorentzVector,              EMB_photonSum)
+        ADD_BRANCH_T(double,                      EMB_IM2g)
+        ADD_BRANCH_T(double,                      EMB_Ebeam)
+
+        ADD_BRANCH_T(double,                      EMB_prob)
+        ADD_BRANCH_T(double,                      EMB_chi2)
+        ADD_BRANCH_T(int,                         EMB_iterations)
+        void SetEMB(const utils::KinFitter& kF, const APLCON::Result_t& result);
+
+        //best tree-fit combination raw
+        ADD_BRANCH_T(double,                        SIG_prob)
+        ADD_BRANCH_T(double,                        SIG_chi2)
+        ADD_BRANCH_T(int,                           SIG_iterations)
+        ADD_BRANCH_T(std::vector<TLorentzVector>,   SIG_pions)
+        void SetSIG(const singlePi0::fitRatings_t&  fitRating);
+    };
+    PionProdTree tree;
+
+    //========================  MAIN     ============================================================
+
+    singlePi0(const std::string& name, OptionsPtr opts);
+    virtual void ProcessEvent(const TEvent& event, manager_t& manager) override;
+    virtual void Finish() override {}
+    virtual void ShowResult() override;
+
+    //========================  TOOLS    ============================================================
+
+    template<typename wtf_ITER>
+    PionProdTree::particleStorage_t makeProtonSelection(const wtf_ITER& selectedProton, const TCandidateList& candidates)
+    {
+        const auto proton = std::make_shared<TParticle>(ParticleTypeDatabase::Proton, selectedProton);
+        TParticleList photons;
+        LorentzVec photonSum({0,0,0},0);
+        for ( auto i_photon : candidates.get_iter())
+            if (!(i_photon == selectedProton))
+            {
+                photons.emplace_back(std::make_shared<TParticle>(ParticleTypeDatabase::Photon, i_photon));
+                photonSum += *photons.back();
+            }
+        return PionProdTree::particleStorage_t(proton,photons,photonSum);
+    }
+
+
+
+    static std::vector<TLorentzVector> MakeTLorenz(const TParticleList& particles)
+    {
+        std::vector<TLorentzVector> lg(particles.size());
+        std::transform(particles.begin(),particles.end(),lg.begin(),
+                       [](const TParticlePtr& ph){return TLorentzVector(*ph);});
+        return lg;
+    }
+
+    void FillStep(const std::string& step) {hist_steps->Fill(step.c_str(),1);}
+
+};
+
+
+}}}
