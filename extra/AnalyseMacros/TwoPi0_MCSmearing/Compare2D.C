23b436d44f78bb10a4898903303370738be18a94
Oliver Steffen <steffeo@uni-mainz.de>
2016-11-29 13:36:41 +0100

    AnalyseMacros: TwoPi0_MCSmearing: Compare2D.C: parameter added: folder to analyse

--- extra/AnalyseMacros/TwoPi0_MCSmearing/Compare2D.C
+++ extra/AnalyseMacros/TwoPi0_MCSmearing/Compare2D.C
@@ -1,13 +1,13 @@
 // root <mc.root.a.root> <data.root.a.root> Compate2D.C
-{
+void Compare2D(const char* folder) {
 	TDirectory* dmc;
-	_file0->GetObject("ETheta", dmc);
+	_file0->GetObject(folder, dmc);
 
 
 	TDirectory* dd;
-	_file1->GetObject("ETheta", dd);
+	_file1->GetObject(folder, dd);
 
-	ant::TwoPi0_MCSmearing_Tool::CompareMCData2D(_file0, _file1, "ETheta");
+	ant::TwoPi0_MCSmearing_Tool::CompareMCData2D(_file0, _file1, folder);
 
 	TH2* sigma;
 	gDirectory->GetObject("datamc_simga", sigma);
