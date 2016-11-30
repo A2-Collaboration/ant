e107f21000ddc56637b3b665b6b3140bbd3ed1ac
Oliver Steffen <steffeo@uni-mainz.de>
2016-11-30 16:00:09 +0100

    analyse macros: TwoPi0_MCsmearing: Fix Comape2D.C

--- extra/AnalyseMacros/TwoPi0_MCSmearing/Compare2D.C
+++ extra/AnalyseMacros/TwoPi0_MCSmearing/Compare2D.C
@@ -1,13 +1,13 @@
 // root <mc.root.a.root> <data.root.a.root> Compate2D.C
 void Compare2D(const char* folder) {
 	TDirectory* dmc;
-	_file0->GetObject(folder, dmc);
+	_file1->GetObject(folder, dmc);
 
 
 	TDirectory* dd;
-	_file1->GetObject(folder, dd);
+	_file0->GetObject(folder, dd);
 
-	ant::TwoPi0_MCSmearing_Tool::CompareMCData2D(_file0, _file1, folder);
+	ant::TwoPi0_MCSmearing_Tool::CompareMCData2D(_file1, _file0, folder);
 
 	TH2* sigma;
 	gDirectory->GetObject("datamc_simga", sigma);
