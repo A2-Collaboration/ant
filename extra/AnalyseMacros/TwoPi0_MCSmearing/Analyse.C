53b5362811b272e0b6852a65c294b93dc1023994
Oliver Steffen <steffeo@uni-mainz.de>
2016-12-06 17:31:06 +0100

    extra: TwoPi0_MCSmearing macros: comment out per element analysis

--- extra/AnalyseMacros/TwoPi0_MCSmearing/Analyse.C
+++ extra/AnalyseMacros/TwoPi0_MCSmearing/Analyse.C
@@ -10,34 +10,34 @@
 		cerr << "Hist not found!" << endl;
 	}
 
-	TH3* h_EElement;
-	_file0->GetObject("TwoPi0_MCSmearing/m2Pi0/cb_pi0_E_Element", h_EElement);
-	if(!h_EElement) {
-		cerr << "Hist not found!" << endl;
-	}
+//	TH3* h_EElement;
+//	_file0->GetObject("TwoPi0_MCSmearing/m2Pi0/cb_pi0_E_Element", h_EElement);
+//	if(!h_EElement) {
+//		cerr << "Hist not found!" << endl;
+//	}
 
 	
-	TH2* h_Element;
-	_file0->GetObject("TwoPi0_MCSmearing/m2Pi0/cb_pi0", h_Element);
-	if(!h_Element) {
-		cerr << "Hist not found!" << endl;
-	}
+//	TH2* h_Element;
+//	_file0->GetObject("TwoPi0_MCSmearing/m2Pi0/cb_pi0", h_Element);
+//	if(!h_Element) {
+//		cerr << "Hist not found!" << endl;
+//	}
 
 	TFile* f = new TFile(Form("%s.a.root", _file0->GetName()), "recreate");
 
 	TDirectory* base = gDirectory;
 	
-	TDirectory* d = base->mkdir("Element");
-	d->cd();
-	ant::TwoPi0_MCSmearing_Tool::FitAllChannels(h_Element);
+//	TDirectory* d = base->mkdir("Element");
+//	d->cd();
+//	ant::TwoPi0_MCSmearing_Tool::FitAllChannels(h_Element);
 
 	d = base->mkdir("ETheta");
 	d->cd();
 	ant::TwoPi0_MCSmearing_Tool::AnalyseChannelE(h_ETheta);
 
-	d = base->mkdir("EElement");
-	d->cd();
-	ant::TwoPi0_MCSmearing_Tool::AnalyseChannelE(h_EElement);
+//	d = base->mkdir("EElement");
+//	d->cd();
+//	ant::TwoPi0_MCSmearing_Tool::AnalyseChannelE(h_EElement);
 	base->cd();
 
 	f->Write();
