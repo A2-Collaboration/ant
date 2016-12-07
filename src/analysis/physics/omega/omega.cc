99e89e58ed8650c02ebd653a77056ab0aa02ae3c
Oliver Steffen <steffeo@uni-mainz.de>
2016-12-07 13:42:34 +0100

    analysis: omega: improved OmegaMCTruePlots detector plot

--- src/analysis/physics/omega/omega.cc
+++ src/analysis/physics/omega/omega.cc
@@ -1181,8 +1181,10 @@ TH1* OmegaMCTruePlots::CBTAPS_Distribution::getMulti(const unsigned n)
 void OmegaMCTruePlots::CBTAPS_Distribution::Fill(const TParticleList& particles)
 {
     auto hist = getMulti(particles.size());
-    const auto nCB = count_if(particles.begin(), particles.end(), [] (const TParticlePtr& p) { return degree_to_radian(interval<double>(20.0, 160.0)).Contains(p->Theta()); });
-    hist->Fill(nCB);
+    const auto nCB   = count_if(particles.begin(), particles.end(), [] (const TParticlePtr& p) { return degree_to_radian(interval<double>(20.0, 160.0)).Contains(p->Theta()); });
+    const auto nTAPS = count_if(particles.begin(), particles.end(), [] (const TParticlePtr& p) { return degree_to_radian(interval<double>(5.0, 20.0)).Contains(p->Theta()); });
+    if(nTAPS+nCB == particles.size())
+        hist->Fill(nTAPS);
 }
 
 
