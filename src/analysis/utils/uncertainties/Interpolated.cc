922fb9ca9efedb5921c5b9be4c47d7971f638986
Oliver Steffen <steffeo@uni-mainz.de>
2017-01-23 15:27:18 +0100

    analysis: Interpolated model: switch to base/ClippedInterpolator

--- src/analysis/utils/uncertainties/Interpolated.cc
+++ src/analysis/utils/uncertainties/Interpolated.cc
@@ -7,6 +7,7 @@
 #include "base/std_ext/memory.h"
 #include "base/WrapTFile.h"
 #include "base/Logger.h"
+#include "base/ClippedInterpolatorWrapper.h"
 
 #include "TAxis.h"
 #include "TH2D.h"
@@ -95,17 +96,6 @@ std::vector<double> getBinPositions(const TAxis* axis) {
     return bins;
 }
 
-struct OrderedGrid2D {
-    vector<double> x;
-    vector<double> y;
-    Array2D        z;
-
-    OrderedGrid2D(const unsigned nx, const unsigned ny, const double dflt=0.0):
-        x(nx),
-        y(ny),
-        z(nx, ny, dflt) {}
-
-};
 
 std::vector<double> getBinContents(const TH2D* hist) {
     const auto nx = hist->GetNbinsX();
@@ -125,72 +115,6 @@ std::vector<double> getBinContents(const TH2D* hist) {
     return z;
 }
 
-std::unique_ptr<const Interpolator2D> makeInterpolator(TH2D* hist) {
-
-    const unsigned nx = unsigned(hist->GetNbinsX());
-    const unsigned pad_x = nx > 3 ? 1 : 2;
-
-    const unsigned ny = unsigned(hist->GetNbinsY());
-    const unsigned pad_y = ny > 3 ? 1 : 2;
-
-    OrderedGrid2D grid(nx+pad_x*2, ny+pad_y*2, std_ext::NaN);
-
-    // extend x bin positions
-    {
-        double avgBinWidth = .0;
-        for(unsigned x=0; x<nx; ++x) {
-            grid.x.at(x+pad_x) = hist->GetXaxis()->GetBinCenter(int(x+1));
-            avgBinWidth += hist->GetXaxis()->GetBinWidth(int(x+1));
-        }
-        avgBinWidth /= nx;
-        for(unsigned x=1; x<=pad_x;++x) {
-            grid.x.at(pad_x-x)    = grid.x.at(pad_x-x+1)    - avgBinWidth;
-            grid.x.at(pad_x+nx+x-1) = grid.x.at(pad_x+nx+x-2) + avgBinWidth;
-        }
-    }
-
-    // eytend y bin positions
-    {
-        double avgBinWidth = .0;
-        for(unsigned y=0; y<ny; ++y) {
-            grid.y.at(y+pad_y) = hist->GetYaxis()->GetBinCenter(int(y+1));
-            avgBinWidth += hist->GetYaxis()->GetBinWidth(int(y+1));
-        }
-        avgBinWidth /= ny;
-        for(unsigned y=1; y<=pad_y;++y) {
-            grid.y.at(pad_y-y)    = grid.y.at(pad_y-y+1)    - avgBinWidth;
-            grid.y.at(pad_y+ny+y-1) = grid.y.at(pad_y+ny+y-2) + avgBinWidth;
-        }
-    }
-
-    // copy data to the "middle". leave some borders around
-    grid.z.CopyRect(Array2D_TH2D(hist), pad_x, pad_y);
-
-
-
-    // fill borders:
-
-    // top and botton rows
-    for(unsigned y=1; y<=pad_y; ++y ) {
-        for(unsigned x=0; x<nx; ++x) {
-            grid.z.at(x+pad_x,pad_y-y)       = grid.z.at(x+pad_x,pad_y-y+1);
-            grid.z.at(x+pad_x,pad_y+ny+y-1)    = grid.z.at(x+pad_x,pad_y+ny+y-2);
-        }
-    }
-
-    // first and last column
-    for(unsigned x=1; x<=pad_x; ++x ) {
-        for(unsigned y=0; y<ny; ++y) {
-            grid.z.at(pad_x-x,   y+pad_y)      = grid.z.at(pad_x-x+1,   y+pad_y);
-            grid.z.at(pad_x+nx+x-1,y+pad_y)    = grid.z.at(pad_x+nx+x-2,y+pad_y);
-        }
-    }
-
-    // flood fill averages the rest
-    FloodFillAverages::fillNeighborAverages(grid.z);
-
-    return std_ext::make_unique<Interpolator2D>(grid.x,grid.y, grid.z.Data());
-}
 
 void Interpolated::LoadSigmas(const string& filename)
 {
@@ -255,7 +179,7 @@ std::unique_ptr<const Interpolator2D> Interpolated::LoadInterpolator(const WrapT
     if(!h)
         throw(std::runtime_error("Histogram not found: "+hname));
 
-    return makeInterpolator(h);
+    return ClippedInterpolatorWrapper::makeInterpolator(h);
 
 }
 
@@ -316,59 +240,5 @@ ostream& Interpolated::EkRxyPhiL::Print(ostream& stream) const
     stream << "Phi:\t\t"       << Phi   << "\n";
     stream << "TAPS_L:\t\t"    << TAPS_L  << "\n";
     stream << "ShowerDepth:\t" << ShowerDepth  << "\n";
-    return stream;
-}
-
-
-Interpolated::ClippedInterpolatorWrapper::ClippedInterpolatorWrapper(Interpolated::ClippedInterpolatorWrapper::interpolator_ptr_t i):
-    interp(move(i)), xrange(interp->getXRange()), yrange(interp->getYRange())
-{}
-
-Interpolated::ClippedInterpolatorWrapper::ClippedInterpolatorWrapper(): interp(nullptr), xrange({0,0}), yrange({0,0})
-{}
-
-Interpolated::ClippedInterpolatorWrapper::~ClippedInterpolatorWrapper()
-{}
-
-double Interpolated::ClippedInterpolatorWrapper::GetPoint(double x, double y) const
-{
-    x = xrange.clip(x);
-    y = yrange.clip(y);
-    return interp->GetPoint(x,y);
-}
-
-void Interpolated::ClippedInterpolatorWrapper::setInterpolator(Interpolated::ClippedInterpolatorWrapper::interpolator_ptr_t i) {
-    interp = move(i);
-    xrange = interp->getXRange();
-    yrange = interp->getYRange();
-}
-
-ostream& Interpolated::ClippedInterpolatorWrapper::Print(ostream& stream) const
-{
-    stream << "Theta Range: " << xrange << " E Range: " << yrange;
-    return stream;
-}
-
-
-double Interpolated::ClippedInterpolatorWrapper::boundsCheck_t::clip(double v) const
-{
-    if(v < range.Start()) {
-        underflow++;
-        return range.Start();
-    }
-
-    if(v > range.Stop()) {
-        overflow++;
-        return range.Stop();
-    }
-
-    unclipped++;
-
-    return v;
-}
-
-ostream& Interpolated::ClippedInterpolatorWrapper::boundsCheck_t::Print(ostream& stream) const
-{
-    stream << range << "-> [" << underflow << "|" << unclipped << "|" << overflow << "]";
     return stream;
 }
--- src/analysis/utils/uncertainties/Interpolated.h
+++ src/analysis/utils/uncertainties/Interpolated.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "analysis/utils/Uncertainties.h"
+#include "base/ClippedInterpolatorWrapper.h"
 
 namespace ant {
 
@@ -33,38 +34,6 @@ public:
 
     static std::shared_ptr<Interpolated> makeAndLoad(UncertaintyModelPtr default_model = nullptr,
                                                      bool use_proton_sigmaE = false);
-
-    struct ClippedInterpolatorWrapper : ant::printable_traits {
-        using interpolator_ptr_t = std::unique_ptr<const Interpolator2D>;
-
-        interpolator_ptr_t interp;
-
-        struct boundsCheck_t : ant::printable_traits {
-            interval<double> range;
-            mutable unsigned underflow = 0;
-            mutable unsigned unclipped = 0;
-            mutable unsigned overflow  = 0;
-
-            double clip(double v) const;
-
-            boundsCheck_t(const interval<double> r): range(r) {}
-
-            std::ostream& Print(std::ostream& stream) const override;
-        };
-
-        boundsCheck_t xrange;
-        boundsCheck_t yrange;
-
-        ClippedInterpolatorWrapper(interpolator_ptr_t i);
-        ClippedInterpolatorWrapper();
-        ~ClippedInterpolatorWrapper();
-        double GetPoint(double x, double y) const;
-
-        void setInterpolator(interpolator_ptr_t i);
-
-        std::ostream& Print(std::ostream& stream) const override;
-
-    };
 
     std::ostream& Print(std::ostream& stream) const override;
 
