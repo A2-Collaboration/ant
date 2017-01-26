a66e2922578eb9fe1d1b2181aedf3e8816e1062c
Andreas Neiser <andreas.neiser@gmail.com>
2017-01-26 09:28:21 +0100

    Extra: Make map_and_reduce work on blaster (outdated split), improve tmpfile handling

--- extra/map_and_reduce.sh
+++ extra/map_and_reduce.sh
@@ -6,41 +6,50 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-function tryget() { ${1} || echo ${2}; }
+function tryget() { ${1} 2>/dev/null || echo ${2}; }
 
 JOBS=$(tryget nproc 1)
 COMMAND=$1
 OUT=$2
 PREFIX="__MAPREDUCE"
-JOBFILES=$(eval echo ${PREFIX}{00..$(($JOBS -1))})
-
 PARALLEL=$(tryget "which parallel" "")
 
-echo "Splitting files into ${JOBS} groups"
-split -d -a 2 --number=r/${JOBS} - ${PREFIX}
+
+WORK_DIR=`mktemp -d -p .`
+echo $WORK_DIR
+
+STDIN=$(cat)
+
+NFILES=$(echo "$STDIN" | wc -l)
+NFILES_PER_JOB=$(($NFILES/$JOBS+1))
+
+echo "Splitting $NFILES files into ${JOBS} groups, $NFILES_PER_JOB each"
+
+cd $WORK_DIR
+echo "$STDIN" | split -d -a 2 -l $NFILES_PER_JOB - ${PREFIX}
+JOBFILES=$(ls -1 $PREFIX*)
+cd ..
 
 echo "Creating chains..."
 for i in $JOBFILES;
 do
-    Ant-chain --ignoretreeevents --nomacro -o chain_${i}.root $(cat ${i} | xargs)
+    Ant-chain --ignoretreeevents --nomacro -o $WORK_DIR/chain_${i}.root $(cat $WORK_DIR/${i} | xargs)
 done
 
 echo "Running ${COMMAND} on all chains..."
 
 if [ $PARALLEL ]; then
-    echo $JOBFILES | tr ' ' '\n' | parallel --results mapreduce_logs $COMMAND -i chain_{}.root -o out_{}.root
+    echo $JOBFILES | tr ' ' '\n' | parallel --results mapreduce_logs $COMMAND -i $WORK_DIR/chain_{}.root -o $WORK_DIR/out_{}.root
 else
     echo "GNU Parallel not found. Falling back to just backgrounding the jobs."
     for f in $JOBFILES; do
-       $COMMAND -i chain_${f}.root -o out_${f}.root &
+       $COMMAND -i $WORK_DIR/chain_${f}.root -o $WORK_DIR/out_${f}.root &
     done
     wait
 fi
 
 echo "Merging results..."
-Ant-hadd $OUT $(for i in $JOBFILES; do echo out_$i.root; done | xargs)
+Ant-hadd $OUT $(for i in $JOBFILES; do echo $WORK_DIR/out_$i.root; done | xargs)
 
-rm $JOBFILES
-rm $(for i in $JOBFILES; do echo out_$i.root; done | xargs)
-rm $(for i in $JOBFILES; do echo chain_$i.root; done | xargs)
-
+echo "Cleaning up..."
+rm -rfv $WORK_DIR
