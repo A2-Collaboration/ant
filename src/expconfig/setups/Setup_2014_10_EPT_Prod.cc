e779698c8834eff79a1f7e36681e7a7807de3579
Oliver Steffen <steffen@kph.uni-mainz.de>
2017-02-07 15:34:09 +0100

    Setup_2014_10_EPT_Prod: reactivate TAPS channels for calibration

--- src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
@@ -19,10 +19,6 @@ public:
     {
         // see https://wwwa2.kph.uni-mainz.de/intern/daqwiki/analysis/beamtimes/2014-10-14
         IgnoreDetectorChannels(Detector_t::Type_t::CB, {17,125,189,265,267,456,547,549,557,565,582,586,597,602,672,677,678,696});
-        IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {2, 73, 127, 137, 138, 144, 145, 146, 147, 200, 218, 220, 222, 283, 291, 346, 353, 356, 357, 363, 364, 368, 437});
-
-        // Not enough statistics for runbyrun "-a 20"
-        IgnoreDetectorChannels(Detector_t::Type_t::TAPS, {0,3,64,72,74,75,139,149,210,217,273,284,295,347,358,362,367,419});
 
         IgnoreDetectorChannels(Detector_t::Type_t::TAPSVeto, {6,64,128,192,256,263,287,321,337,349});
     }
