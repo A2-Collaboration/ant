a918f8ed690ee9b54d8e0b30b7bdadf613a2bbcc
Martin Wolfes <wolfes@kph.uni-mainz.de>
2017-02-21 15:15:21 +0100

    Setyp_2014_12_EPT_Prod: added double peaking channel

--- src/expconfig/setups/Setup_2014_12_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_12_EPT_Prod.cc
@@ -17,24 +17,25 @@ public:
     Setup_2014_12_EPT_Prod(const std::string& name, OptionsPtr opt)
         : Setup_2014_EPT(name, opt)
     {
-        IgnoreDetectorChannel(Detector_t::Type_t::CB,  17); /// odd time [walk]
-        IgnoreDetectorChannel(Detector_t::Type_t::CB,  26); /// empty
-        IgnoreDetectorChannel(Detector_t::Type_t::CB,  41); /// odd time walk and energy
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 125); /// odd time [walk] and energy
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 265); /// few entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 547); /// few entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 549); /// no entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 557); /// double peak in time
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 565); /// no entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 582); /// odd time [walk]
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 586); /// few entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 597); /// no entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 602); /// odd time walk
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 672); /// odd time walk
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 677); /// few entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 678); /// inverted/odd spectra
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 679); /// no entries
-        IgnoreDetectorChannel(Detector_t::Type_t::CB, 696); /// odd time walk
+        IgnoreDetectorChannels(Detector_t::Type_t::CB,{  17,     /// odd time [walk]
+                                                         26,     /// empty
+                                                         41,     /// odd time walk and energy
+                                                        125,     /// odd time [walk] and energy
+                                                        265,     /// few entries
+                                                        418,     /// no peak in Energy calibration
+                                                        547,     /// few entries
+                                                        549,     /// no entries
+                                                        557,     /// double peak in time
+                                                        565,     /// no entries
+                                                        582,     /// odd time [walk]
+                                                        586,     /// few entries
+                                                        597,     /// no entries
+                                                        602,     /// odd time walk
+                                                        672,     /// odd time walk
+                                                        677,     /// few entries
+                                                        678,     /// inverted/odd spectra
+                                                        679,     /// no entries
+                                                        696  }); /// odd time walk
 
         IgnoreDetectorChannels(Detector_t::Type_t::TAPSVeto, {36,41,195,203,242,243,254,256,288,292,307,337,349,356,                /// few stat
                                                              128,129,130,287,320,                                                   /// noise
