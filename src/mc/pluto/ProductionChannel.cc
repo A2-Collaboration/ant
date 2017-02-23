ef46530635c1a07bf784998f04afc8f16a1a219a
Martin Wolfes <wolfes@kph.uni-mainz.de>
2017-02-23 18:24:29 +0100

    mc: pluto-cocktail: added new method of storing data

---
+++ src/mc/pluto/ProductionChannel.cc
@@ -0,0 +1,12 @@
+#include "ProductionChannel.h"
+
+#include "database/gp_ppi0.h"
+
+
+using namespace std;
+using namespace ant::mc::pluto;
+
+const ChannelDataBase_t ChannelDataBase(
+{
+            gp_ppi0
+});
---
+++ src/mc/pluto/ProductionChannel.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+
+#include "base/ParticleTypeTree.h"
+
+#include "Math/Interpolator.h"
+
+
+namespace ant
+{
+namespace mc
+{
+namespace pluto
+{
+
+using ChannelDataBase_t = std::map<ParticleTypeTreeDatabase::Channel,std::function<double(double)>>;
+
+struct DataPoint
+{
+    const double Energy;
+    const double Xsection;
+    DataPoint(double energy, double xsection):
+        Energy(energy),
+        Xsection(xsection){}
+};
+
+static std::function<double(double)> MakeInterPolator( const std::vector<DataPoint>& data)
+{
+    std::vector<double> dataE;
+    std::vector<double> dataXsec;
+
+    for (const auto& d: data)
+    {
+        dataE.emplace_back(d.Energy);
+        dataXsec.emplace_back(d.Xsection);
+    }
+    return [dataE,dataXsec] (double energy)
+    {
+        return ROOT::Math::Interpolator(dataE, dataXsec).Eval(energy);
+    };
+}
+
+
+static const ChannelDataBase_t ChannelDataBase;
+
+}
+}
+}
