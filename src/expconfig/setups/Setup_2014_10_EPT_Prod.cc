3ac1736327f265600925cc6d33f43a303c8ce9a0
Andreas Neiser <andreas.neiser@gmail.com>
2017-02-23 22:57:56 +0100

    Expconfig: Setup_2014_10: Ignore CB channels from Sergey

--- src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
+++ src/expconfig/setups/Setup_2014_10_EPT_Prod.cc
@@ -18,7 +18,7 @@ public:
         : Setup_2014_EPT(name, opt)
     {
         // see https://wwwa2.kph.uni-mainz.de/intern/daqwiki/analysis/beamtimes/2014-10-14
-        CB->SetElementFlag(Detector_t::ElementFlag_t::Broken, {17,125,189,265,267,456,547,549,557,565,582,586,597,602,672,677,678,696});
+        CB->SetElementFlag(Detector_t::ElementFlag_t::Broken, {125,549,557,565,597,677});
         TAPS->SetElementFlag(Detector_t::ElementFlag_t::Broken, {1,76, 128,149,347,353});
         TAPSVeto->SetElementFlag(Detector_t::ElementFlag_t::Broken, {6,64,128,192,256,263,287,321,337,349});
     }
