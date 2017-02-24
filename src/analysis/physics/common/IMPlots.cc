081627163cdfaba1616b7fede31dfe4fe1147b5d
Oliver Steffen <steffeo@uni-mainz.de>
2017-02-24 14:41:01 +0100

    analysis: IM_CB_TAPS_Plots: fix hist names

--- src/analysis/physics/common/IMPlots.cc
+++ src/analysis/physics/common/IMPlots.cc
@@ -162,8 +162,8 @@ IM_CB_TAPS_Plots::hist_t::hist_t(const HistogramFactory& HistFac,
     h_IM_CB    = histFac.makeTH1D("IM: CB",   "IM / MeV","",bins_IM,"IM_CB");
     h_IM_TAPS  = histFac.makeTH1D("IM: TAPS", "IM / MeV","",bins_IM,"IM_TAPS");
 
-    h_Angle_CB   = histFac.makeTH1D("Angle: CB",   "angle [#circ]","",bins_angle,"IM_CB");
-    h_Angle_TAPS = histFac.makeTH1D("Angle: TAPS", "angle [#circ]","",bins_angle,"IM_TAPS");
+    h_Angle_CB   = histFac.makeTH1D("Angle: CB",   "angle [#circ]","",bins_angle,"Angle_CB");
+    h_Angle_TAPS = histFac.makeTH1D("Angle: TAPS", "angle [#circ]","",bins_angle,"Angle_TAPS");
 }
 
 void IM_CB_TAPS_Plots::hist_t::Fill(const TCandidatePtrList& c_CB, const TCandidatePtrList& c_TAPS) const
