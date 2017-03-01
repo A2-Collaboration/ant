6b2036e245957f7603d0a8ce27313a7023d55a74
Andreas Neiser <neiser@kph.uni-mainz.de>
2017-03-01 09:32:00 +0100

    Expconfig: Setup_2014_EPT: Set thresholds to match Sergey's algorithm

--- src/expconfig/setups/Setup_2014_EPT.cc
+++ src/expconfig/setups/Setup_2014_EPT.cc
@@ -195,8 +195,8 @@ ant::ExpConfig::Setup::candidatebuilder_config_t Setup_2014_EPT::GetCandidateBui
 {
     candidatebuilder_config_t conf;
     conf.PID_Phi_Epsilon = std_ext::degree_to_radian(2.0);
-    conf.CB_ClusterThreshold = 15;
-    conf.TAPS_ClusterThreshold = 20;
+    conf.CB_ClusterThreshold = 12;
+    conf.TAPS_ClusterThreshold = 12;
     return conf;
 }
 
