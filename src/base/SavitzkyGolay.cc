9720fbbc4be0380302c7b1b1605c4e481f7262c5
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-07 08:55:36 +0100

    Base: SavitzkyGolay: Code with annoying zero-padding (will improve)

--- src/base/SavitzkyGolay.cc
+++ src/base/SavitzkyGolay.cc
@@ -26,15 +26,12 @@ SavitzkyGolay::SavitzkyGolay(int n_l, int n_r, int m) :
         return gsl_matrix_t(static_cast<gsl_matrix*>(::gsl_matrix_alloc(n1, n2)), ::gsl_matrix_free);
     };
 
-    // the code in the following will set h properly
+    // the code in the following will  eventually set h (the precomputed Savitzky Golay coefficients)
     h = gsl_matrix_alloc(points, points);
 
-    // gsl_permutation is only used here, so keep definitions in this scope
-    struct gsl_permutation : ::gsl_permutation {};
-    using gsl_permutation_t = deleted_unique_ptr<gsl_permutation>;
-
+    // gsl_permutation is only used here, so provide just the exception-safe unique_ptr allocator
     auto gsl_permutation_alloc = [] (size_t n) {
-        return gsl_permutation_t(static_cast<gsl_permutation*>(::gsl_permutation_alloc(n)), ::gsl_permutation_free);
+        return deleted_unique_ptr<gsl_permutation>(static_cast<gsl_permutation*>(::gsl_permutation_alloc(n)), ::gsl_permutation_free);
     };
 
     // provide error handling, as we're using unique_ptr, the code is exception safe!
@@ -70,11 +67,37 @@ SavitzkyGolay::SavitzkyGolay(int n_l, int n_r, int m) :
 
     // finally, compute H = V(V^TV)^(-1)V^T
     catch_gsl_error(gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, vandermonde.get(), vtv_inv_vt.get(), 0.0, h.get()));
-
 }
 
 vector<double> SavitzkyGolay::Smooth(const vector<double>& y)
 {
-    return y;
+    vector<double> result(y.size());
+    int d_n = y.size();
+    int points = d_sav_gol_points + d_smooth_points + 1;
+
+    // handle left edge by zero padding
+    for (int i = 0; i < d_sav_gol_points; i++){
+        double convolution = 0.0;
+        for (int k = d_sav_gol_points - i; k < points; k++)
+            convolution += gsl_matrix_get(h.get(), d_sav_gol_points, k) * y[i - d_sav_gol_points + k];
+        result[i] = convolution;
+    }
+    // central part: convolve with fixed row of h (as given by number of left points to use)
+    for (int i = d_sav_gol_points; i < d_n - d_smooth_points; i++){
+        double convolution = 0.0;
+        for (int k = 0; k < points; k++)
+            convolution += gsl_matrix_get(h.get(), d_sav_gol_points, k) * y[i - d_sav_gol_points + k];
+        result[i] = convolution;
+    }
+
+    // handle right edge by zero padding
+    for (int i = d_n - d_smooth_points; i < d_n; i++){
+        double convolution = 0.0;
+        for (int k = 0; i - d_sav_gol_points + k < d_n; k++)
+            convolution += gsl_matrix_get(h.get(), d_sav_gol_points, k) * y[i - d_sav_gol_points + k];
+        result[i] = convolution;
+    }
+
+    return result;
 }
 
--- test/base/TestSavitzkyGolay.cc
+++ test/base/TestSavitzkyGolay.cc
@@ -9,6 +9,8 @@ using namespace ant;
 TEST_CASE("SavitzkyGolay: Simple average", "[base/std_ext]") {
     SavitzkyGolay sg(2,2,0);
     auto smoothed = sg.Smooth({1,1,1,1,1});
-    auto expected = vector<double>{1,1,1,1,1};
-    REQUIRE(smoothed == expected);
+    // zero-padding, args
+    auto expected = vector<double>{0.6,0.8,1,0.8,0.6};
+    for(auto i=0u;i<expected.size();i++)
+        REQUIRE(smoothed[i] == Approx(expected[i]));
 }
