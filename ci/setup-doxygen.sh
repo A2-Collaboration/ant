ed52ef23171d27707346aebaf39aa15ea94738ed
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 23:15:49 +0100

    CI: Include doxygen again

--- ci/setup-doxygen.sh
+++ ci/setup-doxygen.sh
@@ -2,5 +2,5 @@
 wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.linux.bin.tar.gz -O doxygen.tar.gz
 mkdir doxygen
 tar -xzf doxygen.tar.gz -C doxygen --strip-components=1
-#rm doxygen.tar.gz
+rm doxygen.tar.gz
 export PATH=$(pwd)/doxygen/bin:$PATH
