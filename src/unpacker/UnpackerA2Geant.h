6fee83a5abfb59dc872f36b660cf897fb8a9aef3
Andreas Neiser <neiser@kph.uni-mainz.de>
2017-03-14 13:56:40 +0100

    Unpacker: A2Geant: Remove semicola

--- src/unpacker/UnpackerA2Geant.h
+++ src/unpacker/UnpackerA2Geant.h
@@ -58,34 +58,34 @@ private:
     long long current_entry = -1;
 
     struct TIDTree_t : WrapTTree {
-        ADD_BRANCH_T(TID, tid);
+        ADD_BRANCH_T(TID, tid)
     };
 
     TIDTree_t tidTree;
 
     struct GeantTree_t : WrapTTree {
-        ADD_BRANCH_T(ROOTArray<Float_t>, plab);
-        ADD_BRANCH_T(ROOTArray<Float_t>, tctaps);
-        ADD_BRANCH_T(ROOTArray<Float_t>, vertex);
-        ADD_BRANCH_T(ROOTArray<Float_t>, beam);
-        ADD_BRANCH_T(ROOTArray<Float_t>, ecryst);
-        ADD_BRANCH_T(ROOTArray<Float_t>, tcryst);
-        ADD_BRANCH_T(ROOTArray<Float_t>, ectapfs);
-        ADD_BRANCH_T(ROOTArray<Float_t>, ectapsl);
-        ADD_BRANCH_T(ROOTArray<Float_t>, elab);
-        ADD_BRANCH_T(ROOTArray<Float_t>, eveto);
-        ADD_BRANCH_T(ROOTArray<Float_t>, tveto);
-        ADD_BRANCH_T(ROOTArray<Float_t>, evtaps);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   icryst);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   ictaps);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   ivtaps);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   idpart);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   iveto);
-        ADD_BRANCH_T(ROOTArray<Int_t>,   imwpc);
-        ADD_BRANCH_T(ROOTArray<Float_t>, mposx);
-        ADD_BRANCH_T(ROOTArray<Float_t>, mposy);
-        ADD_BRANCH_T(ROOTArray<Float_t>, mposz);
-        ADD_BRANCH_T(ROOTArray<Float_t>, emwpc);
+        ADD_BRANCH_T(ROOTArray<Float_t>, plab)
+        ADD_BRANCH_T(ROOTArray<Float_t>, tctaps)
+        ADD_BRANCH_T(ROOTArray<Float_t>, vertex)
+        ADD_BRANCH_T(ROOTArray<Float_t>, beam)
+        ADD_BRANCH_T(ROOTArray<Float_t>, ecryst)
+        ADD_BRANCH_T(ROOTArray<Float_t>, tcryst)
+        ADD_BRANCH_T(ROOTArray<Float_t>, ectapfs)
+        ADD_BRANCH_T(ROOTArray<Float_t>, ectapsl)
+        ADD_BRANCH_T(ROOTArray<Float_t>, elab)
+        ADD_BRANCH_T(ROOTArray<Float_t>, eveto)
+        ADD_BRANCH_T(ROOTArray<Float_t>, tveto)
+        ADD_BRANCH_T(ROOTArray<Float_t>, evtaps)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   icryst)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   ictaps)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   ivtaps)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   idpart)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   iveto)
+        ADD_BRANCH_T(ROOTArray<Int_t>,   imwpc)
+        ADD_BRANCH_T(ROOTArray<Float_t>, mposx)
+        ADD_BRANCH_T(ROOTArray<Float_t>, mposy)
+        ADD_BRANCH_T(ROOTArray<Float_t>, mposz)
+        ADD_BRANCH_T(ROOTArray<Float_t>, emwpc)
     };
 
     GeantTree_t geantTree;
