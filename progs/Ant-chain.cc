1331c6901cb74a0629cbadfb69c235cb340d10c3
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-17 09:20:49 +0100

    Progs: Ant-chain: Improve indentation

--- progs/Ant-chain.cc
+++ progs/Ant-chain.cc
@@ -63,11 +63,12 @@ int main(int argc, char** argv) {
     TCLAP::CmdLine cmd("Ant-chain", ' ', "0.1");
     auto cmd_verbose = cmd.add<TCLAP::ValueArg<int>>("v","verbose","Verbosity level (0..9)", false, 0,"level");
     auto cmd_output     = cmd.add<TCLAP::ValueArg<string>>("o","output","Output file",true,"","filename");
-    auto cmd_inputfiles = cmd.add<TCLAP::UnlabeledMultiArg<string>>("inputfiles","input root files",true,"inputfiles");
     auto cmd_writemacro = cmd.add<TCLAP::SwitchArg>("","writemacro","Write a template macro file for opening",false);
     auto cmd_proofworkers  = cmd.add<TCLAP::ValueArg<unsigned>>("","proofworkers","Macro: Specify number of workers for PROOF, =0 disables it.",false,8,"n");
     auto cmd_ignoretreeevents  = cmd.add<TCLAP::SwitchArg>("","ignoretreeevents","Ignore the ubiquitious treeEvents",false);
     auto cmd_checkentries = cmd.add<TCLAP::SwitchArg>("","checkentries","Check the total entries of tree (might be slow)",false);
+    // unlabeled multi arg must be the last element added, and interprets everything as a input file
+    auto cmd_inputfiles = cmd.add<TCLAP::UnlabeledMultiArg<string>>("inputfiles","input root files",true,"inputfiles");
 
     cmd.parse(argc, argv);
 
@@ -75,74 +76,78 @@ int main(int argc, char** argv) {
     if(cmd_verbose->isSet())
         el::Loggers::setVerboseLevel(cmd_verbose->getValue());
 
-    if(cmd_inputfiles->isSet()) {
-        const auto& inputs = cmd_inputfiles->getValue();
-
-        const auto chain_names = GetTreeNames(inputs);
-        if(chain_names.empty()) {
-            LOG(ERROR) << "No TTrees found in input files";
-            return EXIT_FAILURE;
-        }
-
-        const string outfilename = cmd_output->getValue();
-        WrapTFileOutput outfile(outfilename, true);
-        list<TChain*> chains;
-        for(const auto& name : chain_names) {
-            if(cmd_ignoretreeevents->getValue() && name == "treeEvents")
-                continue;
-            auto chain = new TChain(name.c_str());
-            chains.push_back(chain);
-            gDirectory->Add(chain);
-        }
-
-        for(const auto& file : inputs) {
-            for(auto chain : chains) {
-                const auto absFile = std_ext::system::absolutePath(file);
-                const auto res = chain->AddFile(absFile.c_str());
-                if(res != 1) {
-                    LOG(WARNING) << "Problem with " << chain->GetName() << " and file " << file << " (" << res << ")";
-                }
-            }
-            LOG(INFO) << "Added " << file;
-        }
-
-        unique_ptr<ofstream> macrofile;
-        const string macrofilename = outfilename + ".C";
-        if(cmd_writemacro->getValue()) {
-            macrofile = std_ext::make_unique<ofstream>();
-            macrofile->open(macrofilename);
-            *macrofile << "{" << endl;
-            *macrofile << "TFile* myfile = TFile::Open(\"" << outfilename << "\");" << endl;
-            if(cmd_proofworkers->getValue()>0)
-                *macrofile << "TProof::Open(\"workers=" << cmd_proofworkers->getValue() << "\");" << endl;
-        }
-
-        unsigned n = 0;
-        for(auto chain : chains) {
-            if(cmd_checkentries->isSet())
-                LOG(INFO) << "Chain: " << chain->GetName() << ": " << chain->GetEntries() << " Entries";
-            else
-                LOG(INFO) << "Chain: " << chain->GetName();
-            if(macrofile) {
-                string chainname = "tree";
-                if(chains.size()>1)
-                    chainname += to_string(n);
-                *macrofile << "TChain* " << chainname << " = myfile->GetKey(\""
-                           << chain->GetName() <<  "\")->ReadObj();" << endl;
-                if(cmd_proofworkers->getValue()>0)
-                    *macrofile << chainname << "->SetProof();" << endl;
-
-            }
-            n++;
-        }
-
-        if(macrofile) {
-            *macrofile << "}" << endl;
-            macrofile->close();
-            LOG(INFO) << "Created macro '" << macrofilename << "'. Use it when starting ROOT.";
-        }
-
+    if(!cmd_inputfiles->isSet()) {
+        LOG(ERROR) << "Provide at least one inputfile";
+        return EXIT_FAILURE;
     }
+
+    const auto& inputs = cmd_inputfiles->getValue();
+
+    const auto chain_names = GetTreeNames(inputs);
+    if(chain_names.empty()) {
+        LOG(ERROR) << "No TTrees found in input files";
+        return EXIT_FAILURE;
+    }
+
+    const string outfilename = cmd_output->getValue();
+    WrapTFileOutput outfile(outfilename, true);
+    list<TChain*> chains;
+    for(const auto& name : chain_names) {
+        if(cmd_ignoretreeevents->getValue() && name == "treeEvents")
+            continue;
+        auto chain = new TChain(name.c_str());
+        chains.push_back(chain);
+        gDirectory->Add(chain);
+    }
+
+    for(const auto& file : inputs) {
+        for(auto chain : chains) {
+            const auto absFile = std_ext::system::absolutePath(file);
+            const auto res = chain->AddFile(absFile.c_str());
+            if(res != 1) {
+                LOG(WARNING) << "Problem with " << chain->GetName() << " and file " << file << " (" << res << ")";
+            }
+        }
+        LOG(INFO) << "Added " << file;
+    }
+
+    unique_ptr<ofstream> macrofile;
+    const string macrofilename = outfilename + ".C";
+    if(cmd_writemacro->getValue()) {
+        macrofile = std_ext::make_unique<ofstream>();
+        macrofile->open(macrofilename);
+        *macrofile << "{" << endl;
+        *macrofile << "TFile* myfile = TFile::Open(\"" << outfilename << "\");" << endl;
+        if(cmd_proofworkers->getValue()>0)
+            *macrofile << "TProof::Open(\"workers=" << cmd_proofworkers->getValue() << "\");" << endl;
+    }
+
+    unsigned n = 0;
+    for(auto chain : chains) {
+        if(cmd_checkentries->isSet())
+            LOG(INFO) << "Chain: " << chain->GetName() << ": " << chain->GetEntries() << " Entries";
+        else
+            LOG(INFO) << "Chain: " << chain->GetName();
+        if(macrofile) {
+            string chainname = "tree";
+            if(chains.size()>1)
+                chainname += to_string(n);
+            *macrofile << "TChain* " << chainname << " = myfile->GetKey(\""
+                       << chain->GetName() <<  "\")->ReadObj();" << endl;
+            if(cmd_proofworkers->getValue()>0)
+                *macrofile << chainname << "->SetProof();" << endl;
+
+        }
+        n++;
+    }
+
+    if(macrofile) {
+        *macrofile << "}" << endl;
+        macrofile->close();
+        LOG(INFO) << "Created macro '" << macrofilename << "'. Use it when starting ROOT.";
+    }
+
+
 
     return EXIT_SUCCESS;
 }
