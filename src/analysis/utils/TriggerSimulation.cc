bc2bde1b54edf712465203a34e01120c3e5f1a6f
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-17 17:16:38 +0100

    Analysis: Utils: Add TriggerSimulation (copied from Trigger, mostly)

---
+++ src/analysis/utils/TriggerSimulation.cc
@@ -0,0 +1,62 @@
+#include "TriggerSimulation.h"
+
+#include "tree/TEvent.h"
+#include "tree/TEventData.h"
+
+using namespace std;
+using namespace ant;
+using namespace ant::analysis::utils;
+
+bool TriggerSimulation::ProcessEvent(const TEvent& event)
+{
+    const auto& recon = event.Reconstructed();
+
+    const bool isMC = recon.ID.isSet(TID::Flags_t::MC);
+
+    // CBTiming as average over cluster timings
+    {
+        if(isMC) {
+            info.CBTiming = 0;
+        }
+        else {
+            double TimeEsum = 0.0;
+            double TimeE = 0.0;
+            for(const auto& cluster : recon.Clusters) {
+                if(cluster.DetectorType != Detector_t::Type_t::CB)
+                    continue;
+                // ignore weird clusters
+                if(!cluster.isSane())
+                    continue;
+                TimeEsum += cluster.Energy;
+                TimeE += cluster.Energy*cluster.Time;
+            }
+            info.CBTiming = TimeE/TimeEsum;
+        }
+    }
+
+    // CBEsum is sum over detector read hits
+    {
+        info.CBEnergySum = 0.0;
+        for(const TDetectorReadHit& dethit : recon.DetectorReadHits) {
+            if(dethit.DetectorType != Detector_t::Type_t::CB)
+                continue;
+            if(dethit.ChannelType != Channel_t::Type_t::Integral)
+                continue;
+            // one could also use uncalibrated values
+            // with some fixed constant? or average from all gains?
+            for(auto& energy : dethit.Values)
+                info.CBEnergySum += energy.Calibrated;
+        }
+    }
+
+    /// \todo Improve the trigger simulation on MC here, could also depend on config given to
+    /// TriggerSimulation ctor
+    // assume data has always triggered, no simulation needed
+    info.hasTriggered = !isMC || info.CBEnergySum > 550;
+
+    /// \todo The multiplicity is a much harder business, see acqu/root/src/TA2BasePhysics.cc
+    /// the code there might only apply to the old trigger system before 2012
+
+    // return true if information is complete and sane
+    return info.IsSane();
+}
---
+++ src/analysis/utils/TriggerSimulation.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "base/std_ext/math.h"
+
+namespace ant {
+
+struct TEvent;
+
+namespace analysis {
+namespace utils {
+
+class TriggerSimulation {
+
+    struct info_t {
+        bool   hasTriggered;
+        double CBEnergySum;
+        double CBTiming;
+        info_t() {
+            Reset(); // ensure reset on startup
+        }
+        void Reset() {
+            hasTriggered = false;
+            CBEnergySum = std_ext::NaN;
+            CBTiming = std_ext::NaN;
+        }
+        bool IsSane() {
+            return std::isfinite(CBEnergySum) &&
+                    std::isfinite(CBTiming);
+        }
+    };
+
+    info_t info;
+
+
+public:
+    /**
+     * @brief ProcessEvent inspects the full event to tell trigger decision
+     * @param event the event under investigation
+     * @return true if successful, false on failure
+     */
+    bool ProcessEvent(const TEvent& event);
+
+    /**
+     * @brief HasTriggered returns true if the experiment would have accepted this event
+     * @return the trigger decision
+     */
+    bool   HasTriggered() const { return info.hasTriggered; }
+
+    /**
+     * @brief GetCBEsum tries to calculate the energy sum
+     * @return Crystal Ball energy sum in MeV
+     */
+    double GetCBEnergySum() const { return info.CBEnergySum; }
+
+    /**
+     * @brief GetCBTiming tries to calculate the reference
+     * timing given by the analog energy sum
+     * @return Crystal Ball timing offset in nanoseconds
+     */
+    double GetCBTiming() const { return info.CBTiming; }
+};
+
+}
+}
+}
