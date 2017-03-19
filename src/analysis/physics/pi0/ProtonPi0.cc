e364d10905966266ffd55e253eaa9c3b1aac2c1c
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-19 08:46:39 +0100

    Analysis/Progs: Turn ProtonPi0_plot into Ant-plot plotter

--- src/analysis/physics/pi0/ProtonPi0.cc
+++ src/analysis/physics/pi0/ProtonPi0.cc
@@ -1,13 +1,19 @@
 #include "ProtonPi0.h"
 
+#include "physics/Plotter.h"
 #include "utils/uncertainties/Interpolated.h"
+#include "plot/CutTree.h"
+
+#include "expconfig/ExpConfig.h"
 
 #include "base/ParticleTypeTree.h"
 #include "base/std_ext/misc.h"
 
 using namespace std;
 using namespace ant;
+using namespace ant::analysis;
 using namespace ant::analysis::physics;
+using namespace ant::analysis::plot;
 
 ProtonPi0::ProtonPi0(const string& name, OptionsPtr opts) :
     Physics(name, opts),
@@ -159,7 +165,121 @@ void ProtonPi0::ShowResult()
             << endc;
 }
 
+struct Hist_t {
+    using Tree_t = ProtonPi0::Tree_t;
+    struct Fill_t {
+        Fill_t(const Tree_t& t) : Tree(t) {}
+        const Tree_t& Tree;
+        double Weight() const {
+            return Tree.TaggW;
+        }
+    };
+
+    TH1D* h_KinFitProb;
+    TH1D* h_TaggT;
+    TH1D* h_IM_2g;
+    TH1D* h_IM_2g_fitted;
+    TH1D* h_IM_2g_fitted_sub;
+    TH1D* h_nPhotonsCB;
+    TH2D* h_ProtonThetaEk;
+    TH2D* h_ProtonMinPIDPhi;
+    TH1D* h_ProtonMinPIDCh;
+    TH1D* h_ProtonPhi;
+    TH2D* h_Banana;
 
 
+    Hist_t(HistogramFactory HistFac, cuttree::TreeInfo_t) {
+        BinSettings bins_IM(100,50,250);
+
+        auto pid = ExpConfig::Setup::GetDetector(Detector_t::Type_t::PID);
+
+        h_KinFitProb = HistFac.makeTH1D("KinFitProb","p","",BinSettings(100,0,1),"h_KinFitProb");
+        h_TaggT = HistFac.makeTH1D("TaggT-CBAvgTime","t / ns","",BinSettings(200,-40,40),"h_TaggT");
+        h_IM_2g = HistFac.makeTH1D("IM_2g","IM / MeV","",bins_IM,"h_IM_2g");
+        h_IM_2g_fitted = HistFac.makeTH1D("IM_2g_fitted","IM / MeV","",bins_IM,"h_IM_2g_fitted");
+        h_IM_2g_fitted_sub = HistFac.makeTH1D("IM_2g_fitted_sub","IM / MeV","",bins_IM,"h_IM_2g_fitted_sub");
+        h_nPhotonsCB = HistFac.makeTH1D("nPhotonsCB","","",BinSettings(3),"h_nPhotonsCB");
+        h_ProtonThetaEk = HistFac.makeTH2D("Proton #theta E_k","Ek / MeV","#theta / #circ",
+                                           BinSettings(100,0,1000),BinSettings(40,20,80),
+                                           "h_ProtonThetaEk");
+        h_ProtonMinPIDPhi = HistFac.makeTH2D("Proton MinPID","#Delta#phi / #circ","PID Channel",
+                                             BinSettings(200,-200,200),BinSettings(pid->GetNChannels()),
+                                             "h_ProtonMinPIDPhi");
+        h_ProtonMinPIDCh = HistFac.makeTH1D("Proton MinPIDCh","PID Channel","",
+                                            BinSettings(pid->GetNChannels()),
+                                            "h_ProtonMinPIDCh");
+        h_ProtonPhi = HistFac.makeTH1D("Proton Phi","#phi / #circ","",BinSettings(pid->GetNChannels(), -180, 180),"h_ProtonPhi");
+        h_Banana = HistFac.makeTH2D("Banana","E_k / MeV","VetoE / MeV",
+                                    BinSettings(100,0,1000),BinSettings(100,0,10),
+                                    "h_Banana");
+    }
+
+    void Fill(const Fill_t& f) const {
+        h_KinFitProb->Fill(f.Tree.FitProb);
+        h_TaggT->Fill(f.Tree.TaggT-f.Tree.CBAvgTime);
+        h_IM_2g->Fill(f.Tree.IM_2g);
+        h_IM_2g_fitted->Fill(f.Tree.IM_2g_fitted);
+        h_IM_2g_fitted_sub->Fill(f.Tree.IM_2g_fitted, f.Weight());
+        h_nPhotonsCB->Fill(f.Tree.nPhotonsCB);
+        h_ProtonThetaEk->Fill(f.Tree.Proton_Ek, f.Tree.Proton_Theta);
+        h_ProtonMinPIDPhi->Fill(f.Tree.Proton_MinPIDPhi,f.Tree.Proton_MinPIDCh);
+        h_ProtonMinPIDCh->Fill(f.Tree.Proton_MinPIDCh);
+        h_ProtonPhi->Fill(f.Tree.Proton_Phi);
+        h_Banana->Fill(f.Tree.Proton_Ek, f.Tree.Proton_VetoE);
+    }
+
+    static cuttree::Cuts_t<Fill_t> GetCuts() {
+        using cuttree::MultiCut_t;
+        cuttree::Cuts_t<Fill_t> cuts;
+        using i_t = interval<double>;
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              {"prompt", [] (const Fill_t& f) { return i_t(-2.5, 2.5).Contains(f.Tree.TaggT-f.Tree.CBAvgTime); }},
+                              {"-", [] (const Fill_t&) { return true; } },
+                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              {"100<IM<180", [] (const Fill_t& f) { return i_t(100, 180).Contains(f.Tree.IM_2g_fitted); }},
+                              {"-", [] (const Fill_t&) { return true; } },
+                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              {"ProtonTheta>40", [] (const Fill_t& f) { return f.Tree.Proton_Theta>40; } },
+                              {"-", [] (const Fill_t&) { return true; } },
+                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              {"-25<Phi<25", [] (const Fill_t& f) { return i_t(-25,25).Contains(f.Tree.Proton_MinPIDPhi); } },
+                              {"-", [] (const Fill_t&) { return true; } },
+                          });
+        return cuts;
+    }
+};
+
+struct ProtonPi0_plot : Plotter {
+
+    Hist_t::Tree_t tree;
+
+    cuttree::Tree_t<Hist_t> mycuttree;
+
+    ProtonPi0_plot(const string& name, const WrapTFileInput& input, OptionsPtr opts) :
+        Plotter(name, input, opts)
+    {
+        if(!input.GetObject("ProtonPi0/t", tree.Tree))
+            throw Exception("Cannot find tree ProtonPi0/t");
+        tree.LinkBranches();
+
+        mycuttree = cuttree::Make<Hist_t>(HistFac, "tree", Hist_t::GetCuts());
+    }
+
+    virtual long long GetNumEntries() const override
+    {
+        return tree.Tree->GetEntries();
+    }
+
+    virtual void ProcessEntry(const long long entry) override
+    {
+        tree.Tree->GetEntry(entry);
+        cuttree::Fill<Hist_t>(mycuttree, tree);
+    }
+
+};
 
 AUTO_REGISTER_PHYSICS(ProtonPi0)
+AUTO_REGISTER_PLOTTER(ProtonPi0_plot)
