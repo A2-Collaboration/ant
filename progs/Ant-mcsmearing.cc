72f112bc3c5add27fb830e9a6ee6ff587bd6ae13
Oliver Steffen <steffeo@uni-mainz.de>
2017-03-22 11:38:39 +0100

    progs: Ant-mcsmearing: fix options

--- progs/Ant-mcsmearing.cc
+++ progs/Ant-mcsmearing.cc
@@ -85,7 +85,7 @@ int main(int argc, char** argv) {
     }
     auto app = new TRint("Ant-mcsmearing",&fake_argc,fake_argv,nullptr,0,true);
 
-    if(cmd_fit->isSet() && !cmd_step->isSet()) {
+    if(cmd_fit->isSet() && !cmd_step->isSet() && !cmd_compare->isSet()) {
         // FIT
         TH3* h_ETheta = nullptr;
         WrapTFileInput infile(cmd_fit->getValue());
@@ -103,65 +103,62 @@ int main(int argc, char** argv) {
         d->cd();
         ant::TwoPi0_MCSmearing_Tool::AnalyseChannelE(h_ETheta);
 
-        app->Run(kTRUE);
     }
-    else if(!cmd_fit->isSet()) {
+    else if(!cmd_fit->isSet() && ( cmd_compare->isSet() || cmd_step->isSet())) {
 
         WrapTFileInput datafile(cmd_data->getValue());
         WrapTFileInput mcfile(cmd_mc->getValue());
 
         const auto data_width = GetHist(datafile, "sigma");
         const auto mc_width   = GetHist(mcfile,   "sigma");
-        if(cmd_compare) {
+        if(!cmd_step->isSet()) {
 
-            canvas("Compare") << drawoption("colz") << data_width << mc_width << TwoPi0_MCSmearing_Tool::THDataMCDiff(data_width,mc_width,"datamc") << endc;
+            canvas("Compare") << drawoption("colz") << data_width << mc_width << TwoPi0_MCSmearing_Tool::THDataMCDiff(mc_width,data_width,"datamc") << endc;
 
-        }
-        if(cmd_step->isSet()) {
+        } else if(cmd_step->isSet()) {
 
-        const auto setup_name = cmd_setupname->getValue();
+            const auto setup_name = cmd_setupname->getValue();
 
-        ExpConfig::Setup::SetByName(setup_name);
-        auto& setup = ExpConfig::Setup::Get();
+            ExpConfig::Setup::SetByName(setup_name);
+            auto& setup = ExpConfig::Setup::Get();
 
-        manager = setup.GetCalibrationDataManager();
+            manager = setup.GetCalibrationDataManager();
 
-        manager->SetOverrideToDefault(true);
+            manager->SetOverrideToDefault(true);
 
-        const auto id = TID(0,0,{TID::Flags_t::MC});
+            const auto id = TID(0,0,{TID::Flags_t::MC});
 
-        TID next;
+            TID next;
 
-        // smearing
-        {
+            // smearing
+            {
 
-            const string calName = std_ext::formatter() << ToUpper(det) << "_ClusterSmearing";
-            TCalibrationData prev_data;
-            const auto prev_avail = manager->GetData(calName, id, prev_data, next);
+                const string calName = std_ext::formatter() << ToUpper(det) << "_ClusterSmearing";
+                TCalibrationData prev_data;
+                const auto prev_avail = manager->GetData(calName, id, prev_data, next);
 
-            TH2* smearing = nullptr;
+                TH2* smearing = nullptr;
 
-            if(prev_avail) {
-                TH2* prev_hist = calibration::detail::TH2Storage::Decode(prev_data);
-                smearing = TwoPi0_MCSmearing_Tool::CalculateUpdatedSmearing(data_width,mc_width, prev_hist);
-            } else {
-                smearing = TwoPi0_MCSmearing_Tool::CalculateInitialSmearing(data_width, mc_width);
+                if(prev_avail) {
+                    TH2* prev_hist = calibration::detail::TH2Storage::Decode(prev_data);
+                    smearing = TwoPi0_MCSmearing_Tool::CalculateUpdatedSmearing(data_width,mc_width, prev_hist);
+                } else {
+                    smearing = TwoPi0_MCSmearing_Tool::CalculateInitialSmearing(data_width, mc_width);
+                }
+
+                TCalibrationData cdata(calName, id, id);
+                calibration::detail::TH2Storage::Encode(smearing, cdata);
+
+                manager->Add(cdata,  Calibration::AddMode_t::AsDefault);
             }
 
-            TCalibrationData cdata(calName, id, id);
-            calibration::detail::TH2Storage::Encode(smearing, cdata);
-
-            manager->Add(cdata,  Calibration::AddMode_t::AsDefault);
         }
 
-        app->Run(kTRUE);
-        ExpConfig::Setup::Cleanup();
-        manager = nullptr;
+
     }
-    else {
-        LOG(ERROR) << "Don't specify --fit and --step at the same time. First fit, then step.";
-        return EXIT_FAILURE;
-    }
+    app->Run(kTRUE);
+    ExpConfig::Setup::Cleanup();
+    manager = nullptr;
 
     return EXIT_SUCCESS;
 }
