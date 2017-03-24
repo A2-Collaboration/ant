f44c5e923730abf3e16e785c993b7f0e00edb767
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-24 19:16:21 +0100

    Cmake: Remove external check for GSL, should be handled properly now

--- cmake/check_dependencies.cmake
+++ cmake/check_dependencies.cmake
@@ -1,19 +1,6 @@
 # check some dependencies in more detail to ensure the build process
 # does not fail because of the problems below
 
-find_package(GSL REQUIRED)
-# gsl is required for ant to build
-if(NOT GSL_FOUND)
-    message(FATAL_ERROR "gsl is needed! Please make sure to install libgsl-dev or gsl, \
-        depending on your Linux distribution.")
-endif()
-
-# check that the GSL include path is not empty --> gsl headers missing, required for ant/base
-if("${GSL_INCLUDE_DIR}" STREQUAL "")
-    message(FATAL_ERROR "gsl include directory empty, please make sure the \
-        gsl-dev package of your distribution is installed.")
-endif()
-
 # if gsl was missing during ROOT compilation, the mathmore package might be missing
 # check if it is available (needed for Interpolator)
 execute_process(COMMAND ${ROOT_CONFIG_EXECUTABLE} --features
