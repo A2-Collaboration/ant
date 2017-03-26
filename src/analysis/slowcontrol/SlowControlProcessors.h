e9deea7c3640135378b4dc7feacb7467d36dc713
Cristina Collicott <Collicott@dal.ca>
2017-03-26 23:27:48 +0200

    SlowControl: adding tagger scalers to processor list

--- src/analysis/slowcontrol/SlowControlProcessors.h
+++ src/analysis/slowcontrol/SlowControlProcessors.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "processors/EPT_Scalers.h"
+#include "processors/Tagger_Scalers.h"
 #include "processors/Beampolmon.h"
 #include "processors/ExpTrigger.h"
 #include "processors/PairSpec.h"
@@ -14,11 +15,12 @@ namespace slowcontrol {
 // keep list of available processors here, to be used by SlowControlVariables
 struct Processors {
 
-    static const std::shared_ptr<processor::Beampolmon>  Beampolmon;
-    static const std::shared_ptr<processor::EPT_Scalers> EPT_Scalers;
-    static const std::shared_ptr<processor::EPT_Or>      EPT_Or;
-    static const std::shared_ptr<processor::ExpTrigger>  ExpTrigger;
-    static const std::shared_ptr<processor::Beam>        Beam;
+    static const std::shared_ptr<processor::Beampolmon>     Beampolmon;
+    static const std::shared_ptr<processor::EPT_Scalers>    EPT_Scalers;
+    static const std::shared_ptr<processor::Tagger_Scalers> Tagger_Scalers;
+    static const std::shared_ptr<processor::EPT_Or>         EPT_Or;
+    static const std::shared_ptr<processor::ExpTrigger>     ExpTrigger;
+    static const std::shared_ptr<processor::Beam>           Beam;
 
 };
 
