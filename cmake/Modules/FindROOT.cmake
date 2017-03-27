d3d5486f9cc22803ea5e82247fb58f3c3955e862
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-27 10:01:30 +0200

    Cmake: FindROOT: Add HistPainter lib, fixes loading of Histograms from files

--- cmake/Modules/FindROOT.cmake
+++ cmake/Modules/FindROOT.cmake
@@ -43,7 +43,7 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 set(ROOT_LIBRARY_DIRS ${ROOT_LIBRARY_DIR})
 
-set(rootlibs Core Cint RIO Net Hist Graf Graf3d Gpad Tree Rint Postscript Matrix Physics MathCore Thread Gui)
+set(rootlibs Core Cint RIO Net Hist Graf Graf3d Gpad Tree Rint Postscript Matrix Physics MathCore Thread Gui HistPainter)
 set(ROOT_LIBRARIES)
 foreach(_cpt ${rootlibs} ${ROOT_FIND_COMPONENTS})
   find_library(ROOT_${_cpt}_LIBRARY ${_cpt} HINTS ${ROOT_LIBRARY_DIR})
