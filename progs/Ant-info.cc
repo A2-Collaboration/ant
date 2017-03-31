80036d52034e769f8edb80621f7a1df7f017b49f
Oliver Steffen <steffeo@uni-mainz.de>
2017-03-31 14:19:10 +0200

    progs: Ant-info: Print at least some infos in case no setup exists.
    Closes #39

--- progs/Ant-info.cc
+++ progs/Ant-info.cc
@@ -1,5 +1,6 @@
 #include "unpacker/Unpacker.h"
 #include "expconfig/ExpConfig.h"
+#include "expconfig/setups/Setup.h"
 
 #include "tree/TEvent.h"
 #include "tree/TEventData.h"
@@ -10,14 +11,41 @@
 using namespace std;
 using namespace ant;
 
+/**
+ * @brief Empty fake setup that always matches. Used in case no real setup exists.
+ */
+struct FakeSetup : ant::expconfig::Setup {
+    FakeSetup(): Setup("FakeSetup", make_shared<OptionsList>()) {}
+    bool Matches(const TID&) const override { return true; }
+    std::list<std::shared_ptr<Detector_t> > GetDetectors() const override { return {}; }
+};
+
 int main(int argc, char** argv)
 {
     SetupLogger();
 
     if(argc == 2) {
 
-        auto unpacker = Unpacker::Get(argv[1]);
-        LOG(INFO) << "Found setup: " <<  ExpConfig::Setup::Get().GetName();
+        // Try to find a setup for the file. Create an empty one if nothing was found.
+        const auto getUnpacker = [] (const string& filename) {
+            try {
+                auto unpacker = Unpacker::Get(filename);
+                LOG(INFO) << "Found setup: " <<  ExpConfig::Setup::Get().GetName();
+                return unpacker;
+            } catch(const ant::ExpConfig::Exception&) {
+
+                LOG(INFO) << "No setup found. Using a fake one. Expect errors!";
+            }
+
+            auto setup = make_shared<FakeSetup>();
+            expconfig::SetupRegistry::AddSetup(setup->GetName(), setup);
+            ExpConfig::Setup::SetByName(setup->GetName());
+
+            return Unpacker::Get(filename);
+
+        };
+
+        auto unpacker = getUnpacker(argv[1]);
 
         auto firstevent = unpacker->NextEvent();
         if(!firstevent) {
