a03523f7039d25cff21daf2b42eb34c2984df7fb
Andreas Neiser <neiser@kph.uni-mainz.de>
2017-04-20 10:42:40 +0200

    Cmake: Explicitly use --no-as-needed, fixes Ubuntu linking problems

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -65,15 +65,20 @@ endif()
 string(TOUPPER ${CMAKE_BUILD_TYPE} BUILD_TYPE)
 set(DEFAULT_COMPILE_FLAGS ${CMAKE_CXX_FLAGS_${BUILD_TYPE}})
 
+# build shared libraries by default
 SET(BUILD_SHARED_LIBS ON)
+
+# disable the --as-needed globally, fixes linking problems on Ubuntu
+set(DEFAULT_LINKER_FLAGS "-Wl,--no-as-needed")
 
 # check if gold linker is available and use it
 execute_process(COMMAND ${CMAKE_CXX_COMPILER} -fuse-ld=gold -Wl,--version ERROR_QUIET OUTPUT_VARIABLE LD_VERSION)
 if ("${LD_VERSION}" MATCHES "GNU gold")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-fuse-ld=gold")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-fuse-ld=gold")
-    message(STATUS "GNU gold linker will be used.")
+    set(DEFAULT_LINKER_FLAGS "${DEFAULT_LINKER_FLAGS} -Wl,-fuse-ld=gold")
+    message(STATUS "GNU google ld (gold) linker will be used.")
 endif()
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${DEFAULT_LINKER_FLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${DEFAULT_LINKER_FLAGS}")
 
 # for file(GLOB_RECURSE..) don't follow symlinks
 cmake_policy(SET CMP0009 NEW)
