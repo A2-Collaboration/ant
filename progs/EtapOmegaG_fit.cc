41d772e816d3053e4243d7aea39c040034d9f65e
Andreas Neiser <andreas.neiser@gmail.com>
2017-05-17 09:37:01 +0200

    Progs: EtapOmegaG_fit: Fit works with Gaussian convolution, chi2 still suspicious

--- progs/EtapOmegaG_fit.cc
+++ progs/EtapOmegaG_fit.cc
@@ -27,7 +27,14 @@
 #include "RooPlot.h"
 #include "RooDataHist.h"
 #include "RooAddition.h"
+#include "RooProduct.h"
 #include "RooChebychev.h"
+#include "RooConstVar.h"
+#include "RooDerivative.h"
+#include "RooFFTConvPdf.h"
+#include "RooChi2Var.h"
+#include "RooMinuit.h"
+#include "RooFitResult.h"
 
 using namespace ant;
 using namespace std;
@@ -72,19 +79,19 @@ int main(int argc, char** argv) {
         }
     }
 
+    // create TRint as RooFit internally creates functions/histograms, sigh...
+    argc=0; // prevent TRint to parse any cmdline
+    TRint app("EtapOmegaG_plot",&argc,argv,nullptr,0,true);
+
     unique_ptr<WrapTFileOutput> masterFile;
     if(cmd_output->isSet()) {
         // cd into masterFile upon creation
         masterFile = std_ext::make_unique<WrapTFileOutput>(cmd_output->getValue(), true);
     }
 
-
-
-
     // define observable and ranges
     RooRealVar var_IM("IM","IM", h_data->GetXaxis()->GetXmin(), h_data->GetXaxis()->GetXmax(), "MeV");
-    var_IM.setRange("bkg_l", var_IM.getMin(), 930);
-    var_IM.setRange("bkg_r", 990, var_IM.getMax());
+    var_IM.setBins(10000);
     var_IM.setRange("nonzero", var_IM.getMin(), 1000);
 
 
@@ -92,54 +99,70 @@ int main(int argc, char** argv) {
     RooDataHist h_roo_data("h_roo_data","dataset",var_IM,h_data);
 
     // build shifted mc lineshape
-    RooRealVar var_IM_shift("var_IM_shift", "shift in IM", -3.0, -10, 10);
-    RooAddition var_IM_shifted("var_IM_shifted","shifted IM",RooArgSet(var_IM,var_IM_shift));
+    RooRealVar var_IM_shift("var_IM_shift", "shift in IM", 0.0, -10.0, 10.0);
+    RooProduct var_IM_shift_invert("var_IM_shift_invert","shifted IM",RooArgSet(var_IM_shift, RooConst(-1.0)));
+    RooAddition var_IM_shifted("var_IM_shifted","shifted IM",RooArgSet(var_IM,var_IM_shift_invert));
     RooDataHist h_roo_mc("h_roo_mc","MC lineshape", var_IM, h_mc);
-    RooHistPdf pdf_mc_lineshape("pdf_mc_lineshape","MC lineshape as PDF", var_IM_shifted, var_IM, h_roo_mc, 4);
+    RooHistPdf pdf_mc_lineshape("pdf_mc_lineshape","MC lineshape as PDF", var_IM_shifted, var_IM, h_roo_mc, 2);
 
-    // build background
-//    const int polOrder = 5;
+    // build gaussian
+    RooRealVar  var_gauss_sigma("gauss_sigma","width of gaussian", 3.0, 0.0, 100.0);
+    RooGaussian pdf_gaussian("pdf_gaussian","Gaussian smearing", var_IM, RooConst(0.0), var_gauss_sigma);
+
+    // build signal as convolution, note that the gaussian must be the second PDF (see documentation)
+    RooFFTConvPdf pdf_signal("pdf_signal","MC_lineshape (X) gauss",var_IM, pdf_mc_lineshape, pdf_gaussian) ;
+
+    // build background (chebychev or argus?)
+
+//    const int polOrder = 6;
 //    std::vector<std::unique_ptr<RooRealVar>> bkg_params; // RooRealVar cannot be copied, so create them on heap
 //    RooArgSet roo_bkg_params;
 //    for(int p=0;p<polOrder;p++) {
 //        bkg_params.emplace_back(std_ext::make_unique<RooRealVar>((
-//                                    "p_"+to_string(p)).c_str(), ("Bkg Par "+to_string(p)).c_str(), 0, 0, 1)
+//                                    "p_"+to_string(p)).c_str(), ("Bkg Par "+to_string(p)).c_str(), 0.0, -10.0, 10.0)
 //                                );
 //        roo_bkg_params.add(*bkg_params.back());
 //    }
 //    RooChebychev pdf_background("pdf_background","Polynomial background",var_IM,roo_bkg_params);
+//    var_IM.setRange("bkg_l", var_IM.getMin(), 930);
+//    var_IM.setRange("bkg_r", 990, 1000);
+//    pdf_background.fitTo(h_roo_data, Range("bkg_l,bkg_r"), Extended()); // using Range(..., ...) does not work here (bug in RooFit, sigh)
 
     RooRealVar argus_pos("argus_pos","argus pos param", 1010, 1000, 1050);
-    RooRealVar argus_shape("argus_shape","argus shape param", -5.0, -10.0, 0.0);
-    RooRealVar argus_p("argus_p","argus p param", 1.2, 0.0, 2.0);
+    RooRealVar argus_shape("argus_shape","argus shape param", -5.0, -50.0, 0.0);
+    RooRealVar argus_p("argus_p","argus p param", 1.2, 0.0, 4.0);
     RooArgusBG pdf_background("argus","bkg argus",var_IM,argus_pos,argus_shape,argus_p);
-
 
     // build sum
     RooRealVar nsig("nsig","#signal events", 600, 0, 100000);
     RooRealVar nbkg("nbkg","#background events", 1000, 0, 100000);
-    RooAddPdf pdf_sum("pdf_sum","total sum",RooArgList(pdf_mc_lineshape,pdf_background),RooArgList(nsig,nbkg));
+    RooAddPdf pdf_sum("pdf_sum","total sum",RooArgList(pdf_signal,pdf_background),RooArgList(nsig,nbkg));
+
+    // do some pre-fitting to obtain better starting values, make sure function is non-zero in range
+    pdf_sum.chi2FitTo(h_roo_data, Range("nonzero"), Silence()); // using Range(..., ...) does not work here (bug in RooFit, sigh)
+
+    // do the actual maximum likelihood fit
+    pdf_sum.fitTo(h_roo_data, Extended(), SumW2Error(kTRUE), Range(var_IM.getMin(), var_IM.getMax()));
+    const auto numParams = pdf_sum.getVariables()->getSize();
+
+    // draw output, won't be shown in batch mode
+    RooPlot* frame = var_IM.frame();
+    h_roo_data.plotOn(frame);
+    pdf_sum.plotOn(frame, LineColor(kRed));
+    const auto chi2ndf = frame->chiSquare(numParams);
+    pdf_sum.plotOn(frame, Components(pdf_background), LineColor(kBlue));
+    pdf_sum.plotOn(frame, Components(pdf_signal), LineColor(kGreen));
+    frame->Draw();
+
+    LOG(INFO) << "numParams=" << numParams << " chi2ndf=" << chi2ndf;
 
     if(!cmd_batchmode->isSet()) {
         if(!std_ext::system::isInteractive()) {
             LOG(INFO) << "No TTY attached. Not starting ROOT shell.";
         }
         else {
-
-            argc=0; // prevent TRint to parse any cmdline
-            TRint app("EtapOmegaG_plot",&argc,argv,nullptr,0,true);
-
             if(masterFile)
                 LOG(INFO) << "Close ROOT properly to write data to disk.";
-
-            // do some fitting
-            pdf_sum.chi2FitTo(h_roo_data, Range("nonzero")); // using Range(..., ...) does not work here!
-
-            RooPlot* frame = var_IM.frame();
-            h_roo_data.plotOn(frame);
-            pdf_sum.plotOn(frame);
-            pdf_sum.plotOn(frame, Components(pdf_background), LineStyle(kDashed)) ;
-            frame->Draw();
 
             app.Run(kTRUE); // really important to return...
             if(masterFile)
