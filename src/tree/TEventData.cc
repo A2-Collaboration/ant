9b7108b9a9ccbc45d40b72bd618cd5df1470c0b7
Martin Wolfes <wolfes@kph.uni-mainz.de>
2017-05-18 11:54:25 +0200

    tree: TEventData: added output for particle Tree

--- src/tree/TEventData.cc
+++ src/tree/TEventData.cc
@@ -6,6 +6,38 @@ using namespace ant;
 TEventData::TEventData(const TID& id) : ID(id) {}
 TEventData::TEventData() = default;
 
+
+string _GetDecayString(const TParticleTree_t& particletree)
+{
+    if(!particletree)
+        return "empty_unknown";
+
+    stringstream s;
+
+    // the head is the beam part
+    s << particletree->Get()->Type().Name() << " -> ";
+
+    // ignore level==0 since it's the already handled beamparticle
+    size_t lastlevel = 1;
+    particletree->Map_level([&s, &lastlevel] (const TParticlePtr& p, size_t level) {
+        if(level>0) {
+            while(lastlevel<level) {
+                s << "[ "; lastlevel++;
+            }
+            while(lastlevel>level) {
+                s << "] "; lastlevel--;
+            }
+            assert(level == lastlevel);
+            s << p->Type().Name() << " ";
+        }
+    });
+
+    while(lastlevel-- > 1)
+        s << "] ";
+
+    return s.str();
+}
+
 namespace ant {
 ostream& operator<<(ostream& s, const TEventData& o) {
     s << "ID=" << o.ID << endl;
@@ -36,6 +68,11 @@ ostream& operator<<(ostream& s, const TEventData& o) {
     s << ">> Candidates: n=" << o.Candidates.size() << endl;
     for(auto& i : o.Candidates)
         s << i << endl;
+
+    if (o.ParticleTree)
+    {
+        s << ">> ParticleTree: " << _GetDecayString(o.ParticleTree) << endl;
+    }
 
     return s;
 }
