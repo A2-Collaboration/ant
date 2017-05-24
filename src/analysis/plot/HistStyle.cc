25580b97cc0b88c0d9093dd4065408e6f0a0daae
Andreas Neiser <andreas.neiser@gmail.com>
2017-05-24 21:07:41 +0200

    Analysis: Plot: Call Sumw2 only if not already created

--- src/analysis/plot/HistStyle.cc
+++ src/analysis/plot/HistStyle.cc
@@ -68,7 +68,8 @@ Mod_t Mod_t::MakeDataPoints(Color_t color, short linewidth) {
         h->SetLineWidth(linewidth);
         h->SetMarkerColor(color);
         h->SetMarkerStyle(kDot);
-        h->Sumw2();
+        if(h->GetSumw2N()==0)
+            h->Sumw2();
         return ModOption_t{"E"}; // draw error bars
     };
 
