f1ac790e173b579197412bd8a96ff0747cc9b90b
Andreas Neiser <andreas.neiser@gmail.com>
2017-06-23 10:30:05 +0200

    Analysis: Slowcontrol: Connect TaggEff variable to processor

--- src/analysis/slowcontrol/variables/TaggEff.cc
+++ src/analysis/slowcontrol/variables/TaggEff.cc
@@ -1,26 +1,19 @@
 #include "TaggEff.h"
-
 #include "SlowControlProcessors.h"
-
-#include "expconfig/ExpConfig.h"
-
-#include "base/Logger.h"
 
 using namespace std;
 using namespace ant;
 using namespace ant::analysis::slowcontrol;
 using namespace ant::analysis::slowcontrol::variable;
 
-
 list<Variable::ProcessorPtr> TaggEff::GetNeededProcessors() const
 {
-    return {};
+    return {Processors::TaggEff};
 }
 
 TaggerDetector_t::taggeff_t TaggEff::Get(unsigned channel) const
 {
-    (void)channel;
-    return {std_ext::NaN,std_ext::NaN};
+    return Processors::TaggEff->Get().at(channel);
 }
 
 
