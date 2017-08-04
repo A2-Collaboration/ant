7a5848bec748825cc67b16bef15ed65c71cc408a
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-08-04 15:55:11 +0200

    Base: std_ext: Change sorting functions for vector indices, provide them in ascending and descedning order

--- src/base/std_ext/vector.h
+++ src/base/std_ext/vector.h
@@ -20,6 +20,24 @@ std::vector<size_t> get_sorted_indices(std::vector<T> vec)
     std::iota(v.begin(), v.end(), 0);
     std::sort(v.begin(), v.end(),
               [vec] (size_t i, size_t j) {
+        return vec[i] < vec[j];
+    });
+
+    return v;
+}
+
+/**
+ * @brief Takes a vector and compares its contents to obtain sorted indices in descending order
+ * @param vec vector containing comparable objects
+ * @return vector<size_t> of sorted indices
+ */
+template<typename T>
+std::vector<size_t> get_sorted_indices_desc(std::vector<T> vec)
+{
+    std::vector<size_t> v(vec.size());
+    std::iota(v.begin(), v.end(), 0);
+    std::sort(v.begin(), v.end(),
+              [vec] (size_t i, size_t j) {
         return vec[i] > vec[j];
     });
 
