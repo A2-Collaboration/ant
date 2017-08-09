eaf715b0c20b5361729d20c253090072597aa667
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-08-09 16:12:17 +0200

    Physics: EtapDalitz_plot: Include TFF dedicated plot always, re-enable cuts

--- src/analysis/physics/etaprime/EtapDalitz_plot.cc
+++ src/analysis/physics/etaprime/EtapDalitz_plot.cc
@@ -497,8 +497,8 @@ struct SigHist_t : Hist_t<physics::EtapDalitz::SigTree_t> {
             h->Fill(f.Tree.photons().at(idx[2]).Time);
         });
 
-        if (!isLeaf)
-            return;
+//        if (!isLeaf)
+//            return;
 
         AddTH2("IM(e+e-) vs. IM(e+e-g) [TFF]", "IM(e+e-g) [MeV]", "IM(e+e-) [MeV]", BinSettings(240, 0, 1200), BinSettings(20, 0, 1000), "TFFextract",
                [] (TH2D* h, const Fill_t& f) {
@@ -644,24 +644,24 @@ struct SigHist_t : Hist_t<physics::EtapDalitz::SigTree_t> {
 //                               }}
 //                          });
 
-//        cuts.emplace_back(MultiCut_t<Fill_t>{
-//                              //{"lateral moment < .97", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .97); }},
-//                              {"lateral moment < .98", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .98); }},
-//                              //{"lateral moment < .99", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .99); }}
-//                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              //{"lateral moment < .97", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .97); }},
+                              {"lateral moment < .98", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .98); }},
+                              //{"lateral moment < .99", [] (const Fill_t& f) { return TreeCuts::lateral_moment(f, .99); }}
+                          });
 
-//        cuts.emplace_back(MultiCut_t<Fill_t>{
-//                              {"MM < 1030 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1030; }},
-//                              //{"MM < 1010 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1010; }},
-//                              {"MM < 1000 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1000; }},
-//                              {"MM < 990 MeV",  [] (const Fill_t& f) { return f.Tree.mm().M() < 990; }}
-//                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              {"MM < 1030 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1030; }},
+                              //{"MM < 1010 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1010; }},
+                              {"MM < 1000 MeV", [] (const Fill_t& f) { return f.Tree.mm().M() < 1000; }},
+                              {"MM < 990 MeV",  [] (const Fill_t& f) { return f.Tree.mm().M() < 990; }}
+                          });
 
-//        cuts.emplace_back(MultiCut_t<Fill_t>{
-//                              //{"PID e^{#pm} > .4 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .4); }},
-//                              {"PID e^{#pm} > .5 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .5); }},
-//                              {"PID e^{#pm} > .6 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .6); }}
-//                          });
+        cuts.emplace_back(MultiCut_t<Fill_t>{
+                              //{"PID e^{#pm} > .4 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .4); }},
+                              {"PID e^{#pm} > .5 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .5); }},
+                              {"PID e^{#pm} > .6 MeV", [] (const Fill_t& f) { return TreeCuts::pid_cut(f, .6); }}
+                          });
 
         return cuts;
     }
