fc9c60e5f73c140549e96ea3ef94d388c928b086
Oliver Steffen <steffeo@uni-mainz.de>
2017-08-25 19:12:16 +0200

    base: add ForLoopCounter

---
+++ src/base/ForLoopCounter.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iterator>
+#include <iostream>
+
+namespace ant {
+
+template <typename T>
+struct ForLoopCounter {
+    const T start;
+    const T stop;
+
+    constexpr ForLoopCounter(T Stop) noexcept : start(0), stop(Stop) {}
+    constexpr ForLoopCounter(T Start, T Stop) noexcept : start(Start), stop(Stop) {}
+
+    constexpr T counts() const noexcept {
+        return stop-start;
+    }
+
+    struct iterator : std::iterator_traits<T> {
+        const ForLoopCounter& counter;
+        T i;
+        T last;
+        iterator(const ForLoopCounter& c, T start) noexcept : counter(c), i(start), last(i) {}
+
+        iterator& operator++() {
+            ++i;
+            if(i-last >= 0.1*counter.counts()) {
+                std::cout << "Progress: " << 100.0*(i-counter.start)/counter.counts() << "%" << std::endl;
+                last = i;
+            }
+        }
+
+        bool constexpr operator==(const iterator& other) const noexcept {
+            return i == other.i;
+        }
+
+        bool constexpr operator!=(const iterator& other) const noexcept {
+            return i != other.i;
+        }
+
+        const constexpr T& operator*() const noexcept {
+            return i;
+        }
+
+    };
+
+    constexpr iterator begin() const noexcept {
+        return {*this, start};
+    }
+
+    constexpr iterator end() const noexcept {
+        return {*this, stop};
+    }
+
+    constexpr iterator cbegin() const noexcept {
+        return {*this, start};
+    }
+
+    constexpr iterator cend() const noexcept {
+        return {*this, stop};
+    }
+};
+
+}
