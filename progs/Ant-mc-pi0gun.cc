ddeda1aa562e7ed045ebad02d1a6a6a75efecaff
Martin Sobotzik <msobotzi@students.uni-mainz.de>
2017-08-30 11:25:47 +0200

    Analysis: Sobotzik: Now the mass of the produced particle is variable

--- progs/Ant-mc-pi0gun.cc
+++ progs/Ant-mc-pi0gun.cc
@@ -90,10 +90,10 @@ LorentzVec rndPhotonbeamenergy()
 }
 
 
-std::pair<LorentzVec,LorentzVec> Pi0Boost()
+std::pair<LorentzVec,LorentzVec> Pi0Boost(double m_pi)
 {
 
-    auto m_pi = ParticleTypeDatabase::Pi0.Mass()/1000.0;
+//    auto m_pi = ParticleTypeDatabase::Pi0.Mass()/1000.0;
     auto m_p  = ParticleTypeDatabase::Proton.Mass()/1000.0;
 
     const LorentzVec Target = {{0,0,0}, m_p};
@@ -173,7 +173,7 @@ int main(int argc, char** argv) {
     auto cmd_reqsym    = cmd.add<TCLAP::SwitchArg>             ("",   "sym",         "Require symmetric photon energies");
     auto cmd_zboost    = cmd.add<TCLAP::SwitchArg>             ("",   "zboost",      "Boost the Pions in z-Direction; True or False");
     auto cmd_Prod      = cmd.add<TCLAP::SwitchArg>             ("",   "Prod",        "Get the Product of the Pion; Change Beam Energy with E_min and E_max"  );
-
+    auto cmd_Pi0       = cmd.add<TCLAP::ValueArg<double>>     ("",   "Eta",          "If false than Pi0 Mass", false, ParticleTypeDatabase::Pi0.Mass() / GeV,"double [MeV]");
 
     gRandom->SetSeed();
 
@@ -186,12 +186,16 @@ int main(int argc, char** argv) {
     bool zboost = cmd_zboost->getValue();
     bool Prod = cmd_Prod->getValue();
 
+
+    const auto mass = cmd_Pi0->getValue();
+
+
     Erange = interval<double>(cmd_Emin->getValue(), cmd_Emax->getValue()) / 1000.0;
     if(Prod)
     {
-        if (Erange.Start()<0.135)
+        if (Erange.Start()<mass)
         {
-            cout<<"For Prod Emin must be greater than 135 MeV"<<endl;
+            cout<<"For Prod Emin must be greater than "<<mass<<" 135 MeV"<<endl;
             return EXIT_FAILURE;
         }
     }
@@ -199,7 +203,7 @@ int main(int argc, char** argv) {
 
     TTree* tree = new TTree("data","");
 
-    const auto mass = ParticleTypeDatabase::Pi0.Mass() / 1000.0; // GeV
+
 
 /*    const auto nParticles = Prod ? 4 : 3*/;
 
@@ -239,7 +243,7 @@ int main(int argc, char** argv) {
         }
         else
         {
-            const auto pip = Pi0Boost();
+            const auto pip = Pi0Boost(mass);
             pi0lv = pip.first;
         }
 
