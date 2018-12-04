be63a22770fa44f6f9daa964832f5756b009c72f
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-12-04 11:48:17 +0100

    CI: Let wget retry to download doxygen up to 3 times in case of connectivity issues

--- ci/setup-doxygen.sh
+++ ci/setup-doxygen.sh
@@ -1,5 +1,6 @@
 # Get Doxygen
-wget https://github.com/A2-Collaboration/travis-container-packets/blob/doxygen/doxygen-1.8.13.linux.bin.tar.gz?raw=true -O doxygen.tar.gz
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=3 \
+	https://github.com/A2-Collaboration/travis-container-packets/blob/doxygen/doxygen-1.8.13.linux.bin.tar.gz?raw=true -O doxygen.tar.gz
 mkdir doxygen
 tar -xzf doxygen.tar.gz -C doxygen --strip-components=1
 rm doxygen.tar.gz
