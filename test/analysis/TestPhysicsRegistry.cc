248ec7d888adb212e05198facfe0b7a55af3fa16
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-12-08 13:07:01 +0100

    Test: Fix loop to obtain initial intent

--- test/analysis/TestPhysicsRegistry.cc
+++ test/analysis/TestPhysicsRegistry.cc
@@ -52,9 +52,12 @@ void dotest() {
         /// \todo Check if the bug reported <a href="https://sft.its.cern.ch/jira/browse/ROOT-9450">here</a> is fixed and remove exclusion
         /// \bug Certain physics classes will probably crash due to <a href="https://sft.its.cern.ch/jira/browse/ROOT-9450">ROOT-9450</a>
         #if ROOT_VERSION_CODE >= ROOT_VERSION(6,0,0)
+        bool badPhysics = false;
         for (const string& physics : {"EtapDalitz", "EtapOmegaG", "Omega_EpEm", "sigmaPlus", "triplePi0"})
             if (!physics.compare(name))
-                continue;
+                badPhysics = true;
+        if (badPhysics)
+            continue;
         #endif
 
         cout << "Running physics class " << name << endl;
