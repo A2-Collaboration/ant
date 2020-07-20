46f415636cb2e339ee2fdb4fdf85e6ed58899837
Jenna Chisholm <jenna@thanos.mta.ca>
2020-07-20 11:34:11 -0300

    Had to fix the new cut so Ant would build.

--- src/analysis/physics/calibration/Time.cc
+++ src/analysis/physics/calibration/Time.cc
@@ -79,17 +79,20 @@ void Time::ProcessEvent(const TEvent& event, manager_t&)
 	// only use 1 events to reduce background and help identify prompt peaks in early channels
 	// only use clusters with more than two crystals hit (more likely to be photons)
 	if (event.Reconstructed().Candidates.size() == 1 ){
-	if (event.Reconstructed().Candidates.FindCaloCluster().Hits.size() > 2){
-        	for (const auto& tHit: event.Reconstructed().TaggerHits) {
-                	hTime->Fill(tHit.Time, tHit.Channel);
-                	hTimeZoomed->Fill(tHit.Time, tHit.Channel);
-                	hTimeToTriggerRef->Fill(tHit.Time - TriggerRefTime, tHit.Channel);
-		}
-        }}
+		for(const auto& candidate : event.Reconstructed().Candidates){
+			if (candidate.ClusterSize > 2){
+        			for (const auto& tHit: event.Reconstructed().TaggerHits) {
+                			hTime->Fill(tHit.Time, tHit.Channel);
+                			hTimeZoomed->Fill(tHit.Time, tHit.Channel);
+                			hTimeToTriggerRef->Fill(tHit.Time - TriggerRefTime, tHit.Channel);
+				}
+        		}
+		}	
+	}
     }
     else {
-        for(const auto& cand: event.Reconstructed().Candidates) {
-            for(const TCluster& cluster: cand.Clusters) {
+    	for(const auto& cand: event.Reconstructed().Candidates) {
+     	   for(const TCluster& cluster: cand.Clusters) {
                 if(cluster.DetectorType != Detector->Type)
                     continue;
                 hTime->Fill(cluster.Time, cluster.CentralElement);
